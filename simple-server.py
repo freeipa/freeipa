50e79f3e60f441124b51862348bfea2b5af0e9ce
Jason Gerard DeRose <jderose@redhat.com>
2008-10-02 18:05:31 -0600

    Added docstring to simple-server.py

--- simple-server.py
+++ simple-server.py
@@ -19,6 +19,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+"""
+A simple XML-RPC test server using SimpleXMLRPCServer.
+
+The server will run at http://localhost:8080
+"""
+
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 from ipalib import api
 from ipalib import load_plugins
