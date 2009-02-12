c9311da3fd74133fba37c7194d713e03e463a3e3
Jakub Hrozek <jhrozek@redhat.com>
2009-02-12 16:20:46 +0100

    Install policy schema

--- ipaserver/install/dsinstance.py
+++ ipaserver/install/dsinstance.py
@@ -268,6 +268,8 @@ class DsInstance(service.Service):
                         schema_dirname(self.serverid) + "60ipaconfig.ldif")
         shutil.copyfile(ipautil.SHARE_DIR + "60basev2.ldif",
                         schema_dirname(self.serverid) + "60basev2.ldif")
+        shutil.copyfile(ipautil.SHARE_DIR + "60policyv2.ldif",
+                        schema_dirname(self.serverid) + "60policyv2.ldif")
         shutil.move(schema_dirname(self.serverid) + "05rfc2247.ldif",
                         schema_dirname(self.serverid) + "05rfc2247.ldif.old")
         shutil.copyfile(ipautil.SHARE_DIR + "05rfc2247.ldif",
