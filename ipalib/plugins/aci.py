763c7ef9141becfe968779fc058eba82a2d1bea8
David O'Brien <daobrien@daobrien.csb>
2009-04-28 17:47:34 +1000

    trivial update to standardize terms in docstring

--- ipalib/plugins/aci.py
+++ ipalib/plugins/aci.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 """
-Frontend plugins for managing DS ACIs
+Front-end plugins for managing DS ACIs
 """
 
 from ipalib import api, crud, errors
