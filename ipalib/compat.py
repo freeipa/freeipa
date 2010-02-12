a63224f4dced1f81eee1f2fe99dbfd4c8c25a083
Jason Gerard DeRose <jderose@redhat.com>
2010-02-12 13:03:14 -0700

    Add sha1, md5 to compat

--- ipalib/compat.py
+++ ipalib/compat.py
@@ -18,11 +18,14 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 """
-Abstracts some compatability issues for Python2.4 - Python2.6.
+Abstracts some compatibility issues for Python 2.4 - Python 2.6.
 
-The ``json`` module was added in Python2.6, which previously was in a seperate
-package and called ``simplejson``.  This hack abstracts the difference so you
-can use the ``json`` module generically like this:
+Python 2.6
+==========
+
+The ``json`` module was added in Python 2.6, which previously was in an external
+package and called ``simplejson``.  The `compat` module abstracts the difference
+so you can use the ``json`` module generically like this:
 
 >>> from compat import json
 >>> json.dumps({'hello': 'world'})
@@ -40,6 +43,28 @@ future-proofing here so you can import ``parse_qs()`` generically like this:
 For more information, see *What's New in Python 2.6*:
 
     http://docs.python.org/whatsnew/2.6.html
+
+
+Python 2.5
+==========
+
+The ``hashlib`` module was added in Python2.5, after which use of the ``sha``
+and ``md5`` modules is deprecated.  You can generically import a ``sha1`` class
+from the `compat` module like this:
+
+>>> from compat import sha1
+>>> sha1('hello world').hexdigest()
+'2aae6c35c94fcfb415dbe95f408b9ce91ee846ed'
+
+And generically import an ``md5`` class like this:
+
+>>> from compat import md5
+>>> md5('hello world').hexdigest()
+'5eb63bbbe01eeed093cb22bb8f5acdc3'
+
+For more information, see *What's New in Python 2.5*:
+
+    http://python.org/doc/2.5/whatsnew/whatsnew25.html
 """
 
 import sys
@@ -49,3 +74,8 @@ if sys.version_info[:2] >= (2, 6):
 else:
     import simplejson as json
     from cgi import parse_qs
+try:
+    from hashlib import sha1, md5
+except ImportError:
+    from sha import new as sha1
+    from md5 import new as md5
