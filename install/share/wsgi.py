897b296a694224f9d00a4bfcc80f818c858fbfb5
Rob Crittenden <rcritten@redhat.com>
2010-08-10 10:00:35 -0400

    Make the server log level more configurable, not defaulting to debug.
    This disables debug output in the Apache log by default. If you want
    increased output create /etc/ipa/server.conf and set it to:
    
    [global]
    debug=True
    
    If this is too much output you can select verbose output instead:
    
    [global]
    debug=False
    verbose=True
    
    ticket 60

--- install/share/wsgi.py
+++ install/share/wsgi.py
@@ -1,9 +1,19 @@
 """
 WSGI appliction for IPA server.
 """
-
 from ipalib import api
-api.bootstrap(context='server', debug=True, log=None)
+from ipalib.config import Env
+from ipalib.constants import DEFAULT_CONFIG
+
+# Determine what debug level is configured. We can only do this
+# by reading in the configuration file(s). The server always reads
+# default.conf and will also read in `context'.conf.
+env = Env()
+env._bootstrap(context='server', log=None)
+env._finalize_core(**dict(DEFAULT_CONFIG))
+
+# Initialize the API with the proper debug level
+api.bootstrap(context='server', debug=env.debug, log=None)
 try:
     api.finalize()
 except StandardError, e:
