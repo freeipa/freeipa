b683c7261bcadc6899168d7cff8bd218a3501ba6
Nalin Dahyabhai <nalin@redhat.com>
2010-11-24 17:39:46 -0500

    build tweaks - use automake's foreign mode, avoid creating empty files to satisfy gnu mode - run autoreconf -f to ensure that everything matches

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,3 @@
 #!/bin/sh
-
-# automake demands these files exist when run in gnu mode which is the default,
-# automake can be run in foreign mode to avoid failing on the absence of these
-# files, but unfortunately there is no way to pass the --foreign flag to
-# automake when run from autoreconf.
-for f in NEWS README AUTHORS ChangeLog; do
-    if [ ! -e $f ]; then
-        touch $f
-    fi
-done
-
-autoreconf -i
+autoreconf -i -f
 ./configure ${1+"$@"}
