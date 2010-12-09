7493d781dfcaa7995f7864a09ad39ba6a89f1a9c
Jakub Hrozek <jhrozek@redhat.com>
2010-12-09 13:59:11 +0100

    Change FreeIPA license to GPLv3+
    The changes include:
     * Change license blobs in source files to mention GPLv3+ not GPLv2 only
     * Add GPLv3+ license text
     * Package COPYING not LICENSE as the license blobs (even the old ones)
       mention COPYING specifically, it is also more common, I think
    
     https://fedorahosted.org/freeipa/ticket/239

--- ipalib/plugins/batch.py
+++ ipalib/plugins/batch.py
@@ -5,18 +5,18 @@
 # Copyright (c) 2010  Red Hat
 # See file 'copying' for use and warranty information
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the gnu general public license as
-# published by the free software foundation; version 2 only
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
-# but without any warranty; without even the implied warranty of
-# merchantability or fitness for a particular purpose.  See the
-# gnu general public license for more details.
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the gnu general public license
-# along with this program; if not, write to the Free Software
-# Foundation, inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Plugin to make multiple ipa calls via one remote procedure call
