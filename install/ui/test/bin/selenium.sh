e3a76d435e5ff312f503afb899ef6b505621d48a
Endi S. Dewata <edewata@redhat.com>
2011-03-30 22:52:28 -0400

    Initial Selenium test cases.
    http://www.freeipa.org/page/Selenium

---
+++ install/ui/test/bin/selenium.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+
+TEST_SUITE=$1
+TEST_RESULTS=$2
+
+if [ "$TEST_SUITE" = "" -o "$TEST_RESULTS" = "" ]
+then
+    echo "Usage: $0 <test suite> <test results>"
+    exit 1
+fi
+
+if [ ! -f $TEST_SUITE ]
+then
+    echo "Error: Test suite $TEST_SUITE not found."
+    exit 1
+fi
+
+PROFILE_DIR=$HOME/.mozilla/firefox
+PROFILE=`ls $PROFILE_DIR | grep .default`
+PROFILE_TEMPLATE=$PROFILE_DIR/$PROFILE
+
+java -jar /usr/share/java/selenium-server-standalone.jar -firefoxProfileTemplate $PROFILE_TEMPLATE -htmlSuite "*firefox" "http://localhost" $TEST_SUITE $TEST_RESULTS
+
+# Kill Firefox
+ps -ef|grep -i firefox|grep '\-profile'|awk '{print $2;}'|xargs kill -9
