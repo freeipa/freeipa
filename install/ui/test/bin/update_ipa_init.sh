bc9fd7a342f64d5612e5e9673eddbaee8af84dd1
Adam Young <ayoung@redhat.com>
2011-05-12 12:50:11 -0400

    whitespace and -x removal

--- install/ui/test/bin/update_ipa_init.sh
+++ install/ui/test/bin/update_ipa_init.sh
@@ -1,21 +1,20 @@
-#!/bin/bash -x
+#!/bin/bash
 
 FIXTURE_DIR=$1
 
 INIT_FILE=$FIXTURE_DIR/ipa_init.json
 
 usage(){
-	echo "$0 {FIXTURE_DIR}"
-	exit 1
+        echo "$0 {FIXTURE_DIR}"
+        exit 1
 }
 
 if [ ! -f $INIT_FILE ]
 then
-	usage
-	exit 1
+        usage
+        exit 1
 fi
 
 
 
-curl -v -H "Content-Type:application/json" -H "Accept:applicaton/json" --negotiate -u :  --cacert /etc/ipa/ca.crt  -d '{"method":"batch","params":[[ {"method":"json_metadata","params":[[],{}]}, {"method":"i18n_messages","params":[[],{}]}, {"method":"user_find","params":[[],{"whoami":"true","all":"true"}]}, {"method":"env","params":[[],{}]}, {"method":"dns_is_enabled","params":[[],{}]} ],{}],"id":1}'  -X POST  https://`hostname`/ipa/json  | sed 's/[ \t]*$//' >   $INIT_FILE 
-
+curl -v -H "Content-Type:application/json" -H "Accept:applicaton/json" --negotiate -u :  --cacert /etc/ipa/ca.crt  -d '{"method":"batch","params":[[ {"method":"json_metadata","params":[[],{}]}, {"method":"i18n_messages","params":[[],{}]}, {"method":"user_find","params":[[],{"whoami":"true","all":"true"}]}, {"method":"env","params":[[],{}]}, {"method":"dns_is_enabled","params":[[],{}]} ],{}],"id":1}'  -X POST  https://`hostname`/ipa/json  | sed 's/[ \t]*$//' >   $INIT_FILE
