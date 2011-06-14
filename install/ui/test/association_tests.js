724dd997447c84c6eb3893fe40fb2c5a78d4efd7
Endi S. Dewata <edewata@redhat.com>
2011-06-14 00:02:54 -0500

    Renamed associate.js to association.js.

--- install/ui/test/association_tests.js
+++ install/ui/test/association_tests.js
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-module('associate');
+module('association');
 
 
 test("Testing serial_associator().", function() {
