f9e277b9fb85bba5b1def8b0e01aaaa4c421312b
Martin Kosek <mkosek@redhat.com>
2011-09-30 11:10:29 +0200

    Fix HBAC tests hostnames
    Make HBAC test hosts compliant with hostname restrictions placed
    in ticket 1780.

--- tests/test_xmlrpc/test_hbac_plugin.py
+++ tests/test_xmlrpc/test_hbac_plugin.py
@@ -41,9 +41,9 @@ class test_hbac(XMLRPC_test):
 
     test_user = u'hbacrule_test_user'
     test_group = u'hbacrule_test_group'
-    test_host = u'hbacrule._test_netgroup'
+    test_host = u'hbacrule.test-netgroup'
     test_hostgroup = u'hbacrule_test_hostgroup'
-    test_sourcehost = u'hbacrule._test_src_host'
+    test_sourcehost = u'hbacrule.test-src-host'
     test_sourcehostgroup = u'hbacrule_test_src_hostgroup'
     test_service = u'sshd'
     test_host_external = u'notfound.example.com'
