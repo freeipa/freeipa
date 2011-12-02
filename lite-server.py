f7753bf55cf713b93f1c12d0fc6dde7f804dd975
Rob Crittenden <rcritten@redhat.com>
2011-12-02 14:27:47 -0500

    Add labels so HBAC and Sudo rules show under hosts/hostgroups.
    Also fix a bunch of trailing whitespace.
    
    https://fedorahosted.org/freeipa/ticket/1751

--- lite-server.py
+++ lite-server.py
@@ -80,6 +80,7 @@ class WebUIApp(object):
         f = None
         try:
             f = open(requested_file, 'r')
+            api.log.info('Request file %s' % requested_file)
             start_response('200 OK', [('Content-Type', mime_type)])
             return [f.read()]
         except IOError:
@@ -88,6 +89,7 @@ class WebUIApp(object):
         finally:
             if f is not None:
                 f.close()
+            api.log.info('Request done')
 
 
 if __name__ == '__main__':
