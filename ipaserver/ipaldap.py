f5e5bf8f82ba2051ace5fc5f29d7bf25631e0a2c
Rob Crittenden <rcritten@redhat.com>
2012-03-07 17:59:19 -0500

    Fix nsslapd-anonlimitsdn dn in cn=config
    The dn value needs to be quoted otherwise it is interpreted to be a
    multi-value.
    
    This will replace whatever value is currently set.
    
    https://fedorahosted.org/freeipa/ticket/2452

--- ipaserver/ipaldap.py
+++ ipaserver/ipaldap.py
@@ -540,7 +540,7 @@ class IPAdmin(IPAEntryLDAPObject):
 
         # Some attributes, like those in cn=config, need to be replaced
         # not deleted/added.
-        FORCE_REPLACE_ON_UPDATE_ATTRS = ('nsslapd-ssl-check-hostname', 'nsslapd-lookthroughlimit', 'nsslapd-idlistscanlimit')
+        FORCE_REPLACE_ON_UPDATE_ATTRS = ('nsslapd-ssl-check-hostname', 'nsslapd-lookthroughlimit', 'nsslapd-idlistscanlimit', 'nsslapd-anonlimitsdn')
         modlist = []
 
         old_entry = ipautil.CIDict(old_entry)
