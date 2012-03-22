a735420a9ba3d507855a75a1a48f79a2358c7081
Rob Crittenden <rcritten@redhat.com>
2012-03-22 17:19:01 -0400

    Set nsslapd-minssf-exclude-rootdse to on so the DSE is always available.
    If minssf is set in configuration and this is not set then clients won't
    be able to detect the available namingContexts, defaultNamingContext,
    capabilities, etc.
    
    https://fedorahosted.org/freeipa/ticket/2542

--- ipaserver/ipaldap.py
+++ ipaserver/ipaldap.py
@@ -540,7 +540,7 @@ class IPAdmin(IPAEntryLDAPObject):
 
         # Some attributes, like those in cn=config, need to be replaced
         # not deleted/added.
-        FORCE_REPLACE_ON_UPDATE_ATTRS = ('nsslapd-ssl-check-hostname', 'nsslapd-lookthroughlimit', 'nsslapd-idlistscanlimit', 'nsslapd-anonlimitsdn')
+        FORCE_REPLACE_ON_UPDATE_ATTRS = ('nsslapd-ssl-check-hostname', 'nsslapd-lookthroughlimit', 'nsslapd-idlistscanlimit', 'nsslapd-anonlimitsdn', 'nsslapd-minssf-exclude-rootdse')
         modlist = []
 
         old_entry = ipautil.CIDict(old_entry)
