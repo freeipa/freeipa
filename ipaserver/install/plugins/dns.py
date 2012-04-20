38e7d0645ea39c3a416004b11d5368b8dea21abe
Rob Crittenden <rcritten@redhat.com>
2012-04-20 11:07:47 -0400

    Use mixed-case for Read DNS Entries permission
    https://fedorahosted.org/freeipa/ticket/2569

--- ipaserver/install/plugins/dns.py
+++ ipaserver/install/plugins/dns.py
@@ -100,13 +100,13 @@ class update_dns_permissions(PostUpdate):
                              'member:cn=DNS Servers,cn=privileges,cn=pbac,%s' \
                                      % api.env.basedn]
 
-    _read_dns_perm_dn = DN('cn=read dns entries',
+    _read_dns_perm_dn = DN('cn=Read DNS Entries',
                             api.env.container_permission,
                             api.env.basedn)
     _read_dns_perm_entry = ['objectClass:top',
                             'objectClass:groupofnames',
                             'objectClass:ipapermission',
-                            'cn:read dns entries',
+                            'cn:Read DNS Entries',
                             'description:Read DNS entries',
                             'ipapermissiontype:SYSTEM',
                             'member:cn=DNS Administrators,cn=privileges,cn=pbac,%s' \
@@ -118,7 +118,7 @@ class update_dns_permissions(PostUpdate):
     _write_dns_aci_entry = ['add:aci:\'(targetattr = "idnsforwardpolicy || idnsforwarders || idnsallowsyncptr || idnszonerefresh || idnspersistentsearch")(target = "ldap:///cn=dns,%(realm)s")(version 3.0;acl "permission:Write DNS Configuration";allow (write) groupdn = "ldap:///cn=Write DNS Configuration,cn=permissions,cn=pbac,%(realm)s";)\'' % dict(realm=api.env.basedn)]
 
     _read_dns_aci_dn = DN(api.env.container_dns, api.env.basedn)
-    _read_dns_aci_entry = ['add:aci:\'(targetattr = "*")(version 3.0; acl "No access to DNS tree without a permission"; deny (read,search,compare) (groupdn != "ldap:///cn=admins,cn=groups,cn=accounts,%(realm)s") and (groupdn != "ldap:///cn=read dns entries,cn=permissions,cn=pbac,%(realm)s");)\''  % dict(realm=api.env.basedn) ]
+    _read_dns_aci_entry = ['add:aci:\'(targetattr = "*")(version 3.0; acl "No access to DNS tree without a permission"; deny (read,search,compare) (groupdn != "ldap:///cn=admins,cn=groups,cn=accounts,%(realm)s") and (groupdn != "ldap:///cn=Read DNS Entries,cn=permissions,cn=pbac,%(realm)s");)\''  % dict(realm=api.env.basedn) ]
 
     def execute(self, **options):
         ldap = self.obj.backend
