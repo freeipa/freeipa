217341560c0e4ef38c88b843c780a1ff90e47d67
Petr Vobornik <pvoborni@redhat.com>
2012-11-26 13:51:04 +0100

    Minimal Dojo layer
    Added configuration files for building Dojo library and built library itself.
    
    This configuration contains only modules currently needed.
    
    make-dojo.sh script should be used for rebuilding the library when additional
    modules or an update are needed.
    
    https://fedorahosted.org/freeipa/ticket/112

---
+++ install/ui/src/dojo.profile.js
@@ -0,0 +1,99 @@
+//
+// DOJO profile
+//
+
+var profile = (function(){
+    return {
+        basePath: ".",
+        releaseDir: "../release",
+        releaseName: "dojo",
+        action: "release",
+
+        layerOptimize: "comments",
+        optimize: "comments",
+        cssOptimize: "comments",
+        mini: true,
+        stripConsole: "warn",
+        selectorEngine: "lite",
+
+        defaultConfig: {
+            hasCache:{
+                "config-deferredInstrumentation": 0,
+                "config-dojo-loader-catches": 0,
+                "config-tlmSiblingOfDojo": 0,
+                "dojo-amd-factory-scan": 0,
+                "dojo-combo-api": 0,
+                "dojo-config-api": 1,
+                "dojo-config-require": 0,
+                "dojo-debug-messages": 0,
+                "dojo-dom-ready-api": 1,
+                "dojo-firebug": 0,
+                "dojo-guarantee-console": 1,
+                "dojo-has-api": 1,
+                "dojo-inject-api": 1,
+                "dojo-loader": 1,
+                "dojo-log-api": 0,
+                "dojo-modulePaths": 0,
+                "dojo-moduleUrl": 0,
+                "dojo-publish-privates": 0,
+                "dojo-requirejs-api": 0,
+                "dojo-sniff": 0,
+                "dojo-sync-loader": 0,
+                "dojo-test-sniff": 0,
+                "dojo-timeout-api": 0,
+                "dojo-trace-api": 0,
+                "dojo-undef-api": 0,
+                "dojo-v1x-i18n-Api": 1,
+                "dom": 1,
+                "host-browser": 1,
+                "extend-dojo": 1
+            },
+            async: 1
+        },
+
+
+        packages:[
+            {
+                name: "dojo",
+                location: "dojo"
+            }
+        ],
+
+        layers: {
+            "dojo/dojo": {
+                // explicitly include all modules which we want in our build
+                include: [
+                    "dojo/dojo",
+                    "dojo/domReady",
+                    "dojo/_base/declare",
+                    // ^ core is about 20KB
+                    "dojo/_base/lang",
+                    "dojo/_base/array",
+                    // ^ adds 10KB
+                    "dojo/dom",
+                    "dojo/dom-construct",
+                    "dojo/dom-class",
+                    "dojo/dom-style",
+                    "dojo/dom-prop",
+                    // ^ adds 20KB, with router only 5KB
+                    "dojo/Stateful",
+                    "dojo/Evented",
+                    "dojo/on",
+                    "dojo/io-query",
+                    //
+                    "dojo/router",
+                    "dojo/hash", //used by router
+                    "dojo/topic", //used by router
+                    // ^ adds 20 KB, most of it is dojo/dom*
+                    "dojo/store/Observable",
+                    "dojo/store/Memory"
+                    //"dojo/_base/NodeList"
+
+                    // Total size: 75KB
+                ],
+                customBase: true,
+                boot: true
+            }
+        }
+    };
+})();
---
+++ install/ui/util/make-dojo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Authors:
+#    Petr Vobornik <pvoborni@redhat.com>
+#
+#  Copyright (C) 2012 Red Hat
+#  see file 'COPYING' for use and warranty information
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Build script for Dojo library
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+$DIR/prepare-dojo.sh --links --dojo
+
+$DIR/clean.sh
+$DIR/build.sh dojo
+
+# Stop at error. We don't want to overwrite good build with failed build.
+if [[ $? != 0 ]] ; then
+    echo "Build failed"
+    exit 1
+fi
+
+$DIR/compile.sh --release dojo --layer dojo/dojo --output $DIR/../build/dojo/dojo.js
+$DIR/clean.sh
