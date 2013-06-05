08a487f3e4d258f84dd20330e0a45c9b5f4094f0
Tomas Babej <tbabej@redhat.com>
2013-06-05 15:48:35 +0200

    Manage ipa-otpd.socket by IPA
    Adds a new simple service called OtpdInstance, that manages
    ipa-otpd.socket service. Added to server/replica installer
    and ipa-upgradeconfig script.
    
    https://fedorahosted.org/freeipa/ticket/3680

--- ipapython/platform/fedora16/service.py
+++ ipapython/platform/fedora16/service.py
@@ -53,6 +53,7 @@ system_units['pki_cad'] = system_units['pki-cad']
 # Our PKI instance is pki-tomcatd@pki-tomcat.service
 system_units['pki-tomcatd'] = 'pki-tomcatd@pki-tomcat.service'
 system_units['pki_tomcatd'] = system_units['pki-tomcatd']
+system_units['ipa-otpd'] = 'ipa-otpd.socket'
 
 class Fedora16Service(systemd.SystemdService):
     def __init__(self, service_name):
