76ec9384fb112ee528c5198af0261182f1ad049e
Petr Vobornik <pvoborni@redhat.com>
2014-07-01 18:16:14 +0200

    webui-build: use /usr/share/java/js.jar instead of rhino.jar
    /usr/share/java/rhino.jar is a Fedora's symlink to /usr/share/java/js.jar
    
    Debian doesn't have it. Direct usage of upstream /usr/share/java/js.jar should
    work on both systems.
    
    Reviewed-By: Timo Aaltonen <tjaalton@ubuntu.com>

--- install/ui/util/build.sh
+++ install/ui/util/build.sh
@@ -31,6 +31,6 @@ if [[ ! $profile ]] ; then
     exit 1
 fi
 
-RHINO="java -Xss${JAVA_STACK_SIZE:-512k} -classpath /usr/share/java/rhino.jar  org.mozilla.javascript.tools.shell.Main"
+RHINO="java -Xss${JAVA_STACK_SIZE:-512k} -classpath /usr/share/java/js.jar  org.mozilla.javascript.tools.shell.Main"
 $RHINO $DIR/build/build.js baseUrl=$DIR/build load=build profile=$DIR/../src/$profile.profile.js
 exit $?
