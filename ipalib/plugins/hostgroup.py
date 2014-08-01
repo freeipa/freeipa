457aca1b19353cd1eef6b5ccb1f369e4749a442d
Tomas Babej <tbabej@redhat.com>
2014-08-01 12:14:35 +0200

    hostgroup: Remove redundant and star imports
    Part of: https://fedorahosted.org/freeipa/ticket/3979
    
    Reviewed-By: Petr Viktorin <pviktori@redhat.com>
    Reviewed-By: Petr Vobornik <pvoborni@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipalib/plugins/hostgroup.py
+++ ipalib/plugins/hostgroup.py
@@ -19,8 +19,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from ipalib.plugable import Registry
-from ipalib.plugins.baseldap import *
-from ipalib import api, Int, _, ngettext, errors
+from ipalib.plugins.baseldap import (LDAPObject, LDAPCreate, LDAPRetrieve,
+                                     LDAPDelete, LDAPUpdate, LDAPSearch,
+                                     LDAPAddMember, LDAPRemoveMember,
+                                     entry_from_entry, wait_for_value)
+from ipalib import Str, api, _, ngettext, errors
 from ipalib.plugins.netgroup import NETGROUP_PATTERN, NETGROUP_PATTERN_ERRMSG
 from ipapython.dn import DN
 
