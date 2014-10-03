8abc183996477f8b9367f99297813ef2feb26069
Jan Cholasta <jcholast@redhat.com>
2014-10-03 11:23:18 +0200

    Add RHEL platform module
    https://fedorahosted.org/freeipa/ticket/4562
    
    Reviewed-By: Martin Kosek <mkosek@redhat.com>

---
+++ ipaplatform/rhel/paths.py
@@ -0,0 +1,33 @@
+# Authors:
+#   Jan Cholasta <jcholast@redhat.com>
+#
+# Copyright (C) 2014  Red Hat
+# see file 'COPYING' for use and warranty information
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+This RHEL base platform module exports default filesystem paths as common
+in RHEL-based systems.
+'''
+
+# Fallback to default path definitions
+from ipaplatform.redhat.paths import RedHatPathNamespace
+
+
+class RHELPathNamespace(RedHatPathNamespace):
+    pass
+
+
+paths = RHELPathNamespace()
