276e69de874f269f6e9089aebb650a5e0814a626
Petr Spacek <pspacek@redhat.com>
2014-10-19 17:04:40 +0200

    DNSSEC: add ipa dnssec daemons
    Tickets:
    https://fedorahosted.org/freeipa/ticket/3801
    https://fedorahosted.org/freeipa/ticket/4417
    
    Design:
    https://fedorahosted.org/bind-dyndb-ldap/wiki/BIND9/Design/DNSSEC
    
    Reviewed-By: Jan Cholasta <jcholast@redhat.com>
    Reviewed-By: David Kupka <dkupka@redhat.com>

---
+++ ipapython/dnssec/bindmgr.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014  FreeIPA Contributors see COPYING for license
+#
+
+from datetime import datetime
+import dns.name
+import errno
+import os
+import logging
+import shutil
+import stat
+import subprocess
+
+from ipalib import api
+import ipalib.constants
+from ipapython.dn import DN
+from ipapython import ipa_log_manager, ipautil
+from ipaplatform.paths import paths
+
+from temp import TemporaryDirectory
+
+time_bindfmt = '%Y%m%d%H%M%S'
+
+# this daemon should run under ods:named user:group
+# user has to be ods because ODSMgr.py sends signal to ods-enforcerd
+FILE_PERM = (stat.S_IRUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IWUSR)
+DIR_PERM = (stat.S_IRWXU | stat.S_IRWXG)
+
+class BINDMgr(object):
+    """BIND key manager. It does LDAP->BIND key files synchronization.
+
+    One LDAP object with idnsSecKey object class will produce
+    single pair of BIND key files.
+    """
+    def __init__(self, api):
+        self.api = api
+        self.log = ipa_log_manager.log_mgr.get_logger(self)
+        self.ldap_keys = {}
+        self.modified_zones = set()
+
+    def notify_zone(self, zone):
+        cmd = ['rndc', 'sign', zone.to_text()]
+        output = ipautil.run(cmd)[0]
+        self.log.info(output)
+
+    def dn2zone_name(self, dn):
+        """cn=KSK-20140813162153Z-cede9e182fc4af76c4bddbc19123a565,cn=keys,idnsname=test,cn=dns,dc=ipa,dc=example"""
+        # verify that metadata object is under DNS sub-tree
+        dn = DN(dn)
+        container = DN(self.api.env.container_dns, self.api.env.basedn)
+        idx = dn.rfind(container)
+        assert idx != -1, 'Metadata object %s is not inside %s' % (dn, container)
+        assert len(dn[idx - 1]) == 1, 'Multi-valued RDN as zone name is not supported'
+        return dns.name.from_text(dn[idx - 1]['idnsname'])
+
+    def time_ldap2bindfmt(self, str_val):
+        dt = datetime.strptime(str_val, ipalib.constants.LDAP_GENERALIZED_TIME_FORMAT)
+        return dt.strftime(time_bindfmt)
+
+    def dates2params(self, ldap_attrs):
+        attr2param = {'idnsseckeypublish': '-P',
+                'idnsseckeyactivate': '-A',
+                'idnsseckeyinactive': '-I',
+                'idnsseckeydelete': '-D'}
+
+        params = []
+        for attr, param in attr2param.items():
+            if attr in ldap_attrs:
+                params.append(param)
+                assert len(ldap_attrs[attr]) == 1, 'Timestamp %s is expected to be single-valued' % attr
+                params.append(self.time_ldap2bindfmt(ldap_attrs[attr][0]))
+
+        return params
+
+    def ldap_event(self, op, uuid, attrs):
+        """Record single LDAP event - key addition, deletion or modification.
+
+        Change is only recorded to memory.
+        self.sync() has to be called to synchronize change to BIND."""
+        assert op == 'add' or op == 'del' or op == 'mod'
+        zone = self.dn2zone_name(attrs['dn'])
+        self.modified_zones.add(zone)
+        zone_keys = self.ldap_keys.setdefault(zone, {})
+        if op == 'add':
+            self.log.info('Key metadata %s added to zone %s' % (attrs['dn'], zone))
+            zone_keys[uuid] = attrs
+
+        elif op == 'del':
+            self.log.info('Key metadata %s deleted from zone %s' % (attrs['dn'], zone))
+            zone_keys.pop(uuid)
+
+        elif op == 'mod':
+            self.log.info('Key metadata %s updated in zone %s' % (attrs['dn'], zone))
+            zone_keys[uuid] = attrs
+
+    def install_key(self, zone, uuid, attrs, workdir):
+        """Run dnssec-keyfromlabel on given LDAP object.
+        :returns: base file name of output files, e.g. Kaaa.test.+008+19719"""
+        self.log.info('attrs: %s', attrs)
+        assert attrs.get('idnsseckeyzone', ['FALSE'])[0] == 'TRUE', \
+            'object %s is not a DNS zone key' % attrs['dn']
+
+        uri = "%s;pin-source=%s" % (attrs['idnsSecKeyRef'][0], paths.DNSSEC_SOFTHSM_PIN)
+        cmd = [paths.DNSSEC_KEYFROMLABEL, '-K', workdir, '-a', attrs['idnsSecAlgorithm'][0], '-l', uri]
+        cmd += self.dates2params(attrs)
+        if attrs.get('idnsSecKeySep', ['FALSE'])[0].upper() == 'TRUE':
+            cmd += ['-f', 'KSK']
+        if attrs.get('idnsSecKeyRevoke', ['FALSE'])[0].upper() == 'TRUE':
+            cmd += ['-R', datetime.now().strftime(time_bindfmt)]
+        cmd.append(zone.to_text())
+
+        # keys has to be readable by ODS & named
+        basename = ipautil.run(cmd)[0].strip()
+        private_fn = "%s/%s.private" % (workdir, basename)
+        os.chmod(private_fn, FILE_PERM)
+        # this is useful mainly for debugging
+        with open("%s/%s.uuid" % (workdir, basename), 'w') as uuid_file:
+            uuid_file.write(uuid)
+        with open("%s/%s.dn" % (workdir, basename), 'w') as dn_file:
+            dn_file.write(attrs['dn'])
+
+    def sync_zone(self, zone):
+        self.log.info('Synchronizing zone %s' % zone)
+        zone_path = os.path.join(paths.BIND_LDAP_DNS_ZONE_WORKDIR,
+                zone.to_text(omit_final_dot=True))
+        try:
+            os.makedirs(zone_path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise e
+
+        # fix HSM permissions
+        # TODO: move out
+        for prefix, dirs, files in os.walk(paths.DNSSEC_TOKENS_DIR, topdown=True):
+            for name in dirs:
+                fpath = os.path.join(prefix, name)
+                self.log.debug('Fixing directory permissions: %s', fpath)
+                os.chmod(fpath, DIR_PERM | stat.S_ISGID)
+            for name in files:
+                fpath = os.path.join(prefix, name)
+                self.log.debug('Fixing file permissions: %s', fpath)
+                os.chmod(fpath, FILE_PERM)
+        # TODO: move out
+
+        with TemporaryDirectory(zone_path) as tempdir:
+            for uuid, attrs in self.ldap_keys[zone].items():
+                self.install_key(zone, uuid, attrs, tempdir)
+            # keys were generated in a temporary directory, swap directories
+            target_dir = "%s/keys" % zone_path
+            try:
+                shutil.rmtree(target_dir)
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    raise e
+            shutil.move(tempdir, target_dir)
+            os.chmod(target_dir, DIR_PERM)
+
+        self.notify_zone(zone)
+
+    def sync(self):
+        """Synchronize list of zones in LDAP with BIND."""
+        self.log.debug('Key metadata in LDAP: %s' % self.ldap_keys)
+        for zone in self.modified_zones:
+            self.sync_zone(zone)
+
+        self.modified_zones = set()
+
+    def diff_zl(self, s1, s2):
+        """Compute zones present in s1 but not present in s2.
+
+        Returns: List of (uuid, name) tuples with zones present only in s1."""
+        s1_extra = s1.uuids - s2.uuids
+        removed = [(uuid, name) for (uuid, name) in s1.mapping.items()
+                   if uuid in s1_extra]
+        return removed
---
+++ ipapython/dnssec/localhsm.py
@@ -0,0 +1,229 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014  FreeIPA Contributors see COPYING for license
+#
+
+from binascii import hexlify
+import collections
+import logging
+import os
+from pprint import pprint
+import sys
+import time
+
+from ipaplatform.paths import paths
+
+import _ipap11helper
+from abshsm import attrs_name2id, attrs_id2name, AbstractHSM, keytype_id2name, keytype_name2id, ldap2p11helper_api_params
+
+private_key_api_params = set(["label", "id", "data", "unwrapping_key",
+    "wrapping_mech", "key_type", "cka_always_authenticate", "cka_copyable",
+    "cka_decrypt", "cka_derive", "cka_extractable", "cka_modifiable",
+    "cka_private", "cka_sensitive", "cka_sign", "cka_sign_recover",
+    "cka_unwrap", "cka_wrap_with_trusted"])
+
+public_key_api_params = set(["label", "id", "data", "cka_copyable",
+    "cka_derive", "cka_encrypt", "cka_modifiable", "cka_private",
+    "cka_trusted", "cka_verify", "cka_verify_recover", "cka_wrap"])
+
+class Key(collections.MutableMapping):
+    def __init__(self, p11, handle):
+        self.p11 = p11
+        self.handle = handle
+        # sanity check CKA_ID and CKA_LABEL
+        try:
+            cka_id = self.p11.get_attribute(handle, _ipap11helper.CKA_ID)
+            assert len(cka_id) != 0, 'ipk11id length should not be 0'
+        except _ipap11helper.NotFound:
+            raise _ipap11helper.NotFound('key without ipk11id: handle %s' % handle)
+
+        try:
+            cka_label = self.p11.get_attribute(handle, _ipap11helper.CKA_LABEL)
+            assert len(cka_label) != 0, 'ipk11label length should not be 0'
+
+        except _ipap11helper.NotFound:
+            raise _ipap11helper.NotFound('key without ipk11label: id 0x%s'
+                    % hexlify(cka_id))
+
+    def __getitem__(self, key):
+        key = key.lower()
+        try:
+            value = self.p11.get_attribute(self.handle, attrs_name2id[key])
+            if key == 'ipk11keytype':
+                value = keytype_id2name[value]
+            return value
+        except _ipap11helper.NotFound:
+            raise KeyError()
+
+    def __setitem__(self, key, value):
+        key = key.lower()
+        if key == 'ipk11keytype':
+            value = keytype_name2id[value]
+
+        return self.p11.set_attribute(self.handle, attrs_name2id[key], value)
+
+    def __delitem__(self, key):
+        raise _ipap11helper.Exception('__delitem__ is not supported')
+
+    def __iter__(self):
+        """generates list of ipa names of all attributes present in the object"""
+        for pkcs11_id, ipa_name in attrs_id2name.iteritems():
+            try:
+                self.p11.get_attribute(self.handle, pkcs11_id)
+            except _ipap11helper.NotFound:
+                continue
+
+            yield ipa_name
+
+    def __len__(self):
+        cnt = 0
+        for attr in self:
+            cnt += 1
+        return cnt
+
+    def __str__(self):
+        d = {}
+        for ipa_name, value in self.iteritems():
+            d[ipa_name] = value
+
+        return str(d)
+
+    def __repr__(self):
+        return self.__str__()
+
+class LocalHSM(AbstractHSM):
+    def __init__(self, library, slot, pin):
+        self.cache_replica_pubkeys = None
+        self.p11 = _ipap11helper.P11_Helper(slot, pin, library)
+        self.log = logging.getLogger()
+
+    def __del__(self):
+        self.p11.finalize()
+
+    def find_keys(self, **kwargs):
+        """Return dict with Key objects matching given criteria.
+
+        CKA_ID is used as key so all matching objects have to have unique ID."""
+
+        # this is a hack for old p11-kit URI parser
+        # see https://bugs.freedesktop.org/show_bug.cgi?id=85057
+        if 'uri' in kwargs:
+            kwargs['uri'] = kwargs['uri'].replace('type=', 'object-type=')
+
+        handles = self.p11.find_keys(**kwargs)
+        keys = {}
+        for h in handles:
+            key = Key(self.p11, h)
+            o_id = key['ipk11id']
+            assert o_id not in keys, 'duplicate ipk11Id = 0x%s; keys = %s' % (
+                    hexlify(o_id), keys)
+            keys[o_id] = key
+
+        return keys
+
+    @property
+    def replica_pubkeys(self):
+        return self._filter_replica_keys(
+                self.find_keys(objclass=_ipap11helper.KEY_CLASS_PUBLIC_KEY))
+
+    @property
+    def replica_pubkeys_wrap(self):
+        return self._filter_replica_keys(
+                self.find_keys(objclass=_ipap11helper.KEY_CLASS_PUBLIC_KEY,
+                cka_wrap=True))
+
+    @property
+    def master_keys(self):
+        """Get all usable DNSSEC master keys"""
+        keys = self.find_keys(objclass=_ipap11helper.KEY_CLASS_SECRET_KEY, label=u'dnssec-master', cka_unwrap=True)
+
+        for key in keys.itervalues():
+            prefix = 'dnssec-master'
+            assert key['ipk11label'] == prefix, \
+                'secret key ipk11id=0x%s ipk11label="%s" with ipk11UnWrap = TRUE does not have '\
+                '"%s" key label' % (hexlify(key['ipk11id']),
+                        str(key['ipk11label']), prefix)
+
+        return keys
+
+    @property
+    def active_master_key(self):
+        """Get one active DNSSEC master key suitable for key wrapping"""
+        keys = self.find_keys(objclass=_ipap11helper.KEY_CLASS_SECRET_KEY,
+                label=u'dnssec-master', cka_wrap=True, cka_unwrap=True)
+        assert len(keys) > 0, "DNSSEC master key with UN/WRAP = TRUE not found"
+        return keys.popitem()[1]
+
+    @property
+    def zone_pubkeys(self):
+        return self._filter_zone_keys(
+                self.find_keys(objclass=_ipap11helper.KEY_CLASS_PUBLIC_KEY))
+
+    @property
+    def zone_privkeys(self):
+        return self._filter_zone_keys(
+                self.find_keys(objclass=_ipap11helper.KEY_CLASS_PRIVATE_KEY))
+
+
+    def import_public_key(self, source, data):
+        params = ldap2p11helper_api_params(source)
+        # filter out params inappropriate for public keys
+        for par in set(params.keys()).difference(public_key_api_params):
+            del params[par]
+        params['data'] = data
+
+        h = self.p11.import_public_key(**params)
+        return Key(self.p11, h)
+
+    def import_private_key(self, source, data, unwrapping_key):
+        params = ldap2p11helper_api_params(source)
+        # filter out params inappropriate for private keys
+        for par in set(params.keys()).difference(private_key_api_params):
+            del params[par]
+        params['data'] = data
+        params['unwrapping_key'] = unwrapping_key.handle
+
+        h = self.p11.import_wrapped_private_key(**params)
+        return Key(self.p11, h)
+
+
+
+if __name__ == '__main__':
+    if 'SOFTHSM2_CONF' not in os.environ:
+        os.environ['SOFTHSM2_CONF'] = paths.DNSSEC_SOFTHSM2_CONF
+    localhsm = LocalHSM(paths.LIBSOFTHSM2_SO, 0,
+            open(paths.DNSSEC_SOFTHSM_PIN).read())
+
+    print 'replica public keys: CKA_WRAP = TRUE'
+    print '===================================='
+    for pubkey_id, pubkey in localhsm.replica_pubkeys_wrap.iteritems():
+        print hexlify(pubkey_id)
+        pprint(pubkey)
+
+    print ''
+    print 'replica public keys: all'
+    print '========================'
+    for pubkey_id, pubkey in localhsm.replica_pubkeys.iteritems():
+        print hexlify(pubkey_id)
+        pprint(pubkey)
+
+    print ''
+    print 'master keys'
+    print '==========='
+    for mkey_id, mkey in localhsm.master_keys.iteritems():
+        print hexlify(mkey_id)
+        pprint(mkey)
+
+    print ''
+    print 'zone public keys'
+    print '================'
+    for key_id, key in localhsm.zone_pubkeys.iteritems():
+        print hexlify(key_id)
+        pprint(key)
+
+    print ''
+    print 'zone private keys'
+    print '================='
+    for key_id, key in localhsm.zone_privkeys.iteritems():
+        print hexlify(key_id)
+        pprint(key)
