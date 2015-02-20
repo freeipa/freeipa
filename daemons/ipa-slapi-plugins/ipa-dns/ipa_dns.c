4ddcca6435ad685582293b1bac588ea0615e94e4
Martin Kosek <mkosek@redhat.com>
2015-02-20 15:12:25 +0100

    Remove references to GPL v2.0 license
    All FreeIPA original code should be licensed to GPL v3+ license,
    update the respective files:
    
    - daemons/ipa-slapi-plugins/ipa-dns/ipa_dns.c
    
    Remove GPL v2.0 license files from LDIFs or template to keep
    consistency.
    
    Reviewed-By: Simo Sorce <ssorce@redhat.com>

--- daemons/ipa-slapi-plugins/ipa-dns/ipa_dns.c
+++ daemons/ipa-slapi-plugins/ipa-dns/ipa_dns.c
@@ -1,26 +1,44 @@
 /** BEGIN COPYRIGHT BLOCK
- * This Program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; version 2 of the License.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This Program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this Program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2001 Sun Microsystems, Inc. Used by permission.
- * Copyright (C) 2013 Red Hat, Inc.
+ * Additional permission under GPLv3 section 7:
+ *
+ * In the following paragraph, "GPL" means the GNU General Public
+ * License, version 3 or any later version, and "Non-GPL Code" means
+ * code that is governed neither by the GPL nor a license
+ * compatible with the GPL.
+ *
+ * You may link the code of this Program with Non-GPL Code and convey
+ * linked combinations including the two, provided that such Non-GPL
+ * Code only links to the code of this Program through those well
+ * defined interfaces identified in the file named EXCEPTION found in
+ * the source code files (the "Approved Interfaces"). The files of
+ * Non-GPL Code may instantiate templates or use macros or inline
+ * functions from the Approved Interfaces without causing the resulting
+ * work to be covered by the GPL. Only the copyright holders of this
+ * Program may make changes or additions to the list of Approved
+ * Interfaces.
  *
  * Authors:
  * original authors of 389 example ldap/servers/slapd/test-plugins/testpreop.c
  * Petr Spacek <pspacek@redhat.com>
  *
+ * Copyright (C) 2001 Sun Microsystems, Inc. Used by permission.
+ * Copyright (C) 2013 Red Hat, Inc.
  * All rights reserved.
- *
  * END COPYRIGHT BLOCK **/
+
 
 /*
  * This is 389 DS plug-in with supporting functions for IPA-integrated DNS.
