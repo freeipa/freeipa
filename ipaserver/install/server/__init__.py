6dabe6ebb28c9972cedc3fb7cc1e59ddd8e5e5b6
Jan Cholasta <jcholast@redhat.com>
2015-05-28 06:05:28 +0200

    install: Move ipa-server-install code into a module
    https://fedorahosted.org/freeipa/ticket/4468
    
    Reviewed-By: Martin Basti <mbasti@redhat.com>

--- ipaserver/install/server/__init__.py
+++ ipaserver/install/server/__init__.py
@@ -2,4 +2,7 @@
 # Copyright (C) 2015  FreeIPA Contributors see COPYING for license
 #
 
+from .install import install_check, install, uninstall_check, uninstall
+
+from .install import validate_dm_password, validate_admin_password
 from .upgrade import upgrade_configuration
