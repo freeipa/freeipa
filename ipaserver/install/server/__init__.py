3cb42e33c06fb66702ecfd34ddcad9b548f23132
Jan Cholasta <jcholast@redhat.com>
2015-05-28 06:06:05 +0200

    install: Move ipa-replica-install code into a module
    https://fedorahosted.org/freeipa/ticket/4468
    
    Reviewed-By: Martin Basti <mbasti@redhat.com>

--- ipaserver/install/server/__init__.py
+++ ipaserver/install/server/__init__.py
@@ -3,6 +3,8 @@
 #
 
 from .install import install_check, install, uninstall_check, uninstall
+from .replicainstall import install_check as replica_install_check
+from .replicainstall import install as replica_install
 
 from .install import validate_dm_password, validate_admin_password
 from .upgrade import upgrade_configuration
