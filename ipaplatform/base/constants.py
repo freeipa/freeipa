a487e42d3fb71ceb3128fb9a2055a5586c4ed851
Tomas Babej <tbabej@redhat.com>
2015-07-02 12:38:43 +0200

    ipaplatform: Add constants submodule
    Introduce a ipaplatform/constants.py file to store platform related
    constants, which are not paths.
    
    Reviewed-By: Martin Basti <mbasti@redhat.com>
    Reviewed-By: Petr Spacek <pspacek@redhat.com>

---
+++ ipaplatform/base/constants.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2015  FreeIPA Contributors see COPYING for license
+#
+
+'''
+This base platform module exports platform dependant constants.
+'''
+
+
+class BaseConstantsNamespace(object):
+    pass
---
+++ ipaplatform/redhat/constants.py
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2015  FreeIPA Contributors see COPYING for license
+#
+
+'''
+This Red Hat OS family base platform module exports default platform
+related constants for the Red Hat OS family-based systems.
+'''
+
+# Fallback to default path definitions
+from ipaplatform.base.constants import BaseConstantsNamespace
+
+
+class RedHatConstantsNamespace(BaseConstantsNamespace):
+    pass
+
+constants = RedHatConstantsNamespace()
