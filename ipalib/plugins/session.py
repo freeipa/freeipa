freeipa/freeipa

commit cd3ca94ff2ef738cb3a9eae502193413058f976d

move session_logout command to ipalib/plugins directory
API refactoring caused that session_logout command was not registered.

Commands in ipalib/plugins directory are automatically registered.

Reviewed-By: Martin Basti <mbasti@redhat.com>

ipalib/plugins/session.py
@@ -1,0 +1,31 @@
+#
+# Copyright (C) 2015  FreeIPA Contributors see COPYING for license
+#
+
+from ipalib import Command
+from ipalib.request import context
+from ipalib.session import session_mgr
+from ipalib.plugable import Registry
+
+register = Registry()
+
+
+@register()
+class session_logout(Command):
+    '''
+    RPC command used to log the current user out of their session.
+    '''
+    NO_CLI = True
+
+    def execute(self, *args, **options):
+        session_data = getattr(context, 'session_data', None)
+        if session_data is None:
+            self.debug('session logout command: no session_data found')
+        else:
+            session_id = session_data.get('session_id')
+            self.debug('session logout command: session_id=%s', session_id)
+
+            # Notifiy registered listeners
+            session_mgr.auth_mgr.logout(session_data)
+
+        return dict(result=None)

ipalib/session.py
@@ -26,7 +26,6 @@ from urllib2 import urlparse
 from text import _
 from ipapython.ipa_log_manager import *
 from ipalib import api, errors
-from ipalib import Command
 from ipaplatform.paths import paths
 from ipalib.krb_utils import *
 from ipapython.cookie import Cookie
@@ -1277,33 +1276,5 @@ def release_ipa_ccache(ccache_name):
                 root_logger.error('unable to delete session ccache file "%s", %s', name, e)
     else:
         raise ValueError('ccache scheme "%s" unsupported (%s)', scheme, ccache_name)
-
-
-#-------------------------------------------------------------------------------
-
-from ipalib.request import context
-
-class session_logout(Command):
-    '''
-    RPC command used to log the current user out of their session.
-    '''
-
-    def execute(self, *args, **options):
-        session_data = getattr(context, 'session_data', None)
-        if session_data is None:
-            self.debug('session logout command: no session_data found')
-        else:
-            session_id = session_data.get('session_id')
-            self.debug('session logout command: session_id=%s', session_id)
-
-            # Notifiy registered listeners
-            session_mgr.auth_mgr.logout(session_data)
-
-        return dict(result=None)
-
-api.register(session_logout)
-
-#-------------------------------------------------------------------------------
-
 
 session_mgr = MemcacheSessionManager()

VERSION
@@ -90,5 +90,5 @@ IPA_DATA_VERSION=20100614120000
 #                                                      #
 ########################################################
 IPA_API_VERSION_MAJOR=2
-IPA_API_VERSION_MINOR=145
-# Last change: edewata - added vault access control
+IPA_API_VERSION_MINOR=146
+# Last change: pvoborni - move session_logout to ipalib/plugins

commit 6275d94df30c0c17d2317ad0fba3b28d433efa06

do not import memcache on client
Fixes regression caused by cd3ca94ff2ef738cb3a9eae502193413058f976d.

Which caused:
* client installation failure (missing memcache)
* invalid warning in CLI on server

https://fedorahosted.org/freeipa/ticket/5133

Reviewed-By: Tomas Babej <tbabej@redhat.com>

ipalib/plugins/session.py
@@ -2,10 +2,12 @@
 # Copyright (C) 2015  FreeIPA Contributors see COPYING for license
 #
 
-from ipalib import Command
+from ipalib import api, Command
 from ipalib.request import context
-from ipalib.session import session_mgr
 from ipalib.plugable import Registry
+
+if api.env.in_server:
+    from ipalib.session import session_mgr
 
 register = Registry()
 