19dd2ed758210e859a5b0085de558cf13ba09104
Petr Vobornik <pvoborni@redhat.com>
2015-08-25 18:25:50 +0200

    vault: change default vault type to symmetric
    https://fedorahosted.org/freeipa/ticket/5251
    
    Reviewed-By: Martin Basti <mbasti@redhat.com>

--- ipalib/plugins/vault.py
+++ ipalib/plugins/vault.py
@@ -100,6 +100,7 @@ EXAMPLES:
  Add a standard vault:
    ipa vault-add <name>
        [--user <user>|--service <service>|--shared]
+       --type standard
 """) + _("""
  Add a symmetric vault:
    ipa vault-add <name>
@@ -310,7 +311,7 @@ class vault(LDAPObject):
             label=_('Type'),
             doc=_('Vault type'),
             values=(u'standard', u'symmetric', u'asymmetric', ),
-            default=u'standard',
+            default=u'symmetric',
             autofill=True,
         ),
         Bytes(
@@ -577,10 +578,14 @@ class vault_add(PKQuery, Local):
             cli_name='desc',
             doc=_('Vault description'),
         ),
-        Str(
+        StrEnum(
             'ipavaulttype?',
             cli_name='type',
+            label=_('Type'),
             doc=_('Vault type'),
+            values=(u'standard', u'symmetric', u'asymmetric', ),
+            default=u'symmetric',
+            autofill=True,
         ),
         Str(
             'password?',
@@ -608,7 +613,7 @@ class vault_add(PKQuery, Local):
 
     def forward(self, *args, **options):
 
-        vault_type = options.get('ipavaulttype', u'standard')
+        vault_type = options.get('ipavaulttype')
         password = options.get('password')
         password_file = options.get('password_file')
         public_key = options.get('ipavaultpublickey')
