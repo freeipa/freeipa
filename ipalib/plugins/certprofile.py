5c7d6a6a31daca8bf92d85d8c1895279be84c888
Fraser Tweedale <ftweedal@redhat.com>
2015-08-24 20:25:10 -0400

    certprofile: prevent rename (modrdn)
    Fixes: https://fedorahosted.org/freeipa/ticket/5247
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipalib/plugins/certprofile.py
+++ ipalib/plugins/certprofile.py
@@ -323,8 +323,9 @@ class certprofile_mod(LDAPUpdate):
     def pre_callback(self, ldap, dn, entry_attrs, attrs_list, *keys, **options):
         ca_enabled_check()
         # Once a profile id is set it cannot be changed
-        if 'cn' in entry_attrs:
-            raise errors.ACIError(info=_('cn is immutable'))
+        if 'rename' in options or 'cn' in entry_attrs:
+            raise errors.ProtectedEntryError(label='certprofile', key=keys[0],
+                reason=_('Certificate profiles cannot be renamed'))
         if 'file' in options:
             with self.api.Backend.ra_certprofile as profile_api:
                 profile_api.disable_profile(keys[0])
