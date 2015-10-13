5233165ce7062bb7aa649bf95a029103c375207b
Martin Basti <mbasti@redhat.com>
2015-10-13 13:38:29 +0200

    CI: installation with customized DS config
    Test covers:
    
    https://fedorahosted.org/freeipa/ticket/4949
    https://fedorahosted.org/freeipa/ticket/4048
    https://fedorahosted.org/freeipa/ticket/1930
    
    Reviewed-By: Martin Babinsky <mbabinsk@redhat.com>

--- ipatests/test_integration/tasks.py
+++ ipatests/test_integration/tasks.py
@@ -288,11 +288,7 @@ def get_replica_filename(replica):
     return os.path.join(replica.config.test_dir, 'replica-info.gpg')
 
 
-def install_replica(master, replica, setup_ca=True, setup_dns=False,
-                    setup_kra=False):
-    replica.collect_log(paths.IPAREPLICA_INSTALL_LOG)
-    replica.collect_log(paths.IPAREPLICA_CONNCHECK_LOG)
-
+def replica_prepare(master, replica):
     apply_common_fixes(replica)
     fix_apache_semaphores(replica)
     prepare_reverse_zone(master, replica.ip)
@@ -304,6 +300,16 @@ def install_replica(master, replica, setup_ca=True, setup_dns=False,
         paths.REPLICA_INFO_GPG_TEMPLATE % replica.hostname)
     replica_filename = get_replica_filename(replica)
     replica.put_file_contents(replica_filename, replica_bundle)
+
+
+def install_replica(master, replica, setup_ca=True, setup_dns=False,
+                    setup_kra=False):
+    replica.collect_log(paths.IPAREPLICA_INSTALL_LOG)
+    replica.collect_log(paths.IPAREPLICA_CONNCHECK_LOG)
+
+    replica_prepare(master, replica)
+
+    replica_filename = get_replica_filename(replica)
     args = ['ipa-replica-install', '-U',
             '-p', replica.config.dirman_password,
             '-w', replica.config.admin_password,
