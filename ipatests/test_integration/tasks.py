freeipa/freeipa

commit c4b9b295d8184694c50c0d56051e0273445c98ec

CI tests: remove '-p' option from ipa-dns-install calls
fix for https://fedorahosted.org/freeipa/ticket/4933 made ipa-dns-install to
use LDAPI and deprecated -p option for directory manager password. This patche
remove the option from calls to ipa-dns-install in CI tests so that
deprecation warning does not clutter the logs.

Reviewed-By: Milan Kubik <mkubik@redhat.com>

ipatests/test_integration/tasks.py
@@ -935,7 +935,7 @@ def resolve_record(nameserver, query, rtype="SOA", retry=True, timeout=100):
 def install_kra(host, domain_level=None, first_instance=False, raiseonerr=True):
     if not domain_level:
        domain_level = domainlevel(host)
-    command = ["ipa-kra-install", "-U", "-p", host.config.dirman_password]
+    command = ["ipa-kra-install", "-U"]
     if domain_level == DOMAIN_LEVEL_0 and not first_instance:
         replica_file = get_replica_filename(host)
         command.append(replica_file)
@@ -957,7 +957,6 @@ def install_dns(host, raiseonerr=True):
     args = [
         "ipa-dns-install",
         "--forwarder", host.config.dns_forwarder,
-        "-p", host.config.dirman_password,
         "-U",
     ]
     return host.run_command(args, raiseonerr=raiseonerr)

ipatests/test_integration/test_backup_and_restore.py
@@ -285,7 +285,6 @@ class BaseBackupAndRestoreWithDNSSEC(IntegrationTest):
             "ipa-dns-install",
             "--dnssec-master",
             "--forwarder", cls.master.config.dns_forwarder,
-            "-p", cls.master.config.dirman_password,
             "-U",
         ]
         cls.master.run_command(args)

ipatests/test_integration/test_dnssec.py
@@ -91,7 +91,6 @@ class TestInstallDNSSECLast(IntegrationTest):
             "ipa-dns-install",
             "--dnssec-master",
             "--forwarder", self.master.config.dns_forwarder,
-            "-p", self.master.config.dirman_password,
             "-U",
         ]
         self.master.run_command(args)
@@ -249,7 +248,6 @@ class TestInstallDNSSECFirst(IntegrationTest):
             "ipa-dns-install",
             "--dnssec-master",
             "--forwarder", cls.master.config.dns_forwarder,
-            "-p", cls.master.config.dirman_password,
             "-U",
         ]
         cls.master.run_command(args)
@@ -423,7 +421,6 @@ class TestMigrateDNSSECMaster(IntegrationTest):
             "ipa-dns-install",
             "--dnssec-master",
             "--forwarder", cls.master.config.dns_forwarder,
-            "-p", cls.master.config.dirman_password,
             "-U",
         ]
         cls.master.run_command(args)
@@ -458,7 +455,6 @@ class TestMigrateDNSSECMaster(IntegrationTest):
             "ipa-dns-install",
             "--disable-dnssec-master",
             "--forwarder", self.master.config.dns_forwarder,
-            "-p", self.master.config.dirman_password,
             "--force",
             "-U",
         ]
@@ -474,7 +470,6 @@ class TestMigrateDNSSECMaster(IntegrationTest):
             "--dnssec-master",
             "--kasp-db", replica_backup_filename,
             "--forwarder", self.master.config.dns_forwarder,
-            "-p", self.master.config.dirman_password,
             "-U",
         ]
         self.replicas[0].run_command(args)

commit 26899c91afd36b39908a3a73363aff1787c9dc07

CI test: fix regression in task.install_kra
ipa-kra-install needs directory manager password

Regression caused by c4b9b295d8184694c50c0d56051e0273445c98ec

Reviewed-By: Martin Basti <mbasti@redhat.com>

ipatests/test_integration/tasks.py
@@ -957,7 +957,7 @@ def ipa_restore(master, backup_path):
 def install_kra(host, domain_level=None, first_instance=False, raiseonerr=True):
     if domain_level is None:
         domain_level = domainlevel(host)
-    command = ["ipa-kra-install", "-U"]
+    command = ["ipa-kra-install", "-U", "-p", host.config.dirman_password]
     if domain_level == DOMAIN_LEVEL_0 and not first_instance:
         replica_file = get_replica_filename(host)
         command.append(replica_file)