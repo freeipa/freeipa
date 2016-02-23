63d8caf0d105f02decc0b5d865fedf6ad063bc1a
Martin Basti <mbasti@redhat.com>
2016-02-23 10:37:47 +0100

    fix upgrade: wait for proper DS socket after DS restart
    DS restart executed by upgrade plugin causes that upgrade framework
    is waiting for the improper socket. It leads to TimeoutError because
    DS is not listening on 389 port during upgrade. This commit fixes the issue.
    
    Required for: https://fedorahosted.org/freeipa/ticket/5665
    
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Tomas Babej <tbabej@redhat.com>

--- ipaserver/install/ldapupdate.py
+++ ipaserver/install/ldapupdate.py
@@ -44,7 +44,6 @@ from ipaplatform.paths import paths
 from ipaplatform import services
 from ipapython.dn import DN
 from ipapython.ipa_log_manager import *
-from ipapython.ipautil import wait_for_open_socket
 
 UPDATES_DIR=paths.UPDATES_DIR
 
@@ -932,5 +931,4 @@ class LDAPUpdate:
     def restart_ds(self):
         dirsrv = services.knownservices.dirsrv
         self.log.debug('Restarting directory server to apply updates')
-        dirsrv.restart()
-        wait_for_open_socket(self.socket_name)
+        dirsrv.restart(ldapi=self.ldapi)
