657838462c4b0ce5be2cee584b3be112aca6c660
Tomas Babej <tbabej@redhat.com>
2016-02-25 14:02:08 +0100

    ipa-adtrust-install: Allow dash in the NETBIOS name
    Dash should be one of the allowed characters in the netbios names,
    so relax the too strict validation.
    
    Note: the set of allowed characters might expand in the future
    
    https://fedorahosted.org/freeipa/ticket/5286
    
    Reviewed-By: Martin Babinsky <mbabinsk@redhat.com>

--- ipaserver/install/adtrustinstance.py
+++ ipaserver/install/adtrustinstance.py
@@ -44,7 +44,7 @@ from ipaplatform.paths import paths
 from ipaplatform.tasks import tasks
 
 
-ALLOWED_NETBIOS_CHARS = string.ascii_uppercase + string.digits
+ALLOWED_NETBIOS_CHARS = string.ascii_uppercase + string.digits + '-'
 
 UPGRADE_ERROR = """
 Entry %(dn)s does not exist.
@@ -83,13 +83,19 @@ def ipa_smb_conf_exists():
     return False
 
 
-def check_netbios_name(s):
-    # NetBIOS names may not be longer than 15 allowed characters
-    if not s or len(s) > 15 or \
-       ''.join([c for c in s if c not in ALLOWED_NETBIOS_CHARS]):
+def check_netbios_name(name):
+    # Empty NetBIOS name is not allowed
+    if name is None:
         return False
 
-    return True
+    # NetBIOS names may not be longer than 15 allowed characters
+    invalid_netbios_name = any([
+        len(name) > 15,
+        ''.join([c for c in name if c not in ALLOWED_NETBIOS_CHARS])
+    ])
+
+    return not invalid_netbios_name
+
 
 def make_netbios_name(s):
     return ''.join([c for c in s.split('.')[0].upper() \
