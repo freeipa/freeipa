freeipa/freeipa

commit 491447cc5ab8c5eff2be57d609201cefb79f7053

pylint: remove bare except
Bare except should not be used.

Reviewed-By: Petr Spacek <pspacek@redhat.com>
Reviewed-By: Lukas Slebodnik <lslebodn@redhat.com>

ipalib/plugins/stageuser.py
@@ -565,7 +565,7 @@ class stageuser_activate(LDAPQuery):
                     try:
                         v.decode('utf-8')
                         self.log.debug("merge: %s:%r wiped" % (attr, v))
-                    except:
+                    except ValueError:
                         self.log.debug("merge %s: [no_print %s]" % (attr, v.__class__.__name__))
                     if isinstance(entry_to[attr], (list, tuple)):
                         # multi value attribute
@@ -581,7 +581,7 @@ class stageuser_activate(LDAPQuery):
                     try:
                         v.decode('utf-8')
                         self.log.debug("Add: %s:%r" % (attr, v))
-                    except:
+                    except ValueError:
                         self.log.debug("Add %s: [no_print %s]" % (attr, v.__class__.__name__))
 
                     if isinstance(entry_to[attr], (list, tuple)):
@@ -692,7 +692,7 @@ class stageuser_activate(LDAPQuery):
             try:
                 self.log.error("Fail to delete the Staging user after activating it %s " % (staging_dn))
                 self._exc_wrapper(args, options, ldap.delete_entry)(active_dn)
-            except:
+            except Exception:
                 self.log.error("Fail to cleanup activation. The user remains active %s" % (active_dn))
             raise
 

ipapython/config.py
@@ -160,19 +160,19 @@ def __parse_config(discover_server = True):
     try:
         if not config.default_realm:
             config.default_realm = p.get("global", "realm")
-    except:
+    except AttributeError:
         pass
     if discover_server:
         try:
             s = p.get("global", "xmlrpc_uri")
             server = urlsplit(s)
             config.default_server.append(server.netloc)
-        except:
+        except Exception:
             pass
     try:
         if not config.default_domain:
             config.default_domain = p.get("global", "domain")
-    except:
+    except AttributeError:
         pass
 
 def __discover_config(discover_server = True):
@@ -218,7 +218,7 @@ def __discover_config(discover_server = True):
                 hostname = str(server.target).rstrip(".")
                 config.default_server.append(hostname)
 
-    except:
+    except Exception:
         pass
 
 def add_standard_options(parser):

client/ipa-client-install
@@ -2470,11 +2470,11 @@ def install(options, env, fstore, statestore):
         try:
             socket.inet_pton(socket.AF_INET, srv)
             is_ipaddr = True
-        except:
+        except socket.error:
             try:
                 socket.inet_pton(socket.AF_INET6, srv)
                 is_ipaddr = True
-            except:
+            except socket.error:
                 is_ipaddr = False
 
         if is_ipaddr:

install/tools/ipa-replica-manage
@@ -597,7 +597,7 @@ def clean_dangling_ruvs(realm, host, options):
             conn = ipaldap.IPAdmin(master_cn, 636, cacert=CACERT)
             conn.do_simple_bind(bindpw=options.dirman_passwd)
             master_info['online'] = True
-        except:
+        except Exception:
             print("The server '{host}' appears to be offline."
                   .format(host=master_cn))
             offlines.add(master_cn)

install/tools/ipactl
@@ -343,13 +343,13 @@ def ipa_stop(options):
         try:
             print("Stopping %s Service" % svc)
             svchandle.stop(capture_output=False)
-        except:
+        except Exception:
             emit_err("Failed to stop %s Service" % svc)
 
     try:
         print("Stopping Directory Service")
         dirsrv.stop(capture_output=False)
-    except:
+    except Exception:
         raise IpactlError("Failed to stop Directory Service")
 
     # remove file with list of started services
@@ -383,7 +383,7 @@ def ipa_restart(options):
         emit_err("Shutting down")
         try:
             dirsrv.stop(capture_output=False)
-        except:
+        except Exception:
             pass
         if isinstance(e, IpactlError):
             # do not display any other error message
@@ -421,7 +421,7 @@ def ipa_restart(options):
             try:
                 print("Stopping %s Service" % svc)
                 svchandle.stop(capture_output=False)
-            except:
+            except Exception:
                 emit_err("Failed to stop %s Service" % svc)
 
     try:
@@ -521,7 +521,7 @@ def ipa_status(options):
                 print("%s Service: RUNNING" % svc)
             else:
                 print("%s Service: STOPPED" % svc)
-        except:
+        except Exception:
             emit_err("Failed to get %s Service status" % svc)
 
 def main():

ipaclient/ipadiscovery.py
@@ -129,7 +129,7 @@ class IPADiscovery(object):
                 elif line.lower().startswith('search'):
                     domains += [(d, 'search domain from /etc/resolv.conf') for
                         d in line.split()[1:]]
-        except:
+        except Exception:
             pass
         if domain:
             domains = [domain] + domains

ipalib/cli.py
@@ -663,7 +663,7 @@ class textui(backend.Backend):
                 selection = int(resp) - 1
                 if (selection >= 0 and selection < counter):
                     break
-            except:
+            except Exception:
                 # fall through to the error msg
                 pass
 
@@ -907,7 +907,7 @@ class console(frontend.Command):
             try:
                 with script:
                     exec(script, globals(), local)
-            except:
+            except Exception:
                 traceback.print_exc()
                 exit(1)
         else:

ipalib/constants.py
@@ -28,10 +28,10 @@ from ipapython.version import VERSION, API_VERSION
 
 try:
     FQDN = socket.getfqdn()
-except:
+except Exception:
     try:
         FQDN = socket.gethostname()
-    except:
+    except Exception:
         FQDN = None
 
 # Path to CA certificate bundle

commit d1e29fe60e75a6f40a780eb45e86f2ed87d00e5a

Fix: catch Exception instead of more specific exception types
Regression caused by commit 491447cc5ab8c5eff2be57d609201cefb79f7053,
ValueErrori and AttributeError are too much specific for these cases, multiple types of
exception can be raised.

Reviewed-By: Martin Babinsky <mbabinsk@redhat.com>

ipalib/plugins/stageuser.py
@@ -565,7 +565,7 @@ class stageuser_activate(LDAPQuery):
                     try:
                         v.decode('utf-8')
                         self.log.debug("merge: %s:%r wiped" % (attr, v))
-                    except ValueError:
+                    except Exception:
                         self.log.debug("merge %s: [no_print %s]" % (attr, v.__class__.__name__))
                     if isinstance(entry_to[attr], (list, tuple)):
                         # multi value attribute
@@ -581,7 +581,7 @@ class stageuser_activate(LDAPQuery):
                     try:
                         v.decode('utf-8')
                         self.log.debug("Add: %s:%r" % (attr, v))
-                    except ValueError:
+                    except Exception:
                         self.log.debug("Add %s: [no_print %s]" % (attr, v.__class__.__name__))
 
                     if isinstance(entry_to[attr], (list, tuple)):

ipapython/config.py
@@ -160,7 +160,7 @@ def __parse_config(discover_server = True):
     try:
         if not config.default_realm:
             config.default_realm = p.get("global", "realm")
-    except AttributeError:
+    except Exception:
         pass
     if discover_server:
         try:
@@ -172,7 +172,7 @@ def __parse_config(discover_server = True):
     try:
         if not config.default_domain:
             config.default_domain = p.get("global", "domain")
-    except AttributeError:
+    except Exception:
         pass
 
 def __discover_config(discover_server = True):