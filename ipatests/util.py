eadd47eec6ceb38b001bc9bff14e2a5aa83eb2ab
Milan Kubík <mkubik@redhat.com>
2016-04-05 10:04:37 +0200

    ipatests: fix for change_principal context manager
    The context manager was leaving API object disconnected when
    an exception was raised inside of it. This led to resource leak
    in the tests.
    
    https://fedorahosted.org/freeipa/ticket/5733
    
    Reviewed-By: Martin Babinsky <mbabinsk@redhat.com>

--- ipatests/util.py
+++ ipatests/util.py
@@ -687,13 +687,14 @@ def change_principal(user, password, client=None, path=None):
 
     client.Backend.rpcclient.disconnect()
 
-    with private_ccache(ccache_name):
-        kinit_password(user, password, ccache_name)
+    try:
+        with private_ccache(ccache_name):
+            kinit_password(user, password, ccache_name)
+            client.Backend.rpcclient.connect()
+
+            try:
+                yield
+            finally:
+                client.Backend.rpcclient.disconnect()
+    finally:
         client.Backend.rpcclient.connect()
-
-        try:
-            yield
-        finally:
-            client.Backend.rpcclient.disconnect()
-
-    client.Backend.rpcclient.connect()
