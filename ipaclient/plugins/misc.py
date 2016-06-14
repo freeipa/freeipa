365d9737639f38e4252bdb0203760739ca7b1edc
Jan Cholasta <jcholast@redhat.com>
2016-06-14 16:58:24 +0200

    misc: fix empty CLI output of `env` and `plugins` commands
    https://fedorahosted.org/freeipa/ticket/4739
    
    Reviewed-By: David Kupka <dkupka@redhat.com>

---
+++ ipaclient/plugins/misc.py
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2016  FreeIPA Contributors see COPYING for license
+#
+
+from ipaclient.frontend import CommandOverride
+from ipalib.plugable import Registry
+
+register = Registry()
+
+
+@register(override=True)
+class env(CommandOverride):
+    def output_for_cli(self, textui, output, *args, **options):
+        options['all'] = True
+        return super(env, self).output_for_cli(textui, output,
+                                               *args, **options)
+
+
+@register(override=True)
+class plugins(CommandOverride):
+    def output_for_cli(self, textui, output, *args, **options):
+        options['all'] = True
+        return super(plugins, self).output_for_cli(textui, output,
+                                                   *args, **options)
