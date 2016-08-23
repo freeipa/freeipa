95a594af4c99255ea4da27e609cf41b79ca7ed91
Abhijeet Kasurde <akasurde@redhat.com>
2016-08-23 17:34:51 +0530

    Handled empty hostname in server-del command
    Fixes: https://fedorahosted.org/freeipa/ticket/6248
    
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>
    Reviewed-By: Martin Basti <mbasti@redhat.com>
    Reviewed-By: Stanislav Laznicka <slaznick@redhat.com>

--- ipaclient/plugins/server.py
+++ ipaclient/plugins/server.py
@@ -3,7 +3,7 @@
 #
 
 from ipaclient.frontend import MethodOverride
-from ipalib import _
+from ipalib import _, errors
 from ipalib.plugable import Registry
 
 register = Registry()
@@ -12,6 +12,10 @@ register = Registry()
 @register(override=True, no_fail=True)
 class server_del(MethodOverride):
     def interactive_prompt_callback(self, kw):
+        server_list = kw.get('cn')
+        if not server_list:
+            raise errors.RequirementError(name='cn')
+
         self.api.Backend.textui.print_plain(
             _("Removing %(servers)s from replication topology, "
-              "please wait...") % {'servers': ', '.join(kw['cn'])})
+              "please wait...") % {'servers': ', '.join(server_list)})
