5710ecddcaea7b00fbbc2ee24b845f7a4ac90f57
Oleg Fayans <ofayans@redhat.com>
2016-10-13 08:46:35 +0200

    Reverted the essertion for replica uninstall returncode
    As the issue with ipa installer always returning 0 returncode was addressed,
    the test needs to be made aware of this change.
    
    https://fedorahosted.org/freeipa/ticket/6401
    
    Reviewed-By: Martin Basti <mbasti@redhat.com>
    Reviewed-By: Martin Babinsky <mbabinsk@redhat.com>

--- ipatests/test_integration/test_replica_promotion.py
+++ ipatests/test_integration/test_replica_promotion.py
@@ -357,12 +357,8 @@ class TestProhibitReplicaUninstallation(IntegrationTest):
         result = self.replicas[0].run_command(['ipa-server-install',
                                                '--uninstall', '-U'],
                                               raiseonerr=False)
-        # Due to ticket 3230 server installation/uninstallation always returns
-        # 0 unless an uncaught exception occurs. Once this issue is properly
-        # addressed, please care to change expected return code in the
-        # following assert from 0 to something else.
         assert_error(result, "Removal of '%s' leads to disconnected"
-                             " topology" % self.replicas[0].hostname, 0)
+                             " topology" % self.replicas[0].hostname, 1)
         self.replicas[0].run_command(['ipa-server-install', '--uninstall',
                                       '-U', '--ignore-topology-disconnect'])
 
