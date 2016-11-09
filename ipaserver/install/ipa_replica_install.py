882a6dce46a0a80343871d0a6f713e468212baca
Jan Cholasta <jcholast@redhat.com>
2016-11-09 12:49:25 +0100

    install: migrate server installers to the new class hierarchy
    Migrate ipa-server-install and ipa-replica-install from the old installer
    classes to the new installer class hierarchy classes.
    
    https://fedorahosted.org/freeipa/ticket/6392
    
    Reviewed-By: Martin Basti <mbasti@redhat.com>

---
+++ ipaserver/install/ipa_replica_install.py
@@ -0,0 +1,95 @@
+#
+# Copyright (C) 2015  FreeIPA Contributors see COPYING for license
+#
+
+from ipapython.install import cli
+from ipapython.install.core import knob
+from ipaplatform.paths import paths
+from ipaserver.install.server import ServerReplicaInstall
+
+
+class CompatServerReplicaInstall(ServerReplicaInstall):
+    ca_cert_files = None
+    all_ip_addresses = False
+    no_wait_for_dns = True
+    nisdomain = None
+    no_nisdomain = False
+    no_sudo = False
+    request_cert = False
+    ca_file = None
+    zonemgr = None
+
+    replica_file = knob(
+        # pylint: disable=no-member
+        bases=ServerReplicaInstall.replica_file,
+        cli_names='replica_file',
+    )
+
+    auto_password = knob(
+        str, None,
+        description="Password to join the IPA realm. Assumes bulk password "
+                    "unless principal is also set. (domain level 1+) "
+                    "Directory Manager (existing master) password. (domain "
+                    "level 0)",
+        sensitive=True,
+        cli_names=['--password', '-p'],
+        cli_metavar='PASSWORD',
+    )
+
+    @property
+    def dm_password(self):
+        try:
+            return self.__dm_password
+        except AttributeError:
+            pass
+
+        if self.replica_file is not None:
+            return self.auto_password
+
+        return super(CompatServerReplicaInstall, self).dm_password
+
+    @dm_password.setter
+    def dm_password(self, value):
+        self.__dm_password = value
+
+    ip_addresses = knob(
+        # pylint: disable=no-member
+        bases=ServerReplicaInstall.ip_addresses,
+        description="Replica server IP Address. This option can be used "
+                    "multiple times",
+    )
+
+    admin_password = knob(
+        # pylint: disable=no-member
+        bases=ServerReplicaInstall.admin_password,
+        cli_names=list(ServerReplicaInstall.admin_password.cli_names) + ['-w'],
+    )
+
+    @admin_password.default_getter
+    def admin_password(self):
+        if self.replica_file is None and self.principal:
+            return self.auto_password
+
+        return super(CompatServerReplicaInstall, self).admin_password
+
+    @property
+    def host_password(self):
+        admin_password = (
+            super(CompatServerReplicaInstall, self).admin_password)
+        if (self.replica_file is None and
+                (not self.principal or admin_password)):
+            return self.auto_password
+
+        return super(CompatServerReplicaInstall, self).host_password
+
+
+ReplicaInstall = cli.install_tool(
+    CompatServerReplicaInstall,
+    command_name='ipa-replica-install',
+    log_file_name=paths.IPAREPLICA_INSTALL_LOG,
+    debug_option=True,
+)
+
+
+def run():
+    ReplicaInstall.run_cli()
---
+++ ipaserver/install/ipa_server_install.py
@@ -0,0 +1,49 @@
+#
+# Copyright (C) 2015  FreeIPA Contributors see COPYING for license
+#
+
+from ipapython.install import cli
+from ipapython.install.core import knob
+from ipaplatform.paths import paths
+from ipaserver.install.server import ServerMasterInstall
+
+
+class CompatServerMasterInstall(ServerMasterInstall):
+    all_ip_addresses = False
+    nisdomain = None
+    no_nisdomain = False
+    no_sudo = False
+    request_cert = False
+
+    new_dm_password = knob(
+        # pylint: disable=no-member
+        bases=ServerMasterInstall.new_dm_password,
+        cli_names=['--ds-password', '-p'],
+    )
+
+    new_admin_password = knob(
+        # pylint: disable=no-member
+        bases=ServerMasterInstall.new_admin_password,
+        cli_names=(list(ServerMasterInstall.new_admin_password.cli_names) +
+                   ['-a']),
+    )
+
+    ip_addresses = knob(
+        # pylint: disable=no-member
+        bases=ServerMasterInstall.ip_addresses,
+        description="Master Server IP Address. This option can be used "
+                    "multiple times",
+    )
+
+
+ServerInstall = cli.install_tool(
+    CompatServerMasterInstall,
+    command_name='ipa-server-install',
+    log_file_name=paths.IPASERVER_INSTALL_LOG,
+    debug_option=True,
+    uninstall_log_file_name=paths.IPASERVER_UNINSTALL_LOG,
+)
+
+
+def run():
+    ServerInstall.run_cli()
