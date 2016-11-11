e2060e8e5562ed6a4fe760eba1babb5c1761576a
Petr Spacek <pspacek@redhat.com>
2016-11-11 15:32:29 +0100

    Build: update makerpms.sh to use same paths as rpmbuild
    This allows us to simply use makerpms.sh to configure the build tree,
    install RPMs to configure system for the first time and then use make install
    for rapid devel/test cycles.
    
    Configuration parameteres were taken from rpm-4.13.0-0.rc1.27.fc24.x86_64.
    
    https://fedorahosted.org/freeipa/ticket/6418
    
    Reviewed-By: Lukas Slebodnik <lslebodn@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

--- makerpms.sh
+++ makerpms.sh
@@ -4,7 +4,26 @@ set -o errexit
 pushd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 test ! -x "configure" && autoreconf -i
-test ! -f "Makefile" && ./configure --enable-silent-rules "$@"
+# run configure with the same parameters as RPM build
+# this makes it easy to tweak files locally and use make install
+test ! -f "Makefile" && ./configure --enable-silent-rules \
+	--host=$(rpm -E %{_host}) \
+	--build=$(rpm -E %{_build}) \
+	--program-prefix=$(rpm -E %{?_program_prefix}) \
+	--prefix=$(rpm -E %{_prefix}) \
+	--exec-prefix=$(rpm -E %{_exec_prefix}) \
+	--bindir=$(rpm -E %{_bindir}) \
+	--sbindir=$(rpm -E %{_sbindir}) \
+	--sysconfdir=$(rpm -E %{_sysconfdir}) \
+	--datadir=$(rpm -E %{_datadir}) \
+	--includedir=$(rpm -E %{_includedir}) \
+	--libdir=$(rpm -E %{_libdir}) \
+	--libexecdir=$(rpm -E %{_libexecdir}) \
+	--localstatedir=$(rpm -E %{_localstatedir}) \
+	--sharedstatedir=$(rpm -E %{_sharedstatedir}) \
+	--mandir=$(rpm -E %{_mandir}) \
+	--infodir=$(rpm -E %{_infodir}) \
+	"$@"
 make rpms
 
 popd
