c568b0a008d99aa10740d1f77c55aaf50560ae92
Petr Spacek <pspacek@redhat.com>
2016-11-11 12:46:00 +0100

    Build: enable silent build in makerpms.sh
    Build called from makerpms.sh is not verbose by default anymore.
    It still prints all directories and files it builds but the long
    command lines are hidden by default.
    
    It has the advantage that compiler and other warnings are visible to
    developers right away. If you need to debug something,
    use --disable-silent-rules to override the default
    (or call configure manually).
    
    https://fedorahosted.org/freeipa/ticket/6418

--- makerpms.sh
+++ makerpms.sh
@@ -4,7 +4,7 @@ set -o errexit
 pushd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 test ! -x "configure" && autoreconf -i
-test ! -f "Makefile" && ./configure "$@"
+test ! -f "Makefile" && ./configure --enable-silent-rules "$@"
 make rpms
 
 popd
