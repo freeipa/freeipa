6f7d982fe2e2d2f042e85710b8d8d59167e5796f
Fraser Tweedale <ftweedal@redhat.com>
2016-12-20 23:29:22 +1000

    Set up DS TLS on replica in CA-less topology
    Fixes: https://fedorahosted.org/freeipa/ticket/6226
    Reviewed-By: Tomas Krizek <tkrizek@redhat.com>

--- ipaserver/install/dsinstance.py
+++ ipaserver/install/dsinstance.py
@@ -390,7 +390,9 @@ class DsInstance(service.Service):
 
         self.step("creating DS keytab", self._request_service_keytab)
         if self.promote:
-            if self.ca_is_configured:
+            if self.pkcs12_info:
+                self.step("configuring ssl for ds instance", self.__enable_ssl)
+            else:
                 self.step("retrieving DS Certificate", self.__get_ds_cert)
             self.step("restarting directory server", self.__restart_instance)
 
