083b4241d287a731e2cf7fed5c61b30da52a8e37
Martin Basti <mbasti@redhat.com>
2017-01-09 19:49:26 +0100

    Remove duplicated step from DS install
    "Adding SASL mappings.." is duplicated step in __common_setup in DS
    instance and should be removed.
    
    Reviewed-By: Stanislav Laznicka <slaznick@redhat.com>

--- ipaserver/install/dsinstance.py
+++ ipaserver/install/dsinstance.py
@@ -345,8 +345,6 @@ class DsInstance(service.Service):
         self.step("adding range check plugin", self.__add_range_check_plugin)
         if hbac_allow:
             self.step("creating default HBAC rule allow_all", self.add_hbac)
-        self.step("adding sasl mappings to the directory",
-                  self.__configure_sasl_mappings)
         self.step("adding entries for topology management", self.__add_topology_entries)
 
         self.__common_post_setup()
