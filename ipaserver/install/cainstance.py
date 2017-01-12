9e33ca9cbac1837cf779673e5479f3718ef8e759
Florence Blanc-Renaud <flo@redhat.com>
2017-01-12 18:17:15 +0100

    Do not configure PKI ajp redirection to use "::1"
    When ipa-server-install configures PKI, it provides a configuration file
    with the parameter pki_ajp_host set to ::1. This parameter is used to configure
    Tomcat redirection in /etc/pki/pki-tomcat/server.xml:
        <Connector port="8009"
                protocol="AJP/1.3"
                redirectPort="8443"
                address="::1" />
    ie all requests to port 8009 are redirected to port 8443 on address ::1.
    
    If the /etc/hosts config file does not define ::1 for localhost, then AJP
    redirection fails and replica install is not able to request a certificate
    for the replica.
    
    Since PKI has been fixed (see PKI ticket 2570) to configure by default the AJP
    redirection with "localhost", FreeIPA does not need any more to override
    this setting.
    The code now depends on pki 10.3.5-11 which provides the fix in the template
    and the upgrade.
    
    https://fedorahosted.org/freeipa/ticket/6575

--- ipaserver/install/cainstance.py
+++ ipaserver/install/cainstance.py
@@ -594,10 +594,6 @@ class CAInstance(DogtagInstance):
             config.set("CA", "pki_external_ca_cert_chain_path", cert_chain_file.name)
             config.set("CA", "pki_external_step_two", "True")
 
-        # PKI IPv6 Configuration
-        config.add_section("Tomcat")
-        config.set("Tomcat", "pki_ajp_host", "::1")
-
         # Generate configuration file
         with open(cfg_file, "w") as f:
             config.write(f)
