acdd1f59782bb836d6c4c255689918368adb8dab
Christian Heimes <cheimes@redhat.com>
2017-02-17 17:46:42 +0100

    Add placeholders for ipaplatform, ipaserver and ipatests
    I also renamed the base directory to pypi and added a new build target
    pypi_packages.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Martin Basti <mbasti@redhat.com>

---
+++ pypi/ipaplatform/ipaplatform/__init__.py
@@ -0,0 +1,5 @@
+#
+# Copyright (C) 2017 FreeIPA Contributors see COPYING for license
+#
+
+raise ImportError("ipaplatform is not yet supported as PyPI package.")
---
+++ pypi/ipaserver/ipaserver/__init__.py
@@ -0,0 +1,5 @@
+#
+# Copyright (C) 2017 FreeIPA Contributors see COPYING for license
+#
+
+raise ImportError("ipaserver is not yet supported as PyPI package.")
