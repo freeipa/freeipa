3cd45f702bd68ba13288d57f650c668451539202
Christian Heimes <cheimes@redhat.com>
2017-04-25 14:52:35 +0200

    ipa-custodia: use Dogtag's alias/pwdfile.txt
    /etc/pki/pki-tomcat/password.conf contains additional passwords like
    replicadb. ipa-custodia does not need these passwords.
    /etc/pki/pki-tomcat/alias/pwdfile.txt holds the passphrase for Tomcat's
    NSSDB. The file also simplifies implementation because it removes
    another temporary file.
    
    pwdfile.txt is created by CAInstance.create_certstore_passwdfile()
    
    Related: https://pagure.io/freeipa/issue/6888
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- ipaplatform/base/paths.py
+++ ipaplatform/base/paths.py
@@ -93,6 +93,7 @@ class BasePathNamespace(object):
     NSS_DB_DIR = "/etc/pki/nssdb"
     PKI_TOMCAT = "/etc/pki/pki-tomcat"
     PKI_TOMCAT_ALIAS_DIR = "/etc/pki/pki-tomcat/alias"
+    PKI_TOMCAT_ALIAS_PWDFILE_TXT = "/etc/pki/pki-tomcat/alias/pwdfile.txt"
     PKI_TOMCAT_PASSWORD_CONF = "/etc/pki/pki-tomcat/password.conf"
     ETC_REDHAT_RELEASE = "/etc/redhat-release"
     RESOLV_CONF = "/etc/resolv.conf"
