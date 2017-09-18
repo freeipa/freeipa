freeipa/freeipa

commit b5732efda6d27f588adbc3f41259bc4511716f43

x509: Make certificates represented as objects
https://pagure.io/freeipa/issue/4985

Reviewed-By: Fraser Tweedale <ftweedal@redhat.com>
Reviewed-By: Rob Crittenden <rcritten@redhat.com>
Reviewed-By: Martin Basti <mbasti@redhat.com>

ipaplatform/redhat/tasks.py
@@ -30,7 +30,6 @@ import os
 import pwd
 import shutil
 import socket
-import base64
 import traceback
 import errno
 
@@ -266,10 +265,10 @@ class RedHatTaskNamespace(BaseTaskNamespace):
         has_eku = set()
         for cert, nickname, trusted, ext_key_usage in ca_certs:
             try:
-                subject = x509.get_der_subject(cert, x509.DER)
-                issuer = x509.get_der_issuer(cert, x509.DER)
-                serial_number = x509.get_der_serial_number(cert, x509.DER)
-                public_key_info = x509.get_der_public_key_info(cert, x509.DER)
+                subject = cert.subject_bytes
+                issuer = cert.issuer_bytes
+                serial_number = cert.serial_number
+                public_key_info = cert.public_key_info_bytes
             except (PyAsn1Error, ValueError, CertificateError) as e:
                 logger.warning(
                     "Failed to decode certificate \"%s\": %s", nickname, e)
@@ -278,11 +277,8 @@ class RedHatTaskNamespace(BaseTaskNamespace):
             label = urllib.parse.quote(nickname)
             subject = urllib.parse.quote(subject)
             issuer = urllib.parse.quote(issuer)
-            serial_number = urllib.parse.quote(serial_number)
+            serial_number = urllib.parse.quote(str(serial_number))
             public_key_info = urllib.parse.quote(public_key_info)
-
-            cert = base64.b64encode(cert)
-            cert = x509.make_pem(cert)
 
             obj = ("[p11-kit-object-v1]\n"
                    "class: certificate\n"
@@ -302,14 +298,12 @@ class RedHatTaskNamespace(BaseTaskNamespace):
                 obj += "trusted: true\n"
             elif trusted is False:
                 obj += "x-distrusted: true\n"
-            obj += "%s\n\n" % cert
+            obj += "{pem}\n\n".format(pem=cert.public_bytes(x509.Encoding.PEM))
             f.write(obj)
 
             if ext_key_usage is not None and public_key_info not in has_eku:
-                if not ext_key_usage:
-                    ext_key_usage = {x509.EKU_PLACEHOLDER}
                 try:
-                    ext_key_usage = x509.encode_ext_key_usage(ext_key_usage)
+                    ext_key_usage = cert.extended_key_usage_bytes
                 except PyAsn1Error as e:
                     logger.warning(
                         "Failed to encode extended key usage for \"%s\": %s",

install/certmonger/dogtag-ipa-ca-renew-agent-submit
@@ -31,7 +31,6 @@ import syslog
 import traceback
 import tempfile
 import shutil
-import base64
 import contextlib
 import json
 
@@ -266,8 +265,8 @@ def store_cert(**kwargs):
     cert = os.environ.get('CERTMONGER_CERTIFICATE')
     if not cert:
         return (REJECTED, "New certificate requests not supported")
-
-    dercert = x509.normalize_certificate(cert)
+    cert = x509.load_pem_x509_certificate(cert)
+    dercert = cert.public_bytes(x509.Encoding.DER)
 
     dn = DN(('cn', nickname), ('cn', 'ca_renewal'),
             ('cn', 'ipa'), ('cn', 'etc'), api.env.basedn)
@@ -365,7 +364,8 @@ def retrieve_or_reuse_cert(**kwargs):
     if not nickname:
         return (REJECTED, "Nickname could not be determined")
 
-    cert = os.environ.get('CERTMONGER_CERTIFICATE')
+    cert = x509.load_pem_x509_certificate(
+        fix_pem(os.environ.get('CERTMONGER_CERTIFICATE'))) # TODO: the fix_pem somehow got there early, so making this comment way too long to get rid of it later
     if not cert:
         return (REJECTED, "New certificate requests not supported")
 
@@ -378,11 +378,10 @@ def retrieve_or_reuse_cert(**kwargs):
         except errors.NotFound:
             pass
         else:
-            cert = entry.single_value['usercertificate']
-            cert = base64.b64encode(cert)
-            cert = x509.make_pem(cert)
+            cert = x509.load_der_x509_certificate(
+                entry.single_value['usercertificate'])
 
-    return (ISSUED, cert)
+    return (ISSUED, cert.public_bytes(x509.Encoding.PEM))
 
 
 def retrieve_cert_continuous(reuse_existing, **kwargs):
@@ -392,7 +391,7 @@ def retrieve_cert_continuous(reuse_existing, **kwargs):
     """
     old_cert = os.environ.get('CERTMONGER_CERTIFICATE')
     if old_cert:
-        old_cert = x509.normalize_certificate(old_cert)
+        old_cert = x509.load_pem_x509_certificate(old_cert)
 
     result = call_handler(retrieve_or_reuse_cert,
                           reuse_existing=reuse_existing,
@@ -400,7 +399,7 @@ def retrieve_cert_continuous(reuse_existing, **kwargs):
     if result[0] != ISSUED or reuse_existing:
         return result
 
-    new_cert = x509.normalize_certificate(result[1])
+    new_cert = x509.load_pem_x509_certificate(result[1])
     if new_cert == old_cert:
         syslog.syslog(syslog.LOG_INFO, "Updated certificate not available")
         # No cert available yet, tell certmonger to wait another 8 hours
@@ -431,7 +430,8 @@ def renew_ca_cert(reuse_existing, **kwargs):
     cert = os.environ.get('CERTMONGER_CERTIFICATE')
     if not cert:
         return (REJECTED, "New certificate requests not supported")
-    is_self_signed = x509.is_self_signed(cert)
+    cert = x509.load_pem_x509_certificate(cert)
+    is_self_signed = cert.is_self_signed()
 
     operation = os.environ.get('CERTMONGER_OPERATION')
     if operation == 'SUBMIT':

install/restart_scripts/renew_ca_cert
@@ -29,7 +29,7 @@ import traceback
 
 from ipalib.install import certstore
 from ipapython import ipautil
-from ipalib import api, errors, x509
+from ipalib import api, errors
 from ipalib.install.kinit import kinit_keytab
 from ipaserver.install import certs, cainstance, installutils
 from ipaserver.plugins.ldap2 import ldap2
@@ -64,7 +64,7 @@ def _main():
 
     # Fetch the new certificate
     db = certs.CertDB(api.env.realm, nssdir=paths.PKI_TOMCAT_ALIAS_DIR)
-    cert = db.get_cert_from_db(nickname, pem=False)
+    cert = db.get_cert_from_db(nickname)
     if not cert:
         syslog.syslog(syslog.LOG_ERR, 'No certificate %s found.' % nickname)
         sys.exit(1)
@@ -106,7 +106,7 @@ def _main():
                 cfg_path, 'subsystem.select', '=')
             if config == 'New':
                 syslog.syslog(syslog.LOG_NOTICE, "Updating CS.cfg")
-                if x509.is_self_signed(cert, x509.DER):
+                if cert.is_self_signed():
                     installutils.set_directive(
                         cfg_path, 'hierarchy.select', 'Root',
                         quotes=False, separator='=')

install/tools/ipa-replica-conncheck
@@ -47,7 +47,6 @@ from socket import SOCK_STREAM, SOCK_DGRAM
 import distutils.spawn
 from ipaplatform.paths import paths
 import gssapi
-from cryptography.hazmat.primitives import serialization
 
 logger = logging.getLogger(os.path.basename(__file__))
 
@@ -544,10 +543,8 @@ def main():
                         ca_certs = x509.load_certificate_list_from_file(
                             options.ca_cert_file)
                         for ca_cert in ca_certs:
-                            data = ca_cert.public_bytes(
-                                serialization.Encoding.DER)
                             nss_db.add_cert(
-                                data,
+                                ca_cert,
                                 str(DN(ca_cert.subject)),
                                 certdb.EXTERNAL_CA_TRUST_FLAGS)
 

ipaclient/install/client.py
@@ -27,7 +27,6 @@ import tempfile
 import time
 import traceback
 
-from cryptography.hazmat.primitives import serialization
 # pylint: disable=import-error
 from six.moves.configparser import RawConfigParser
 from six.moves.urllib.parse import urlparse, urlunparse
@@ -1647,9 +1646,7 @@ def get_ca_certs_from_ldap(server, basedn, realm):
         logger.debug("get_ca_certs_from_ldap() error: %s", e)
         raise
 
-    certs = [x509.load_der_x509_certificate(c[0]) for c in certs
-             if c[2] is not False]
-
+    certs = [c[0] for c in certs if c[2] is not False]
     return certs
 
 
@@ -1830,10 +1827,6 @@ def get_ca_certs(fstore, options, server, basedn, realm):
 
     if ca_certs is not None:
         try:
-            ca_certs = [
-                cert.public_bytes(serialization.Encoding.DER)
-                for cert in ca_certs
-            ]
             x509.write_certificate_list(ca_certs, ca_file)
         except Exception as e:
             if os.path.exists(ca_file):
@@ -2680,10 +2673,6 @@ def _install(options):
 
         # Add CA certs to a temporary NSS database
         ca_certs = x509.load_certificate_list_from_file(paths.IPA_CA_CRT)
-        ca_certs = [
-            cert.public_bytes(serialization.Encoding.DER)
-            for cert in ca_certs
-        ]
         try:
             tmp_db.create_db()
 

ipaclient/plugins/ca.py
@@ -2,7 +2,6 @@
 # Copyright (C) 2016  FreeIPA Contributors see COPYING for license
 #
 
-import base64
 from ipaclient.frontend import MethodOverride
 from ipalib import errors, util, x509, Str
 from ipalib.plugable import Registry
@@ -34,15 +33,15 @@ class WithCertOutArgs(MethodOverride):
 
         result = super(WithCertOutArgs, self).forward(*keys, **options)
         if filename:
-            def to_pem(x):
-                return x509.make_pem(x)
             if options.get('chain', False):
-                ders = result['result']['certificate_chain']
-                data = '\n'.join(to_pem(base64.b64encode(der)) for der in ders)
+                certs = (x509.load_der_x509_certificate(c)
+                         for c in result['result']['certificate_chain'])
             else:
-                data = to_pem(result['result']['certificate'])
-            with open(filename, 'wb') as f:
-                f.write(data)
+                certs = [
+                    x509.load_der_x509_certificate(
+                        result['result']['certificate'])
+                ]
+            x509.write_certificate_list(certs, filename)
 
         return result
 

ipaclient/plugins/cert.py
@@ -66,10 +66,9 @@ class CertRetrieveOverride(MethodOverride):
                 certs = result['result']['certificate_chain']
             else:
                 certs = [result['result']['certificate']]
-            certs = (x509.ensure_der_format(cert) for cert in certs)
-            certs = (x509.make_pem(base64.b64encode(cert)) for cert in certs)
-            with open(certificate_out, 'w') as f:
-                f.write('\n'.join(certs))
+            certs = (x509.load_der_x509_certificate(
+                     x509.ensure_der_format(cert)) for cert in certs)
+            x509.write_certificate_list(certs, certificate_out)
 
         return result
 

commit d1709b252428260923e9ac9aea7a64c0ff3770c4

Don't write p11-kit EKU extension object if no EKU
b5732efd introduced a regression because it tries to write EKU
that's actually in the CA cert instead of using the LDAP information.
However, when no EKU is available,
IPACertificate.extended_key_usage_bytes still returned at least
EKU_PLACEHOLDER OID to keep the behavior the same as in previous
versions. This caused the EKU_PLACEHOLDER to be written in the
ipa.p11-kit file which made Firefox report FreeIPA Web UI as
improperly configured.

https://pagure.io/freeipa/issue/7119

ipalib/x509.py
@@ -295,8 +295,11 @@ class IPACertificate(object):
 
     @property
     def extended_key_usage_bytes(self):
+        eku = self.extended_key_usage
+        if eku is None:
+            return
+
         ekurfc = rfc2459.ExtKeyUsageSyntax()
-        eku = self.extended_key_usage or {EKU_PLACEHOLDER}
         for i, oid in enumerate(eku):
             ekurfc[i] = univ.ObjectIdentifier(oid)
         ekurfc = encoder.encode(ekurfc)

ipaplatform/redhat/tasks.py
@@ -257,7 +257,7 @@ class RedHatTaskNamespace(BaseTaskNamespace):
                 "\n")
 
         has_eku = set()
-        for cert, nickname, trusted, ext_key_usage in ca_certs:
+        for cert, nickname, trusted, _ext_key_usage in ca_certs:
             try:
                 subject = cert.subject_bytes
                 issuer = cert.issuer_bytes
@@ -296,7 +296,8 @@ class RedHatTaskNamespace(BaseTaskNamespace):
                 pem=cert.public_bytes(x509.Encoding.PEM).decode('ascii'))
             f.write(obj)
 
-            if ext_key_usage is not None and public_key_info not in has_eku:
+            if (cert.extended_key_usage is not None and
+                    public_key_info not in has_eku):
                 try:
                     ext_key_usage = cert.extended_key_usage_bytes
                 except PyAsn1Error as e: