7fa6eb29ef9fd29efaed36b7e31a0c8a11d93fec
Felipe Barreto <fbarreto@redhat.com>
2017-09-20 09:51:44 -0300

    Checks if Dir Server is installed and running before IPA installation
    In cases when IPA is installed in two steps (external CA), it's
    necessary to check (in the second step) if Dir. Server is
    running before continue with the installation. If it's not,
    start Directory Server.
    
    https://pagure.io/freeipa/issue/6611

--- ipaserver/install/server/install.py
+++ ipaserver/install/server/install.py
@@ -618,6 +618,14 @@ def install_check(installer):
         # check addresses here, dns module is doing own check
         no_matching_interface_for_ip_address_warning(ip_addresses)
 
+    instance_name = "-".join(realm_name.split("."))
+    dirsrv = services.knownservices.dirsrv
+    if (options.external_cert_files
+           and dirsrv.is_installed(instance_name)
+           and not dirsrv.is_running(instance_name)):
+        logger.debug('Starting Directory Server')
+        services.knownservices.dirsrv.start(instance_name)
+
     if options.setup_adtrust:
         adtrust.install_check(False, options, api)
 
