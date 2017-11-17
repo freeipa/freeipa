a57f61331459fc16769858b5043e869ea7f350fc
Alexander Bokovoy <abokovoy@redhat.com>
2017-11-17 17:19:25 +0200

    trust: detect and error out when non-AD trust with IPA domain name exists
    Quite often users choose wrong type of trust on Active Directory side
    when setting up a trust to freeIPA. The trust type supported by freeIPA
    is just a normal forest trust to another Active Directory. However,
    some people follow old internet recipes that force using a trust to MIT
    Kerberos realm.
    
    This is a wrong type of trust. Unfortunately, when someone used MIT
    Kerberos realm trust, there is no way to programmatically remote the
    trust from freeIPA side. As result, we have to detect such situation and
    report an error.
    
    To do proper reporting, we need reuse some constants and trust type
    names we use in IPA CLI/Web UI. These common components were moved to
    a separate ipaserver/dcerpc_common.py module that is imported by both
    ipaserver/plugins/trust.py and ipaserver/dcerpc.py.
    
    Fixes https://pagure.io/freeipa/issue/7264
    
    Reviewed-By: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Thierry Bordaz <tbordaz@redhat.com>

--- ipaserver/dcerpc.py
+++ ipaserver/dcerpc.py
@@ -31,6 +31,10 @@ from ipalib import errors
 from ipapython import ipautil
 from ipapython.dn import DN
 from ipaserver.install import installutils
+from ipaserver.dcerpc_common import (TRUST_BIDIRECTIONAL,
+                                     TRUST_JOIN_EXTERNAL,
+                                     trust_type_string)
+
 from ipalib.util import normalize_name
 
 import os
@@ -75,15 +79,6 @@ and Samba4 python bindings.
 """)
 
 logger = logging.getLogger(__name__)
-
-# Both constants can be used as masks against trust direction
-# because bi-directional has two lower bits set.
-TRUST_ONEWAY = 1
-TRUST_BIDIRECTIONAL = 3
-
-# Trust join behavior
-# External trust -- allow creating trust to a non-root domain in the forest
-TRUST_JOIN_EXTERNAL = 1
 
 
 def is_sid_valid(sid):
@@ -149,6 +144,7 @@ pysss_type_key_translation_dict = {
     # Used for users with magic private groups
     pysss_nss_idmap.ID_BOTH: 'both',
 }
+
 
 class TrustTopologyConflictSolved(Exception):
     """
@@ -1263,9 +1259,26 @@ class TrustDomainInstance(object):
             dname = lsa.String()
             dname.string = another_domain.info['dns_domain']
             res = self._pipe.QueryTrustedDomainInfoByName(
-                                self._policy_handle,
-                                dname,
-                                lsa.LSA_TRUSTED_DOMAIN_INFO_FULL_INFO)
+                self._policy_handle,
+                dname,
+                lsa.LSA_TRUSTED_DOMAIN_INFO_FULL_INFO
+            )
+            if res.info_ex.trust_type != lsa.LSA_TRUST_TYPE_UPLEVEL:
+                msg = _('There is already a trust to {ipa_domain} with '
+                        'unsupported type {trust_type}. Please remove '
+                        'it manually on AD DC side.')
+                ttype = trust_type_string(
+                    res.info_ex.trust_type, res.info_ex.trust_attributes
+                )
+                err = unicode(msg).format(
+                    ipa_domain=another_domain.info['dns_domain'],
+                    trust_type=ttype)
+
+                raise errors.ValidationError(
+                    name=_('AD domain controller'),
+                    error=err
+                )
+
             self._pipe.DeleteTrustedDomain(self._policy_handle,
                                            res.info_ex.sid)
         except RuntimeError as e:
