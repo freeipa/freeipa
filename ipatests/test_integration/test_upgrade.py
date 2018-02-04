60654ae6d1f379277784d7761d6a03f3ae643bb3
Rob Crittenden <rcritten@redhat.com>
2018-02-04 11:40:24 -0500

    Fix detection of KRA installation so upgrades can succeed
    Use is_installed() instead of is_configured() because
    is_installed() does a config file check to see if the service
    is in use.
    
    https://pagure.io/freeipa/issue/7389
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

---
+++ ipatests/test_integration/test_upgrade.py
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2018  FreeIPA Contributors see COPYING for license
+#
+
+"""
+Module provides tests to verify that the upgrade script works.
+"""
+
+from ipatests.test_integration.base import IntegrationTest
+from ipatests.pytest_plugins.integration import tasks
+
+
+class TestUpgrade(IntegrationTest):
+    @classmethod
+    def install(cls, mh):
+        tasks.install_master(cls.master, setup_dns=False)
+
+    def test_invoke_upgrader(self):
+        cmd = self.master.run_command(['ipa-server-upgrade'],
+                                      raiseonerr=False)
+        assert cmd.returncode == 0
