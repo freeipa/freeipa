454da60a7c5048dd883f3271168af7eeba1cac42
Felipe Barreto <fbarreto@redhat.com>
2018-02-08 16:07:49 -0200

    WebUI Tests: fixing test_range test case
    As described in the commit [1] and ticket [2], it should not be possible to
    change the range of a local IPA domain.
    
    The basic_crud was changed to make it flexible to do not run the mod operation
    if needed.
    
    [1] 55feea500be1f4ae7bf02ef3c48377a6751ca71d
    [2] https://pagure.io/freeipa/issue/4826

--- ipatests/test_webui/ui_driver.py
+++ ipatests/test_webui/ui_driver.py
@@ -1283,6 +1283,7 @@ class UI_driver(object):
                    update_btn='save',
                    breadcrumb=None,
                    navigate=True,
+                   mod=True,
                    delete=True):
         """
         Basic CRUD operation sequence.
@@ -1333,7 +1334,7 @@ class UI_driver(object):
         self.validate_fields(data.get('add_v'))
 
         # 4. Mod values
-        if data.get('mod'):
+        if mod and data.get('mod'):
             self.mod_record(entity, data, details_facet, update_btn)
             self.validate_fields(data.get('mod_v'))
 
