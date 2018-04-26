cdd39b2f14706beb7d61934cd39ac80f7cdf9b76
Christian Heimes <cheimes@redhat.com>
2018-04-26 12:06:36 +0200

    Use single Custodia instance in installers
    Installers now pass a single CustodiaInstance object around, instead of
    creating new instances on demand. In case of replica promotion with CA,
    the instance gets all secrets from a master with CA present. Before, an
    installer created multiple instances and may have requested CA key
    material from a different machine than DM password hash.
    
    In case of Domain Level 1 and replica promotion, the CustodiaInstance no
    longer adds the keys to the local instance and waits for replication to
    other replica. Instead the installer directly uploads the new public
    keys to the remote 389-DS instance.
    
    Without promotion, new Custodia public keys are still added to local
    389-DS over LDAPI.
    
    Fixes: https://pagure.io/freeipa/issue/7518
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- ipaserver/install/kra.py
+++ ipaserver/install/kra.py
@@ -18,7 +18,6 @@ from ipaplatform.paths import paths
 from ipapython import certdb
 from ipapython import ipautil
 from ipapython.install.core import group
-from ipaserver.install import custodiainstance
 from ipaserver.install import cainstance
 from ipaserver.install import krainstance
 from ipaserver.install import dsinstance
@@ -70,7 +69,7 @@ def install_check(api, replica_config, options):
                                    "new replica file.")
 
 
-def install(api, replica_config, options):
+def install(api, replica_config, options, custodia):
     if replica_config is None:
         if not options.setup_kra:
             return
@@ -93,9 +92,6 @@ def install(api, replica_config, options):
                     'host/{env.host}@{env.realm}'.format(env=api.env),
                     paths.KRB5_KEYTAB,
                     ccache)
-                custodia = custodiainstance.CustodiaInstance(
-                    replica_config.host_name,
-                    replica_config.realm_name)
                 custodia.get_kra_keys(
                     replica_config.kra_host_name,
                     krafile,
