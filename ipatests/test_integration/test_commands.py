6652eb088e87cd3f74bd58be3ccda89ec0a46664
Rob Crittenden <rcritten@redhat.com>
2018-05-25 10:16:24 -0400

    Rename test class for testing simple commands, add test
    The concensus in the review was that the name test_commands was
    more generic than test_ipa_cli.
    
    Add a test to change the password for sysaccount users using
    using ldappasswd to confirm that a segfault fix does not regress.
    
    https://pagure.io/freeipa/issue/7561
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>
    Reviewed-By: Tibor Dudlak <tdudlak@redhat.com>

--- ipatests/test_integration/test_commands.py
+++ ipatests/test_integration/test_commands.py
@@ -5,7 +5,15 @@
 """
 from __future__ import absolute_import
 
+import base64
+import ssl
+from tempfile import NamedTemporaryFile
+import textwrap
+
+from ipaplatform.paths import paths
+
 from ipatests.test_integration.base import IntegrationTest
+from ipatests.pytest_plugins.integration import tasks
 
 
 class TestIPACommand(IntegrationTest):
@@ -15,6 +23,88 @@ class TestIPACommand(IntegrationTest):
     tested without having to fire up a full server to run one command.
     """
     topology = 'line'
+
+    def get_cert_base64(self, host, path):
+        """Retrieve cert and return content as single line, base64 encoded
+        """
+        cacrt = host.get_file_contents(path, encoding='ascii')
+        cader = ssl.PEM_cert_to_DER_cert(cacrt)
+        return base64.b64encode(cader).decode('ascii')
+
+    def test_certmap_match_issue7520(self):
+        # https://pagure.io/freeipa/issue/7520
+        tasks.kinit_admin(self.master)
+        result = self.master.run_command(
+            ['ipa', 'certmap-match', paths.IPA_CA_CRT],
+            raiseonerr=False
+        )
+        assert result.returncode == 1
+        assert not result.stderr_text
+        assert "0 users matched" in result.stdout_text
+
+        cab64 = self.get_cert_base64(self.master, paths.IPA_CA_CRT)
+        result = self.master.run_command(
+            ['ipa', 'certmap-match', '--certificate', cab64],
+            raiseonerr=False
+        )
+        assert result.returncode == 1
+        assert not result.stderr_text
+        assert "0 users matched" in result.stdout_text
+
+    def test_cert_find_issue7520(self):
+        # https://pagure.io/freeipa/issue/7520
+        tasks.kinit_admin(self.master)
+        subject = 'CN=Certificate Authority,O={}'.format(
+            self.master.domain.realm)
+
+        # by cert file
+        result = self.master.run_command(
+            ['ipa', 'cert-find', '--file', paths.IPA_CA_CRT]
+        )
+        assert subject in result.stdout_text
+        assert '1 certificate matched' in result.stdout_text
+
+        # by base64 cert
+        cab64 = self.get_cert_base64(self.master, paths.IPA_CA_CRT)
+        result = self.master.run_command(
+            ['ipa', 'cert-find', '--certificate', cab64]
+        )
+        assert subject in result.stdout_text
+        assert '1 certificate matched' in result.stdout_text
+
+    def test_change_sysaccount_password_issue7561(self):
+        sysuser = 'system'
+        original_passwd = 'Secret123'
+        new_passwd = 'userPasswd123'
+
+        master = self.master
+
+        base_dn = str(master.domain.basedn)  # pylint: disable=no-member
+        tf = NamedTemporaryFile()
+        ldif_file = tf.name
+        entry_ldif = textwrap.dedent("""
+            dn: uid=system,cn=sysaccounts,cn=etc,{base_dn}
+            changetype: add
+            objectclass: account
+            objectclass: simplesecurityobject
+            uid: system
+            userPassword: {original_passwd}
+            passwordExpirationTime: 20380119031407Z
+            nsIdleTimeout: 0
+        """).format(
+            base_dn=base_dn,
+            original_passwd=original_passwd)
+        master.put_file_contents(ldif_file, entry_ldif)
+        arg = ['ldapmodify',
+               '-h', master.hostname,
+               '-p', '389', '-D',
+               str(master.config.dirman_dn),   # pylint: disable=no-member
+               '-w', master.config.dirman_password,
+               '-f', ldif_file]
+        master.run_command(arg)
+
+        tasks.ldappasswd_sysaccount_change(sysuser, original_passwd,
+                                           new_passwd, master)
 
     def test_change_selinuxusermaporder(self):
         """
