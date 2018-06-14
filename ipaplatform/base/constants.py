0571ed71489e4eddbf3d88e97c063325a29076eb
Christian Heimes <cheimes@redhat.com>
2018-06-14 17:04:13 +0200

    Increase WSGI process count to 5 on 64bit
    Increase the WSGI daemon worker process count from 2 processes to 5
    processes. This allows IPA RPC to handle more parallel requests. The
    additional processes increase memory consumption by approximante 250 MB
    in total.
    
    Since memory is scarce on 32bit platforms, only 64bit platforms are
    bumped to 5 workers.
    
    Fixes: https://pagure.io/freeipa/issue/7587
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipaplatform/base/constants.py
+++ ipaplatform/base/constants.py
@@ -5,9 +5,11 @@
 '''
 This base platform module exports platform dependant constants.
 '''
+import sys
 
 
 class BaseConstantsNamespace(object):
+    IS_64BITS = sys.maxsize > 2 ** 32
     DS_USER = 'dirsrv'
     DS_GROUP = 'dirsrv'
     HTTPD_USER = "apache"
@@ -28,3 +30,6 @@ class BaseConstantsNamespace(object):
     # nfsd init variable used to enable kerberized NFS
     SECURE_NFS_VAR = "SECURE_NFS"
     SSSD_USER = "sssd"
+    # WSGIDaemonProcess process count. On 64bit platforms, each process
+    # consumes about 110 MB RSS, from which are about 35 MB shared.
+    WSGI_PROCESSES = 5 if IS_64BITS else 2
