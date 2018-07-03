009d6bf8567054b7f1f26b5215dfb0282c4a988c
Armando Neto <abiagion@redhat.com>
2018-07-03 11:00:57 -0300

    ipa-client-install: Update how comments are added by ipachangeconf
    Due to how 'openldap-client' parses its configuration files this patch
    changes how comments are added, moving them to the line above instead
    of appending to the same line.
    
    IPA doesn't want to break existing configuration, if a value already
    exists it adds a comment to the modified setting and a note about that
    on the line above.
    
    New settings will be added without any note.
    
    Issue: https://pagure.io/freeipa/issue/5202
    
    Signed-off-by: Armando Neto <abiagion@redhat.com>
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

---
+++ ipatests/test_install/test_changeconf.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2018  FreeIPA Contributors see COPYING for license
+
+from __future__ import absolute_import
+
+import pytest
+from ipaclient.install.ipachangeconf import IPAChangeConf
+
+
+@pytest.fixture(scope='function')
+def config_filename(tmpdir):
+    filename = tmpdir.mkdir('data').join('config_file.conf')
+    filename.write('SOME_CONF /some/user/defined/path\n')
+    return filename
+
+
+def test_addifnotset_action(config_filename):
+    """Test if addifnotset action adds a comment about the modified conf.
+
+    IPA doesn't want to break existing configuration, if a value already exists
+    it adds a comment to the modified setting and a note about that on the line
+    above.
+
+    New settings will be added without any note.
+    """
+    ipa_conf = IPAChangeConf('IPA Installer Test')
+    ipa_conf.setOptionAssignment(' ')
+
+    opts = [
+        {
+            'action': 'addifnotset',
+            'name': 'SOME_CONF',
+            'type': 'option',
+            'value': '/path/defined/by/ipa',
+        },
+        {
+            'action': 'addifnotset',
+            'name': 'NEW_CONF',
+            'type': 'option',
+            'value': '/path/to/somewhere',
+        },
+    ]
+
+    ipa_conf.changeConf(str(config_filename), opts)
+
+    assert config_filename.readlines() == [
+        '# SOME_CONF modified by IPA\n',
+        '#SOME_CONF /path/defined/by/ipa\n',
+        'SOME_CONF /some/user/defined/path\n',
+        'NEW_CONF /path/to/somewhere\n',
+    ]
