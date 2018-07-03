a9cc862df7ba99126bd7cfaf8cfb2f78cb944ac8
Christian Heimes <cheimes@redhat.com>
2018-07-03 19:40:05 +0200

    Tune DS replication settings
    Tune 389-DS replication settings to improve performance and avoid
    timeouts. During installation of a replica, the value of
    nsDS5ReplicaBindDnGroupCheckInterval is reduced to 2 seconds. At the end
    of the installation, the value is increased sensible production
    settings. This avoids long delays during replication.
    
    See: https://pagure.io/freeipa/issue/7617
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Tibor Dudlak <tdudlak@redhat.com>

--- ipaserver/install/cainstance.py
+++ ipaserver/install/cainstance.py
@@ -389,8 +389,13 @@ class CAInstance(DogtagInstance):
             self.step("set up CRL publishing", self.__enable_crl_publish)
             self.step("enable PKIX certificate path discovery and validation", self.enable_pkix)
             if promote:
-                self.step("destroying installation admin user", self.teardown_admin)
-            self.step("starting certificate server instance", self.start_instance)
+                self.step("destroying installation admin user",
+                          self.teardown_admin)
+            self.step("starting certificate server instance",
+                      self.start_instance)
+            if promote:
+                self.step("Finalize replication settings",
+                          self.finalize_replica_config)
         # Step 1 of external is getting a CSR so we don't need to do these
         # steps until we get a cert back from the external CA.
         if self.external != 1:
@@ -1197,12 +1202,15 @@ class CAInstance(DogtagInstance):
         api.Backend.ldap2.add_entry(entry)
 
     def __setup_replication(self):
-
         repl = replication.CAReplicationManager(self.realm, self.fqdn)
         repl.setup_cs_replication(self.master_host)
 
         # Activate Topology for o=ipaca segments
         self.__update_topology()
+
+    def finalize_replica_config(self):
+        repl = replication.CAReplicationManager(self.realm, self.fqdn)
+        repl.finalize_replica_config(self.master_host)
 
     def __enable_instance(self):
         basedn = ipautil.realm_to_suffix(self.realm)
