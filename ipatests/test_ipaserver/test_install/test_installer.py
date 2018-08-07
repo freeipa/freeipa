c1ea0a38c01999a98ca15e5fb68a2edbe6076132
Thomas Woerner <twoerner@redhat.com>
2018-08-07 17:51:28 +0200

    ipatests: Drop test_password_option_DL0
    DL0 is not supported anymore therefore this test is failing.
    
    See: https://pagure.io/freeipa/issue/7669
    Signed-off-by: Thomas Woerner <twoerner@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

--- ipatests/test_ipaserver/test_install/test_installer.py
+++ ipatests/test_ipaserver/test_install/test_installer.py
@@ -146,31 +146,3 @@ class TestReplicaInstaller(InstallerTestBase):
             assert_equal(ic.password, self.PASSWORD.value)
             assert_equal(ic.principal, self.PRINCIPAL.value)
             assert_equal(ic.admin_password, self.ADMIN_PASSWORD.value)
-
-    def test_password_option_DL0(self):
-        with tempfile.NamedTemporaryFile() as repl_file:
-            REPLICA_FILE_PATH = repl_file.name
-            # when --password is set in DL0 installation, it sets both
-            # password and dm_password attributes
-            for passwd_opt in self.all_option_permutations(self.PASSWORD):
-                ic = self.get_installer_instance(
-                    self.combine_options(passwd_opt, REPLICA_FILE_PATH)
-                )
-                assert_equal(ic.password, self.PASSWORD.value)
-                assert_equal(ic.dm_password, self.PASSWORD.value)
-                assert_equal(ic.admin_password, None)
-                assert_equal(ic.principal, None)
-
-            # try adding admin_password just to see it is set correctly
-            for passwd_opt, adm_password_opt in (
-                self.all_option_permutations(
-                    self.PASSWORD, self.ADMIN_PASSWORD)
-            ):
-                ic = self.get_installer_instance(
-                    self.combine_options(passwd_opt, adm_password_opt,
-                                         REPLICA_FILE_PATH)
-                )
-                assert_equal(ic.password, self.PASSWORD.value)
-                assert_equal(ic.admin_password, self.ADMIN_PASSWORD.value)
-                assert_equal(ic.dm_password, self.PASSWORD.value)
-                assert_equal(ic.principal, None)
