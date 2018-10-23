da89182b43362e4df498d2b76e7e403ae0727313
Florence Blanc-Renaud <flo@redhat.com>
2018-10-23 09:29:38 +0200

    ipa-replica-install --setup-adtrust: check for package ipa-server-trust-ad
    When adding the option --setup-adtrust to ipa-replica-install,
    we need to check that the package freeipa-server-trust-ad is
    installed.
    To avoid relying on OS-specific commands like yum, the check is instead
    ensuring that the file /usr/share/ipa/smb.conf.empty is present
    (this file is delivered by the package).
    When the check is unsuccessful, ipa-replica-install exits with an error
    message.
    
    Fixes: https://pagure.io/freeipa/issue/7602

--- ipaplatform/base/constants.py
+++ ipaplatform/base/constants.py
@@ -15,6 +15,7 @@ class BaseConstantsNamespace:
     HTTPD_USER = "apache"
     HTTPD_GROUP = "apache"
     GSSPROXY_USER = "root"
+    IPA_ADTRUST_PACKAGE_NAME = "freeipa-server-trust-ad"
     IPA_DNS_PACKAGE_NAME = "freeipa-server-dns"
     KDCPROXY_USER = "kdcproxy"
     NAMED_USER = "named"
