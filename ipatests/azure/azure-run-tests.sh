abac641c563883a29e4134f22dd5689c9bcd3724
Alexander Bokovoy <abbra@users.noreply.github.com>
2019-02-23 22:23:17 +0200

    Set up CI with Azure Pipelines
    Sets up a pipeline to run FreeIPA build and tests in Azure Pipelines.
    
    Azure Pipelines provides 10 parallel free runners for open source projects.
    Use them to run following jobs:
    
     - Build: build RPMs and Fedora 30 container with them
     - Lint: run linting of the source code
     - Tox: run py36,pypi,pylint tests using Tox
     - Web UI unit tests: run Web UI unit tests with Grunt/QUnit/PhantomJS
     - XMLRPC tests: install FreeIPA server and run XMLRPC tests against it
    
    All jobs are running in Fedora 30 containers. Build, Lint, Tox, and Web
    UI unit tests run inside f30/fedora-toolbox container. Build job
    generates a container with pre-installed FreeIPA packages using official
    fedora:30 container. All containers are picked up from
    registry.fedoraproject.org.
    
    Artifacts from the build job are pushed to a pipeline storage and reused
    in the XMLRPC tests. They also are accessible in the 'Summary' tab to
    download.
    
    XUnit and QUnit outputs from the tests that produce it are reported in
    the 'Tests' tab.
    
    Logs from individual steps from each job are available for review in
    the 'Logs' tab. They also can be downloaded.
    
    Reviewed-By: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>

---
+++ ipatests/azure/azure-run-tests.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -ex
+server_realm=EXAMPLE.TEST
+server_domain=example.test
+server_password=Secret123
+
+# Expand list of tests into -k...  -k... -k... .. sequence
+tests_to_run=$(eval "eval echo -k{$(echo $TESTS_TO_RUN | sed -e 's/[ \t]+*/,/g')}")
+
+systemctl --now enable firewalld
+ipa-server-install -U --domain ${server_domain} --realm ${server_realm} -p ${server_password} -a ${server_password} --setup-dns --setup-kra --auto-forwarders
+sed -ri "s/mode = production/mode = development/" /etc/ipa/default.conf
+systemctl restart httpd.service
+firewall-cmd --add-service={freeipa-ldap,freeipa-ldaps,dns}
+
+
+cd /freeipa
+
+echo ${server_password} | kinit admin && ipa ping
+cp -r /etc/ipa/* ~/.ipa/
+echo ${server_password} > ~/.ipa/.dmpw
+echo 'wait_for_dns=5' >> ~/.ipa/default.conf
+ipa-test-config --help
+ipa-test-task --help
+ipa-run-tests --with-xunit -k-{test_integration,test_webui,test_ipapython/test_keyring.py,test_dns_soa} -v ${tests_to_run}
+grep -n -C5 BytesWarning /var/log/httpd/error_log
+ipa-server-install --uninstall -U
+# second uninstall to verify that --uninstall without installation works
+ipa-server-install --uninstall -U
+firewall-cmd --remove-service={freeipa-ldap,freeipa-ldaps,dns}
+
+mkdir -p /freeipa/logs
+cd /freeipa/logs
+journalctl -b --no-pager > systemd_journal.log
+tar --ignore-failed-read -cvf var_log.tar \
+    /var/log/dirsrv \
+    /var/log/httpd \
+    /var/log/ipa* \
+    /var/log/krb5kdc.log \
+    /var/log/pki \
+    systemd_journal.log
+
+ls -laZ /etc/dirsrv/slapd-*/ /etc/httpd/alias/ /etc/pki/pki-tomcat/alias/ || true
+
