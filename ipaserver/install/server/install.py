690f903cd8a60d2fdb329c2f075ffc235e05e148
Florence Blanc-Renaud <flo@redhat.com>
2019-03-06 21:12:49 +0100

    ipa server: prevent uninstallation if the server is CRL master
    If ipa-server-install --uninstall is called on a server that
    is CRL generation master, refuse uninstallation unless
    --ignore-last-of-role is specified or (in interactive mode)
    the admin is OK to force uninstallation.
    
    Related to https://pagure.io/freeipa/issue/5803

--- ipaserver/install/server/install.py
+++ ipaserver/install/server/install.py
@@ -1046,6 +1046,8 @@ def uninstall_check(installer):
     else:
         dns.uninstall_check(options)
 
+        ca.uninstall_check(options)
+
         if domain_level == DOMAIN_LEVEL_0:
             rm = replication.ReplicationManager(
                 realm=api.env.realm,
