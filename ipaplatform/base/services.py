0dad7d798a9dce77d174902871ea2e0130a92d73
Christian Heimes <cheimes@redhat.com>
2019-04-05 13:39:13 +0200

    Use Network Manager to configure resolv.conf
    IPA used to write a custom /etc/resolv.conf. On Fedora and RHEL,
    NetworkManager is typically maintaining resolv.conf. On reboot or
    restart of the service, NM overwrites the custom settings.
    
    On systems with NM enabled, the DNS server installer now drops a config
    file into NM's global config directory and delegates resolv.conf to NM.
    
    On systems without NM, fall back to create /etc/resolv.conf directly.
    
    Fixes: https://pagure.io/freeipa/issue/7900
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- ipaplatform/base/services.py
+++ ipaplatform/base/services.py
@@ -55,7 +55,7 @@ wellknownservices = ['certmonger', 'dirsrv', 'httpd', 'ipa', 'krb5kdc',
                      'rpcbind', 'kadmin', 'sshd', 'autofs', 'rpcgssd',
                      'rpcidmapd', 'pki_tomcatd', 'chronyd', 'domainname',
                      'named', 'ods_enforcerd', 'ods_signerd', 'gssproxy',
-                     'nfs-utils', 'sssd']
+                     'nfs-utils', 'sssd', 'NetworkManager']
 
 # The common ports for these services. This is used to wait for the
 # service to become available.
