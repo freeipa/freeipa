deafde4e5016cf047644d96914171ada2c6fc58a
Christian Heimes <cheimes@redhat.com>
2019-04-23 15:57:28 +0200

    Deprecate ipa-client-install --request-cert
    Mark the --request-cert option for ipa-client-install as deprecated.
    Users are encouraged to request a PEM certificate with certmonger
    instead. The option and /etc/ipa/nssdb will be removed in a future
    version.
    
    Related: https://pagure.io/freeipa/issue/7492
    Signed-off-by: Christian Heimes <cheimes@redhat.com>

--- ipaclient/install/client.py
+++ ipaclient/install/client.py
@@ -3693,6 +3693,7 @@ class ClientInstallInterface(hostname_.HostNameInstallInterface,
 
     request_cert = knob(
         None,
+        deprecated=True,
         description="request certificate for the machine",
     )
     request_cert = prepare_only(request_cert)
@@ -3705,7 +3706,10 @@ class ClientInstallInterface(hostname_.HostNameInstallInterface,
                 "--server cannot be used without providing --domain")
 
         if self.force_ntpd:
-            logger.warning("Option --force-ntpd has been deprecated")
+            logger.warning(
+                "Option --force-ntpd has been deprecated and will be "
+                "removed in a future release."
+            )
 
         if self.ntp_servers and self.no_ntp:
             raise RuntimeError(
@@ -3714,6 +3718,12 @@ class ClientInstallInterface(hostname_.HostNameInstallInterface,
         if self.ntp_pool and self.no_ntp:
             raise RuntimeError(
                 "--ntp-pool cannot be used together with --no-ntp")
+
+        if self.request_cert:
+            logger.warning(
+                "Option --request-cert has been deprecated and will be "
+                "removed in a future release."
+            )
 
         if self.no_nisdomain and self.nisdomain:
             raise RuntimeError(
