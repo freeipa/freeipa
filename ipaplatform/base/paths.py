freeipa/freeipa

commit da2cf1c513ea42dbb38952273421fefdd92d4d0b

Debian: Add paths for open-sans and font-awesome
Debian has different paths and path suffix for font-awesome. Let's have
explicit paths for all our fonts.

Co-authored-by: Timo Aaltonen <tjaalton@debian.org>
Signed-off-by: Christian Heimes <cheimes@redhat.com>
Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
Reviewed-By: Rob Crittenden <rcritten@redhat.com>

ipaplatform/base/paths.py
@@ -241,6 +241,8 @@ class BasePathNamespace:
     SMBD = "/usr/sbin/smbd"
     USERADD = "/usr/sbin/useradd"
     FONTS_DIR = "/usr/share/fonts"
+    FONTS_OPENSANS_DIR = "/usr/share/fonts/open-sans"
+    FONTS_FONTAWESOME_DIR = "/usr/share/fonts/font-awesome"
     USR_SHARE_IPA_DIR = "/usr/share/ipa/"
     USR_SHARE_IPA_CLIENT_DIR = "/usr/share/ipa/client"
     CA_TOPOLOGY_ULDIF = "/usr/share/ipa/ca-topology.uldif"

ipaplatform/debian/paths.py
@@ -85,6 +85,8 @@ class DebianPathNamespace(BasePathNamespace):
     IPA_PKI_WAIT_RUNNING = "/usr/lib/ipa/ipa-pki-wait-running"
     HTTPD = "/usr/sbin/apache2ctl"
     FONTS_DIR = "/usr/share/fonts/truetype"
+    FONTS_OPENSANS_DIR = "/usr/share/fonts/truetype/open-sans"
+    FONTS_FONTAWESOME_DIR = "/usr/share/fonts/truetype/fontawesome"
     VAR_KERBEROS_KRB5KDC_DIR = "/var/lib/krb5kdc/"
     VAR_KRB5KDC_K5_REALM = "/var/lib/krb5kdc/.k5."
     CACERT_PEM = "/var/lib/krb5kdc/cacert.pem"

install/share/ipa.conf.template
@@ -182,8 +182,8 @@ Alias /ipa/crl "$CRL_PUBLISH_PATH"
 
 
 #  List explicitly only the fonts we want to serve
-Alias /ipa/ui/fonts/open-sans "${FONTS_DIR}/open-sans"
-Alias /ipa/ui/fonts/fontawesome "${FONTS_DIR}/fontawesome"
+Alias /ipa/ui/fonts/open-sans "${FONTS_OPENSANS_DIR}"
+Alias /ipa/ui/fonts/fontawesome "${FONTS_FONTAWESOME_DIR}"
 <Directory "${FONTS_DIR}">
   SetHandler None
   AllowOverride None

ipaserver/install/httpinstance.py
@@ -104,6 +104,8 @@ class HTTPInstance(service.Service):
             AUTOREDIR='' if auto_redirect else '#',
             CRL_PUBLISH_PATH=paths.PKI_CA_PUBLISH_DIR,
             FONTS_DIR=paths.FONTS_DIR,
+            FONTS_OPENSANS_DIR=paths.FONTS_OPENSANS_DIR,
+            FONTS_FONTAWESOME_DIR=paths.FONTS_FONTAWESOME_DIR,
             GSSAPI_SESSION_KEY=paths.GSSAPI_SESSION_KEY,
             IPA_CUSTODIA_SOCKET=paths.IPA_CUSTODIA_SOCKET,
             IPA_CCACHES=paths.IPA_CCACHES,

ipaserver/install/server/upgrade.py
@@ -1837,6 +1837,8 @@ def upgrade_configuration():
         WSGI_PROCESSES=constants.WSGI_PROCESSES,
         GSSAPI_SESSION_KEY=paths.GSSAPI_SESSION_KEY,
         FONTS_DIR=paths.FONTS_DIR,
+        FONTS_OPENSANS_DIR=paths.FONTS_OPENSANS_DIR,
+        FONTS_FONTAWESOME_DIR=paths.FONTS_FONTAWESOME_DIR,
         IPA_CCACHES=paths.IPA_CCACHES,
         IPA_CUSTODIA_SOCKET=paths.IPA_CUSTODIA_SOCKET,
         KDCPROXY_CONFIG=paths.KDCPROXY_CONFIG,

commit 486ba017ceab1fb240f2fc48fea6169bc8c97319

Correct default fontawesome path (broken by da2cf1c5)
On Fedora/RHEL, it does not have a dash in it. The changes in
da2cf1c5 inadvertently added a dash to the path in the 'base'
paths definition (used on Fedora/RHEL), so the font wasn't found.

Signed-off-by: Adam Williamson <awilliam@redhat.com>

ipaplatform/base/paths.py
@@ -249,7 +249,7 @@ class BasePathNamespace:
     USERADD = "/usr/sbin/useradd"
     FONTS_DIR = "/usr/share/fonts"
     FONTS_OPENSANS_DIR = "/usr/share/fonts/open-sans"
-    FONTS_FONTAWESOME_DIR = "/usr/share/fonts/font-awesome"
+    FONTS_FONTAWESOME_DIR = "/usr/share/fonts/fontawesome"
     USR_SHARE_IPA_DIR = "/usr/share/ipa/"
     USR_SHARE_IPA_CLIENT_DIR = "/usr/share/ipa/client"
     CA_TOPOLOGY_ULDIF = "/usr/share/ipa/ca-topology.uldif"