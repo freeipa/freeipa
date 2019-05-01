59ec479b095bc212176e4b1d1ddfce2c7b9bf468
Adam Williamson <awilliam@redhat.com>
2019-05-01 16:19:53 -0700

    Correct default fontawesome path (broken by da2cf1c5)
    On Fedora/RHEL, it does not have a dash in it. The changes in
    da2cf1c5 inadvertently added a dash to the path in the 'base'
    paths definition (used on Fedora/RHEL), so the font wasn't found.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

--- ipaplatform/base/paths.py
+++ ipaplatform/base/paths.py
@@ -257,7 +257,7 @@ class BasePathNamespace(object):
     USERADD = "/usr/sbin/useradd"
     FONTS_DIR = "/usr/share/fonts"
     FONTS_OPENSANS_DIR = "/usr/share/fonts/open-sans"
-    FONTS_FONTAWESOME_DIR = "/usr/share/fonts/font-awesome"
+    FONTS_FONTAWESOME_DIR = "/usr/share/fonts/fontawesome"
     USR_SHARE_IPA_DIR = "/usr/share/ipa/"
     USR_SHARE_IPA_CLIENT_DIR = "/usr/share/ipa/client"
     CA_TOPOLOGY_ULDIF = "/usr/share/ipa/ca-topology.uldif"
