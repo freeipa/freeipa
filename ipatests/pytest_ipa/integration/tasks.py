a7a8622d8b94cf8a3584047666f5b3279d5ee0ac
Tibor Dudlák <tdudlak@redhat.com>
2019-07-17 15:46:43 +0200

    ipatests: Add tests for interactive chronyd config
    Add interactive configuration tests for
    ipa-server-install and ipa-client-install
    FreeIPA server as it is now is unable to
    configure NTP interactively for replica
    installations.
    
    Resolves: https://pagure.io/freeipa/issue/7908

--- ipatests/pytest_ipa/integration/tasks.py
+++ ipatests/pytest_ipa/integration/tasks.py
@@ -498,7 +498,7 @@ def install_replica(master, replica, setup_ca=True, setup_dns=False,
     return result
 
 
-def install_client(master, client, extra_args=(), user=None,
+def install_client(master, client, extra_args=[], user=None,
                    password=None, unattended=True, stdin_text=None):
     client.collect_log(paths.IPACLIENT_INSTALL_LOG)
 
@@ -528,7 +528,9 @@ def install_client(master, client, extra_args=(), user=None,
     if unattended:
         args.append('-U')
 
-    result = client.run_command(args + list(extra_args), stdin_text=stdin_text)
+    args.extend(extra_args)
+
+    result = client.run_command(args, stdin_text=stdin_text)
 
     setup_sssd_debugging(client)
     kinit_admin(client)
