b66e8a1ee295e11e286979a0bd9ce303cbab2aa5
Changmin Teng <cteng@redhat.com>
2019-07-29 11:10:31 -0400

    Modify webUI to adhere to new IPA server API
    Given the changes in IPA server API changes, whebUI is modified to
    utilize new authentication indicators, and disabled custom indicators
    for services' white list.
    
    Resolves: https://pagure.io/freeipa/issue/8001
    Signed-off-by: Changmin Teng <cteng@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Simo Sorce <ssorce@redhat.com>
    Reviewed-By: Robbie Harwood <rharwood@redhat.com>

--- install/ui/src/freeipa/serverconfig.js
+++ install/ui/src/freeipa/serverconfig.js
@@ -103,7 +103,9 @@ return {
                                 { label: '@i18n:authtype.type_disabled', value: 'disabled' },
                                 { label: '@i18n:authtype.type_password', value: 'password' },
                                 { label: '@i18n:authtype.type_radius', value: 'radius' },
-                                { label: '@i18n:authtype.type_otp', value: 'otp' }
+                                { label: '@i18n:authtype.type_otp', value: 'otp' },
+                                { label: '@i18n:authtype.type_pkinit', value: 'pkinit' },
+                                { label: '@i18n:authtype.type_hardened', value: 'hardened' }
                             ],
                             tooltip: {
                                 title: '@i18n:authtype.config_tooltip',
