c0b0c6b4b598acd7a867594d91b7f7cff47d2e5e
Serhii Tsymbaliuk <stsymbal@redhat.com>
2019-11-18 12:42:52 +0100

    WebUI: Fix adding member manager for groups and host groups
    - fix API method call for adding member manager
    - fix regressions in host group associated tables
    
    Ticket: https://pagure.io/freeipa/issue/8123
    
    Signed-off-by: Serhii Tsymbaliuk <stsymbal@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

--- install/ui/src/freeipa/hostgroup.js
+++ install/ui/src/freeipa/hostgroup.js
@@ -66,28 +66,28 @@ return {
         {
             $type: 'association',
             name: 'membermanager_group',
-            associator: IPA.serial_associator,
+            add_method: 'add_member_manager',
+            remove_method: 'remove_member_manager',
             add_title: '@i18n:objects.hostgroup.add_membermanager_group',
             remove_title: '@i18n:objects.hostgroup.remove_membermanager_group'
         },
         {
             $type: 'association',
             name: 'membermanager_user',
-            associator: IPA.serial_associator,
+            add_method: 'add_member_manager',
+            remove_method: 'remove_member_manager',
             add_title: '@i18n:objects.hostgroup.add_membermanager_user',
             remove_title: '@i18n:objects.hostgroup.remove_membermanager_user'
         },
         {
             $type: 'association',
             name: 'member_host',
-            associator: IPA.serial_associator,
             add_title: '@i18n:objects.hostgroup.add_hosts',
             remove_title: '@i18n:objects.hostgroup.remove_hosts'
         },
         {
             $type: 'association',
             name: 'member_hostgroup',
-            associator: IPA.serial_associator,
             add_title: '@i18n:objects.hostgroup.add_hostgroups',
             remove_title: '@i18n:objects.hostgroup.remove_hostgroups'
         },
