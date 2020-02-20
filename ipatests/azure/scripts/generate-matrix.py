1fa033c32d4c94ea9456a2a728893e206957930e
Stanislav Levin <slev@altlinux.org>
2020-02-20 11:42:43 +0300

    Azure: Preliminary check for provided limits
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipatests/azure/scripts/generate-matrix.py
+++ ipatests/azure/scripts/generate-matrix.py
@@ -6,15 +6,19 @@ import yaml
 parser = argparse.ArgumentParser(description='Generate Azure jobs matrix.')
 parser.add_argument('azure_template', help='path to Azure template')
 
+parser.add_argument('max_azure_env_jobs', type=int,
+                    help='maximum number of Docker envs within VM')
+
 args = parser.parse_args()
 
 with open(args.azure_template) as f:
     data = yaml.safe_load(f)
     matrix_jobs = {}
     for vm in data['vms']:
+        vm_jobs = vm['vm_jobs']
         jobs = {}
         job_name = ''
-        for job_id, vm_job in enumerate(vm['vm_jobs'], 1):
+        for job_id, vm_job in enumerate(vm_jobs, 1):
             if not job_name:
                 job_name = f'{vm_job["container_job"]}_{job_id}'
             jobs[f'ipa_tests_env_name_{job_id}'] = vm_job['container_job']
@@ -33,7 +37,11 @@ with open(args.azure_template) as f:
             jobs[f'ipa_tests_replicas_{job_id}'] = replicas
             jobs[f'ipa_tests_clients_{job_id}'] = clients
 
-        job_name = f'{job_name}_to_{len(vm["vm_jobs"])}'
+        if len(vm_jobs) > args.max_azure_env_jobs:
+            raise ValueError(
+                f"Number of defined jobs:{len(vm_jobs)} within VM:'{job_name}'"
+                f" is greater than limit:{args.max_azure_env_jobs}")
+        job_name = f'{job_name}_to_{len(vm_jobs)}'
         if job_name in matrix_jobs:
             raise ValueError(f"Environment names should be unique:{job_name}")
         matrix_jobs[job_name] = jobs
