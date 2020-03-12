ee16ad6ad852a95af53c896f89c11fbc97d66a0a
Anuja More <amore@redhat.com>
2020-03-12 18:43:17 +0530

    Mark test to skip sssd-2.2.2
    Test test_ext_grp_with_ldap is marked as skip as
    fix for https://pagure.io/SSSD/sssd/issue/4073
    unavailable with sssd-2.2.2
    
    Related: https://pagure.io/SSSD/sssd/issue/4073
    
    Signed-off-by: Anuja More <amore@redhat.com>

--- ipatests/test_integration/test_sssd.py
+++ ipatests/test_integration/test_sssd.py
@@ -15,6 +15,7 @@ import textwrap
 
 from ipatests.test_integration.base import IntegrationTest
 from ipatests.pytest_ipa.integration import tasks
+from ipaplatform.tasks import tasks as platform_tasks
 from ipaplatform.osinfo import osinfo
 from ipaplatform.paths import paths
 from ipapython.dn import DN
@@ -296,6 +297,12 @@ class TestSSSDWithAdTrust(IntegrationTest):
         in group with same name of nonprivate ipa user and possix id, then
         lookup of aduser and group should be successful when cache is empty.
         """
+        cmd = self.master.run_command(['sssd', '--version'])
+        sssd_version = platform_tasks.parse_ipa_version(
+            cmd.stdout_text.strip())
+        if sssd_version <= platform_tasks.parse_ipa_version('2.2.2'):
+            pytest.skip("Fix for https://pagure.io/SSSD/sssd/issue/4073 "
+                        "unavailable with sssd-2.2.2")
         client = self.clients[0]
         user = 'ipatest'
         userid = '100996'
