f812e2cdbcc514cee0fe70e21f57d57b50059504
Christian Heimes <cheimes@redhat.com>
2020-04-27 14:16:21 +0200

    Fix APIVersion.__getnewargs__
    ``__getnewargs__()`` must return a tuple.
    
    Fixes ``E0312(invalid-getnewargs-returned), APIVersion.__getnewargs__]
     __getnewargs__ does not return a tuple)``.
    
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>

--- ipapython/ipautil.py
+++ ipapython/ipautil.py
@@ -1580,7 +1580,7 @@ class APIVersion(tuple):
         return "<APIVersion('{}.{}')>".format(*self)
 
     def __getnewargs__(self):
-        return str(self)
+        return (str(self),)
 
     @property
     def major(self):
