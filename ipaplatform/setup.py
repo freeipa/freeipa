490682ac3cab8978635655bdcc880f28227e900b
Christian Heimes <cheimes@redhat.com>
2020-05-04 19:51:06 +0200

    Make ipaplatform a regular top-level package
    ipaplatform was made a namespace package so that 3rd party OS
    distributors can easily define their own distribution subpackage. Since
    major distributions have contributed to FreeIPA project and no 3rd party
    ipaplatform subpackage was uploaded to PyPI, it doesn't make much sense
    to keep ipaplatform a namespace package.
    
    The ipaplatform-*-nspkg.pth file for namespace package definition is
    causing trouble with local testing on developer boxes.
    
    Fixes: https://pagure.io/freeipa/issue/8309
    See: https://pagure.io/freeipa/issue/6474
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipaplatform/setup.py
+++ ipaplatform/setup.py
@@ -31,7 +31,6 @@ if __name__ == '__main__':
         name="ipaplatform",
         doc=__doc__,
         package_dir={'ipaplatform': ''},
-        namespace_packages=['ipaplatform'],
         packages=[
             "ipaplatform",
             "ipaplatform.base",
