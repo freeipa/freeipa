freeipa/freeipa

commit a5cbdb57e50cfc62f61affda19ce878b2abd33de

Split named custom config to allow changes in options stanza
Upgrade path to add additional include to named.conf is not handled.

Remove bindkeys-file directive from named config
The ISC DVL service was shut down (https://www.isc.org/bind-keys/).
BIND versions since April 2017 (i.e. 9.9.10, 9.10.5, 9.11.1 and later)
include a hard-coded copy of the root KSK which gets updates automatically
according to RFC 5011.

Move dnssec-enable directive to custom named config

Move comment named config being managed by FreeIPA to the top

Move settings which could be changed by administrators to
ipa-options-ext.conf. Settings defined there are sole responsibility of the
administrator. We do not check if they might collide with our settings in
named.conf.

Fixes: https://pagure.io/freeipa/issue/8287
Co-authored-by: Peter Keresztes Schmidt <carbenium@outlook.com>
Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

ipaserver/install/server/upgrade.py
@@ -594,9 +594,18 @@ def named_add_ipa_ext_conf_file():
         logger.info('DNS is not configured.')
         return False
 
-    return bindinstance.named_add_ext_conf_file(
-        paths.NAMED_CUSTOM_CFG_SRC,
-        paths.NAMED_CUSTOM_CONFIG)
+    tasks = [
+        bindinstance.named_add_ext_conf_file(
+            paths.NAMED_CUSTOM_CFG_SRC,
+            paths.NAMED_CUSTOM_CONFIG
+        ),
+        bindinstance.named_add_ext_conf_file(
+            paths.NAMED_CUSTOM_OPTIONS_CFG_SRC,
+            paths.NAMED_CUSTOM_OPTIONS_CONFIG
+        )
+    ]
+
+    return any(tasks)
 
 
 def named_set_minimum_connections():

freeipa.spec.in
@@ -1221,6 +1221,7 @@ fi
 %{_usr}/share/ipa/*.uldif
 %{_usr}/share/ipa/*.template
 %{_usr}/share/ipa/bind.ipa-ext.conf
+%{_usr}/share/ipa/bind.ipa-options-ext.conf.template
 %dir %{_usr}/share/ipa/advise
 %dir %{_usr}/share/ipa/advise/legacy
 %{_usr}/share/ipa/advise/legacy/*.template
@@ -1266,6 +1267,7 @@ fi
 %ghost %attr(0644,root,root) %config(noreplace) %{_sysconfdir}/ipa/kdcproxy/ipa-kdc-proxy.conf
 %ghost %attr(0644,root,root) %config(noreplace) %{_usr}/share/ipa/html/ca.crt
 %ghost %attr(0640,root,named) %config(noreplace) %{_sysconfdir}/named/ipa-ext.conf
+%ghost %attr(0640,root,named) %config(noreplace) %{_sysconfdir}/named/ipa-options-ext.conf
 %ghost %attr(0644,root,root) %{_usr}/share/ipa/html/krb.con
 %ghost %attr(0644,root,root) %{_usr}/share/ipa/html/krb5.ini
 %ghost %attr(0644,root,root) %{_usr}/share/ipa/html/krbrealm.con

install/share/Makefile.am
@@ -46,6 +46,7 @@ dist_app_DATA =				\
 	kerberos.ldif			\
 	indices.ldif			\
 	bind.ipa-ext.conf		\
+	bind.ipa-options-ext.conf.template	\
 	bind.named.conf.template	\
 	certmap.conf.template		\
 	kdc.conf.template		\

install/share/bind.ipa-ext.conf
@@ -7,9 +7,5 @@
 //   234.234.234.0/24;
 //   2001::co:ffee:babe:1/48;
 // };
-// options {
-//   allow-recursion {trusted_network;};
-//   allow-query-cache {trusted_network;};
-// };
 //
 // This file will NOT be overridden during updates!

install/share/bind.ipa-options-ext.conf.template
@@ -1,0 +1,13 @@
+// Custom managed file.
+// Here you can set your own options included inside the options stanza:
+//
+// allow-recursion { trusted_network; };
+// allow-query-cache { trusted_network; };
+//
+// This file will NOT be overridden during updates!
+
+// turns on IPv6 for port 53, IPv4 is on by default for all ifaces
+listen-on-v6 { any; };
+
+/* dnssec-enable is obsolete and 'yes' by default */
+dnssec-validation $NAMED_DNSSEC_VALIDATION;

install/share/bind.named.conf.template
@@ -1,7 +1,8 @@
-options {
-	// turns on IPv6 for port 53, IPv4 is on by default for all ifaces
-	listen-on-v6 {any;};
+/* WARNING: This part of the config file is IPA-managed.
+ * Modifications may break IPA setup or upgrades.
+ */
 
+options {
 	// Put files that named is allowed to write in the data/ directory:
 	directory "$NAMED_VAR_DIR"; // the default
 	dump-file		"${NAMED_DATA_DIR}cache_dump.db";
@@ -12,14 +13,12 @@ options {
 	// "allow-recursion" are set to "localnets; localhost;".
 	// If either "allow-query-cache" or "allow-recursion" is set,
 	// the other would be set the same value.
-	// Please refer to $CUSTOM_CONFIG
+	// Please refer to $CUSTOM_OPTIONS_CONFIG
 	// for more informations
+	include "$CUSTOM_OPTIONS_CONFIG";
 
 	tkey-gssapi-keytab "$NAMED_KEYTAB";
 	pid-file "$NAMED_PID";
-
-	/* dnssec is enabled by default */
-	dnssec-validation yes;
 
 	managed-keys-directory "$MANAGED_KEYS_DIR";
 
@@ -50,9 +49,6 @@ include "$ROOT_KEY";
 /* custom configuration snippet */
 include "$CUSTOM_CONFIG";
 
-/* WARNING: This part of the config file is IPA-managed.
- * Modifications may break IPA setup or upgrades.
- */
 dyndb "ipa" "$BIND_LDAP_SO" {
 	uri "ldapi://%2fvar%2frun%2fslapd-$SERVER_ID.socket";
 	base "cn=dns, $SUFFIX";

ipaplatform/base/paths.py
@@ -80,7 +80,10 @@ class BasePathNamespace:
     LIBNSS_LDAP_CONF = "/etc/libnss-ldap.conf"
     NAMED_CONF = "/etc/named.conf"
     NAMED_CUSTOM_CONFIG = "/etc/named/ipa-ext.conf"
+    NAMED_CUSTOM_OPTIONS_CONFIG = "/etc/named/ipa-options-ext.conf"
     NAMED_CUSTOM_CFG_SRC = '/usr/share/ipa/bind.ipa-ext.conf'
+    NAMED_CUSTOM_OPTIONS_CFG_SRC = \
+        '/usr/share/ipa/bind.ipa-options-ext.conf.template'
     NAMED_VAR_DIR = "/var/named"
     NAMED_KEYTAB = "/etc/named.keytab"
     NAMED_RFC1912_ZONES = "/etc/named.rfc1912.zones"

ipaplatform/debian/paths.py
@@ -34,6 +34,7 @@ class DebianPathNamespace(BasePathNamespace):
     HTTPD_PASSWORD_CONF = "/etc/apache2/password.conf"
     NAMED_CONF = "/etc/bind/named.conf"
     NAMED_CUSTOM_CONFIG = "/etc/bind/ipa-ext.conf"
+    NAMED_CUSTOM_OPTIONS_CONFIG = "/etc/bind/ipa-options-ext.conf"
     NAMED_VAR_DIR = "/var/cache/bind"
     NAMED_KEYTAB = "/etc/bind/named.keytab"
     NAMED_RFC1912_ZONES = "/etc/bind/named.conf.default-zones"

commit e9e99d1ccd334f23e8df6756a071b658081dde52

Fix named.conf update bug NAMED_DNSSEC_VALIDATION
Commit a5cbdb57e50cfc62f61affda19ce878b2abd33de introduced a bug when
updating IPA from 4.8.6 to 4.8.7. NAMED_DNSSEC_VALIDATION template
variable was not declared.

Fixes: https://pagure.io/freeipa/issue/8363
Signed-off-by: Christian Heimes <cheimes@redhat.com>

ipaserver/install/server/upgrade.py
@@ -594,6 +594,15 @@ def named_add_ipa_ext_conf_file():
         logger.info('DNS is not configured.')
         return False
 
+    # migrate value from named.conf
+    dnssec_validation = bindinstance.named_conf_get_directive(
+        "dnssec-validation",
+        bindinstance.NAMED_SECTION_OPTIONS,
+        str_val=False
+    )
+    if dnssec_validation is None:
+        dnssec_validation = "yes"
+
     tasks = [
         bindinstance.named_add_ext_conf_file(
             paths.NAMED_CUSTOM_CFG_SRC,
@@ -601,7 +610,10 @@ def named_add_ipa_ext_conf_file():
         ),
         bindinstance.named_add_ext_conf_file(
             paths.NAMED_CUSTOM_OPTIONS_CFG_SRC,
-            paths.NAMED_CUSTOM_OPTIONS_CONFIG
+            paths.NAMED_CUSTOM_OPTIONS_CONFIG,
+            dict(
+                NAMED_DNSSEC_VALIDATION=dnssec_validation
+            )
         )
     ]
 

ipatests/prci_definitions/gating.yaml
@@ -286,3 +286,15 @@ jobs:
         template: *ci-master-latest
         timeout: 3600
         topology: *master_1repl_1client
+
+  fedora-latest/test_upgrade:
+    requires: [fedora-latest/build]
+    priority: 100
+    job:
+      class: RunPytest
+      args:
+        build_url: '{fedora-latest/build_url}'
+        test_suite: test_integration/test_upgrade.py
+        template: *ci-master-latest
+        timeout: 3600
+        topology: *master_1repl

ipatests/test_integration/test_upgrade.py
@@ -8,6 +8,8 @@ Module provides tests to verify that the upgrade script works.
 
 import base64
 from cryptography.hazmat.primitives import serialization
+
+from ipaplatform.paths import paths
 from ipapython.dn import DN
 from ipatests.test_integration.base import IntegrationTest
 from ipatests.pytest_ipa.integration import tasks
@@ -65,3 +67,20 @@ class TestUpgrade(IntegrationTest):
         except ValueError:
             raise AssertionError('%s contains a double-encoded cert'
                                  % entry.dn)
+
+    def test_update_named_conf(self):
+        tasks.install_dns(self.master)
+        # remove files to force a migration
+        self.master.run_command(
+            [
+                "rm",
+                "-f",
+                paths.NAMED_CUSTOM_CONFIG,
+                paths.NAMED_CUSTOM_OPTIONS_CONFIG,
+            ]
+        )
+        self.master.run_command(['ipa-server-upgrade'])
+        txt = self.master.get_file_contents(
+            paths.NAMED_CUSTOM_OPTIONS_CONFIG, encoding="utf-8"
+        )
+        assert "dnssec-validation yes;" in txt