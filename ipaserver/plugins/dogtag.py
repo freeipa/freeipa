9ded9e2573a00c388533f2a09365c499a4e2961e
Alexander Scheel <ascheel@redhat.com>
2020-06-19 08:48:56 -0400

    Specify cert_paths when calling PKIConnection
    PKIConnection now defaults to specifying verify=True. We've introduced
    a new parameter, cert_paths, to specify additional paths (directories or
    files) to load as certificates. Specify the IPA CA certificate file so
    we can guarantee connections succeed and validate the peer's certificate.
    
    Point to IPA CA certificate during pkispawn
    
    Bump pki_version to 10.9.0-0.4 (aka -b2)
    
    Fixes: https://pagure.io/freeipa/issue/8379
    Fixes: https://bugzilla.redhat.com/show_bug.cgi?id=1849155
    Related: https://github.com/dogtagpki/pki/pull/443
    Related: https://bugzilla.redhat.com/show_bug.cgi?id=1426572
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Christian Heimes <cheimes@redhat.com>

--- ipaserver/plugins/dogtag.py
+++ ipaserver/plugins/dogtag.py
@@ -2082,13 +2082,12 @@ class kra(Backend):
             'https',
             self.kra_host,
             str(self.kra_port),
-            'kra')
+            'kra',
+            cert_paths=paths.IPA_CA_CRT
+        )
 
-        connection.session.cert = (paths.RA_AGENT_PEM, paths.RA_AGENT_KEY)
-        # uncomment the following when this commit makes it to release
-        # https://git.fedorahosted.org/cgit/pki.git/commit/?id=71ae20c
-        # connection.set_authentication_cert(paths.RA_AGENT_PEM,
-        #                                    paths.RA_AGENT_KEY)
+        connection.set_authentication_cert(paths.RA_AGENT_PEM,
+                                           paths.RA_AGENT_KEY)
 
         try:
             yield KRAClient(connection, crypto)
