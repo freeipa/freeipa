8f6502db03576c92b25f4460fb98d75e0fe68f28
Christian Heimes <cheimes@redhat.com>
2020-07-28 14:04:34 +0200

    Convert ipa-httpd-pwdreader into Python script
    and use paths from ipaplatform.
    
    Fixes: https://pagure.io/freeipa/issue/8401
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Francois Cami <fcami@redhat.com>

--- ipaplatform/rhel_container/paths.py
+++ ipaplatform/rhel_container/paths.py
@@ -24,6 +24,7 @@ class RHELContainerPathNamespace(RHELPathNamespace):
     PKI_CONFIGURATION = data(RHELPathNamespace.PKI_CONFIGURATION)
     SAMBA_DIR = data(RHELPathNamespace.SAMBA_DIR)
     HTTPD_IPA_WSGI_MODULES_CONF = None
+    HTTPD_PASSWD_FILE_FMT = data(RHELPathNamespace.HTTPD_PASSWD_FILE_FMT)
 
 
 paths = RHELContainerPathNamespace()
