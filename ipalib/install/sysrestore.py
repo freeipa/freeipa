6bab1d06dbbb4dfdbed7e72fa30a5d54ac7fa0b2
Rob Crittenden <rcritten@redhat.com>
2020-08-06 11:41:14 -0400

    Address legacy pylint issues in sysrestore.py
    These were triggered because of the movement of sysrestore.py in
    the tree
    
    https://pagure.io/freeipa/issue/8384

--- ipalib/install/sysrestore.py
+++ ipalib/install/sysrestore.py
@@ -8,10 +8,12 @@ Facade for ipalib.sysrestore for backwards compatibility
 
 from ipalib import sysrestore as real_sysrestore
 
+
 class FileStore(real_sysrestore.FileStore):
     def __init__(self, path=real_sysrestore.SYSRESTORE_PATH,
                  index_file=real_sysrestore.SYSRESTORE_INDEXFILE):
         super(FileStore, self).__init__(path, index_file)
+
 
 class StateFile(real_sysrestore.StateFile):
     def __init__(self, path=real_sysrestore.SYSRESTORE_PATH,
