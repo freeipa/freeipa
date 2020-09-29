1684b0f2c3de8f6dd3d15945c87bfe7e887f966c
Christian Heimes <cheimes@redhat.com>
2020-09-29 10:30:42 +0200

    Add missing fedora_container platform members
    The fedora_container platform was missing User and Group members.
    
    Add test case to verify that all known platforms define correct module
    API.
    
    Fixes: https://pagure.io/freeipa/issue/8519
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipaplatform/fedora_container/constants.py
+++ ipaplatform/fedora_container/constants.py
@@ -3,7 +3,10 @@
 #
 """Fedora container constants
 """
-from ipaplatform.fedora.constants import FedoraConstantsNamespace
+from ipaplatform.fedora.constants import FedoraConstantsNamespace, User, Group
+
+
+__all__ = ("constants", "User", "Group")
 
 
 class FedoraContainerConstantsNamespace(FedoraConstantsNamespace):
