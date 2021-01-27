8082a2d9eb96c4d5471b72e6a008cd273ec743cd
Rob Crittenden <rcritten@redhat.com>
2021-01-27 16:35:22 -0500

    ipatests: Handle non-zero return code in test_ipactl_scenario_check
    https://pagure.io/freeipa/issue/8550
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>
    Reviewed-By: Florence Blanc-Renaud <flo@redhat.com>

--- ipatests/test_integration/test_installation.py
+++ ipatests/test_integration/test_installation.py
@@ -766,7 +766,8 @@ class TestInstallMaster(IntegrationTest):
         service_start = [
             svcs for svcs in ipa_services_name if svcs not in service_stop
         ]
-        cmd = self.master.run_command(['ipactl', 'status'])
+        cmd = self.master.run_command(['ipactl', 'status'], raiseonerr=False)
+        assert cmd.returncode == 3
         for service in service_start:
             assert f"{service} Service: RUNNING" in cmd.stdout_text
         for service in service_stop:
