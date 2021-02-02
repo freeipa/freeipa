adc4d8d7f674d159ea8f17395013859474af3290
Stanislav Levin <slev@altlinux.org>
2021-02-02 17:15:17 +0300

    Azure: Show disk usage
    Collect disk usage information may be helpful, for example, for
    debugging code required free space such as healthcheck tests.
    
    Signed-off-by: Stanislav Levin <slev@altlinux.org>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipatests/azure/scripts/azure-run-base-tests.sh
+++ ipatests/azure/scripts/azure-run-base-tests.sh
@@ -101,14 +101,5 @@ if [ "$install_result" -eq 0 ] ; then
     firewalld_cmd --remove-service={freeipa-ldap,freeipa-ldaps,dns}
 fi
 
-echo "Report memory statistics"
-cat /sys/fs/cgroup/memory/memory.memsw.failcnt
-cat /sys/fs/cgroup/memory/memory.memsw.limit_in_bytes
-cat /sys/fs/cgroup/memory/memory.memsw.max_usage_in_bytes
-cat /sys/fs/cgroup/memory/memory.failcnt
-cat /sys/fs/cgroup/memory/memory.max_usage_in_bytes
-cat /sys/fs/cgroup/memory/memory.limit_in_bytes
-cat /proc/sys/vm/swappiness
-
 # Final result depends on the exit code of the ipa-run-tests
 test "$tests_result" -eq 0 -a "$install_result" -eq 0
