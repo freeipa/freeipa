798c29d53c2ad92da60ec259db50e2e15aa0d1b2
Stanislav Levin <slev@altlinux.org>
2021-05-19 18:47:16 +0300

    azure: Collect installed packages
    The list of installed packages may be useful for checking the
    versions of packages for analysis. Previously, only the newly
    installed packages can be observed on Build phase.
    
    This is convenient for experienced users of PR-CI.
    
    Note: the read-only access provided for non-master containers
    to be able to execute Azure scripts. The logs are still collected
    only on controller.
    
    Only RPM-based collection is implemented for Fedora. By default
    nothing is collected.
    
    Users may want to override `installed_packages` function
    in the corresponding `ipatests/azure/scripts/variables-DISTRO.sh`.
    
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>

--- ipatests/azure/scripts/variables.sh
+++ ipatests/azure/scripts/variables.sh
@@ -12,5 +12,7 @@ BIND_LOGGING_OPTIONS_CONF='/etc/named/ipa-logging-ext.conf'
 
 function firewalld_cmd() { :; }
 
+function installed_packages() { :; }
+
 # this should be the last to override base variables with platform specific
 source "$IPA_TESTS_SCRIPTS/variables-${IPA_PLATFORM}.sh"
