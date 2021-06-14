470bb6ed45b9b1b2391c2e12a11be7769807edcd
Christian Heimes <cheimes@redhat.com>
2021-06-14 11:45:40 +0200

    Add Custodia tests
    Signed-off-by: Christian Heimes <cheimes@redhat.com>
    Reviewed-By: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>

---
+++ ipatests/test_custodia/test_plugins.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2016  Custodia Project Contributors - see LICENSE file
+import pkg_resources
+import pytest
+
+from ipaserver.custodia.client import CustodiaHTTPClient
+from ipaserver.custodia.plugin import (
+    CSStore, HTTPAuthenticator, HTTPAuthorizer
+)
+
+
+class TestCustodiaPlugins:
+    project_name = 'ipaserver.custodia'
+
+    def get_entry_points(self, group):
+        eps = []
+        for e in pkg_resources.iter_entry_points(group):
+            if e.dist.project_name != self.project_name:
+                # only interested in our own entry points
+                continue
+            eps.append(e)
+        return eps
+
+    def assert_ep(self, ep, basecls):
+        try:
+            # backwards compatibility with old setuptools
+            if hasattr(ep, "resolve"):
+                cls = ep.resolve()
+            else:
+                cls = ep.load(require=False)
+        except Exception as e:  # pylint: disable=broad-except
+            pytest.fail("Failed to load %r: %r" % (ep, e))
+        if not issubclass(cls, basecls):
+            pytest.fail("%r is not a subclass of %r" % (cls, basecls))
+
+    def test_authenticators(self):
+        for ep in self.get_entry_points('custodia.authenticators'):
+            self.assert_ep(ep, HTTPAuthenticator)
+
+    def test_authorizers(self):
+        for ep in self.get_entry_points('custodia.authorizers'):
+            self.assert_ep(ep, HTTPAuthorizer)
+
+    def test_clients(self):
+        for ep in self.get_entry_points('custodia.clients'):
+            self.assert_ep(ep, CustodiaHTTPClient)
+
+    def test_stores(self):
+        for ep in self.get_entry_points('custodia.stores'):
+            self.assert_ep(ep, CSStore)
---
+++ ipatests/test_custodia/test_server.py
@@ -0,0 +1,99 @@
+# Copyright (C) 2017  Custodia Project Contributors - see LICENSE file
+import os
+import socket
+
+import pytest
+
+from ipaserver.custodia.server.args import parse_args
+from ipaserver.custodia.server.config import parse_config
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+EMPTY_CONF = os.path.join(HERE, 'empty.conf')
+
+# pylint: disable=redefined-outer-name
+
+
+@pytest.fixture()
+def args():
+    return parse_args([EMPTY_CONF])
+
+
+@pytest.fixture()
+def args_instance():
+    return parse_args(['--instance=testing', '--debug', EMPTY_CONF])
+
+
+def test_args(args):
+    assert not args.debug
+    assert args.instance is None
+    assert args.configfile.name == EMPTY_CONF
+
+
+def test_args_instance(args_instance):
+    assert args_instance.debug
+    assert args_instance.instance == 'testing'
+    assert args_instance.configfile.name == EMPTY_CONF
+
+
+def test_parse_config(args):
+    parser, config = parse_config(args)
+
+    assert parser.has_section(u'/')
+    assert parser.get(u'/', u'handler') == u'Root'
+
+    assert config == {
+        'auditlog': u'/var/log/custodia/audit.log',
+        'authenticators': {},
+        'authorizers': {},
+        'confdpattern': EMPTY_CONF + u'.d/*.conf',
+        'configdir': HERE,
+        'configfiles': [
+            EMPTY_CONF,
+            EMPTY_CONF + u'.d/root.conf'
+        ],
+        'consumers': {},
+        'debug': False,
+        'hostname': socket.gethostname(),
+        'instance': u'',
+        'libdir': u'/var/lib/custodia',
+        'logdir': u'/var/log/custodia',
+        'makedirs': False,
+        'rundir': u'/var/run/custodia',
+        'server_url': 'http+unix://%2Fvar%2Frun%2Fcustodia%2Fcustodia.sock/',
+        'socketdir': u'/var/run/custodia',
+        'stores': {},
+        'tls_verify_client': False,
+        'umask': 23
+    }
+
+
+def test_parse_config_instance(args_instance):
+    parser, config = parse_config(args_instance)
+
+    assert parser.has_section(u'/')
+    assert parser.get(u'/', u'handler') == u'Root'
+
+    assert config == {
+        'auditlog': u'/var/log/custodia/testing/audit.log',
+        'authenticators': {},
+        'authorizers': {},
+        'confdpattern': EMPTY_CONF + u'.d/*.conf',
+        'configdir': HERE,
+        'configfiles': [
+            EMPTY_CONF,
+            EMPTY_CONF + u'.d/root.conf'
+        ],
+        'consumers': {},
+        'debug': True,
+        'hostname': socket.gethostname(),
+        'instance': u'testing',
+        'libdir': u'/var/lib/custodia/testing',
+        'logdir': u'/var/log/custodia/testing',
+        'makedirs': False,
+        'rundir': u'/var/run/custodia/testing',
+        'server_url': 'http+unix://%2Fvar%2Frun%2Fcustodia%2Ftesting.sock/',
+        'socketdir': u'/var/run/custodia',
+        'stores': {},
+        'tls_verify_client': False,
+        'umask': 23
+    }
