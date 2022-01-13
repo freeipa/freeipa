ace0bbfdc8eb02a4ba47f8293809ff4734856ab8
Alexander Bokovoy <abokovoy@redhat.com>
2022-01-13 14:34:55 +0200

    ipa-kdb: refactor KDB driver to prepare for KDB version 9
    MIT Kerberos 1.20 changes DAL interface around PAC record issuance:
    sign_authdata callback is removed and replaced with issue_pac one.
    The signatures are different and logic changed as well.
    
    Prepare for KDB version 9 by moving PAC implementation into separate
    source files. ipa_kdb_mspac.c is left with most of the common code.
    
    FreeIPA supports sign_authdata callback since KDB version 6, move current
    implementation to ipa_kdb_mspac_v6.c.
    
    KDB version 8 actually changed sign_authdata interface and we accounted
    to that in ipa_kdb.c with a stub that re-uses v6 version. Keep it as it
    is right now.
    
    Finally, add KDB version 9 stub files. Compiling against MIT Kerberos
    1.20 does not work yet, thus explicit #error message in ipa_kdb.c. This
    will be worked on later.
    
    Related: https://pagure.io/freeipa/issue/9083
    
    Signed-off-by: Alexander Bokovoy <abokovoy@redhat.com>
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>

---
+++ daemons/ipa-kdb/ipa_kdb_mspac_v9.c
@@ -0,0 +1,34 @@
+/*
+ * MIT Kerberos KDC database backend for FreeIPA
+ *
+ * Authors: Simo Sorce <ssorce@redhat.com>
+ *
+ * Copyright (C) 2011  Simo Sorce, Red Hat
+ * see file 'COPYING' for use and warranty information
+ *
+ * This program is free software you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "ipa_hostname.h"
+#include "ipa_kdb.h"
+#include <talloc.h>
+#include <unicase.h>
+#include "util/time.h"
+#include "gen_ndr/ndr_krb5pac.h"
+
+#include "ipa_kdb_mspac_private.h"
+
+
