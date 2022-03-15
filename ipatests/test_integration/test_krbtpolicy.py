28cc1815bc0002bdce82ddeff750b8fb14fc5579
Alexander Bokovoy <abokovoy@redhat.com>
2022-03-15 11:39:46 +0200

    test_krbtpolicy: skip SPAKE-related tests in FIPS mode
    SPAKE is based on the crypto primitives which are not FIPS compliant
    yet. This means that in FIPS mode use of 'hardened' authentication
    indicator is not possible. Skip corresponding tests in FIPS mode.
    
    Related: https://pagure.io/freeipa/issue/9119
    
    Signed-off-by: Alexander Bokovoy <abokovoy@redhat.com>

--- ipatests/test_integration/test_krbtpolicy.py
+++ ipatests/test_integration/test_krbtpolicy.py
@@ -105,6 +105,9 @@ class TestPWPolicy(IntegrationTest):
 
     def test_krbtpolicy_password_and_hardended(self):
         """Test a pwd and hardened kerberos ticket policy with 10min tickets"""
+        if self.master.is_fips_mode:
+            pytest.skip("SPAKE pre-auth is not compatible with FIPS mode")
+
         master = self.master
         master.run_command(['ipa', 'user-mod', USER1,
                             '--user-auth-type', 'password',
@@ -133,6 +136,9 @@ class TestPWPolicy(IntegrationTest):
 
     def test_krbtpolicy_hardended(self):
         """Test a hardened kerberos ticket policy with 30min tickets"""
+        if self.master.is_fips_mode:
+            pytest.skip("SPAKE pre-auth is not compatible with FIPS mode")
+
         master = self.master
         master.run_command(['ipa', 'user-mod', USER1,
                             '--user-auth-type', 'hardened'])
