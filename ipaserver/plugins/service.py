02a3d319abf6e960d96cf3a637dab5f3ed96d876
Florence Blanc-Renaud <flo@redhat.com>
2022-10-20 09:01:16 +0200

    CLI: add support for passkey authentication type
    Add a new authentication type for Passkey in the following commands:
    
    ipa user-add --user-auth-type=AUTHTYPE
    ipa user-mod --user-auth-type=AUTHTYPE
    ipa config-mod --user-auth-type=AUTHTYPE
    ipa service-add --auth-ind=AUTHTYPE
    ipa service-mod --auth-ind=AUTHTYPE
    ipa host-add --auth-ind=AUTHTYPE
    ipa host-mod --auth-ind=AUTHTYPE
    ipa krbtpolicy-mod --passkey-maxlife=INT --passkey-maxrenew=INT
    
    Fixes: https://pagure.io/freeipa/issue/9262
    Signed-off-by: Florence Blanc-Renaud <flo@redhat.com>

--- ipaserver/plugins/service.py
+++ ipaserver/plugins/service.py
@@ -614,9 +614,11 @@ class service(LDAPObject):
                   " Use 'idp' to allow authentication against an external"
                   " Identity Provider supporting OAuth 2.0 Device"
                   " Authorization Flow (RFC 8628)."
+                  " Use 'passkey' to allow passkey-based 2FA authentications."
                   " With no indicator specified,"
                   " all authentication mechanisms are allowed."),
-            values=(u'radius', u'otp', u'pkinit', u'hardened', u'idp'),
+            values=(u'radius', u'otp', u'pkinit', u'hardened', u'idp',
+                    u'passkey'),
         ),
     ) + ticket_flags_params
 
