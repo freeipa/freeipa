05b4f9dd3be126b6c9de73bde79af1aa962d9ddf
Rob Crittenden <rcritten@redhat.com>
2023-09-12 18:30:05 +0000

    Don't assume KRB5CCNAME is in the environment in replica install
    The replica install was unilaterally removing KRB5CCNAME from
    os.environ in some cases. Instead check first to see if it is
    present and only remove in that case.
    
    Fixes: https://pagure.io/freeipa/issue/9446
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- ipaserver/install/server/replicainstall.py
+++ ipaserver/install/server/replicainstall.py
@@ -988,7 +988,7 @@ def promote_check(installer):
                 raise errors.ACIError(info="Not authorized")
 
             if installer._ccache is None:
-                del os.environ['KRB5CCNAME']
+                os.environ.pop('KRB5CCNAME', None)
             else:
                 os.environ['KRB5CCNAME'] = installer._ccache
 
@@ -1197,7 +1197,7 @@ def promote_check(installer):
         if add_to_ipaservers:
             # use user's credentials when the server host is not ipaservers
             if installer._ccache is None:
-                del os.environ['KRB5CCNAME']
+                os.environ.pop('KRB5CCNAME', None)
             else:
                 os.environ['KRB5CCNAME'] = installer._ccache
 
