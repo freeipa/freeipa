9b6a51ff0d8c664d7887841a3cc648c8e80a6e9f
Antonio Torres <antorres@redhat.com>
2024-04-29 11:36:52 +0200

    Add DNS over TLS support
    Add DNS over TLS support using Unbound as a local resolver. This
    includes new options on both server and client side.
    
    * `--dns-over-tls`: enable DNS over TLS support. This option is present
      on both client and server. It deploys Unbound and configures BIND on
    the server to receive DoT requests.
    * `--dot-forwarder`: the upstream DNS server with DoT support. It must
      be specified in the format `1.2.3.4#dns.server.test`
    * `--dns-over-tls-key` and `--dns-over-tls-cert`: in case user prefers
      to have the DoT certificate in BIND generated by themselves. If these
      are empty, IPA CA is used instead to request a new certificate.
    
    Signed-off-by: Antonio Torres <antorres@redhat.com>

--- ipaplatform/base/services.py
+++ ipaplatform/base/services.py
@@ -53,7 +53,7 @@ wellknownservices = [
     'named', 'ods_enforcerd', 'ods_signerd', 'gssproxy',
     'nfs-utils', 'sssd', 'NetworkManager', 'ipa-custodia',
     'ipa-dnskeysyncd', 'ipa-otpd', 'ipa-ods-exporter',
-    'systemd-resolved',
+    'systemd-resolved', 'unbound',
 ]
 
 # The common ports for these services. This is used to wait for the
