b9ccb49ffa90b30951540d70dea31e14ce5a4eff
Julien Rische <jrische@redhat.com>
2025-03-12 13:50:46 +0100

    Add test for master key upgrade
    Since commit 4ed7da378940198cf4415f86d4eb013de6ac6455 in MIT krb5,
    kdb5_util sets the required bits on the modification mask of updated
    principal entries to enable ipa-kdb to switch the active master key.
    
    This commit creates a "test_fedora_legacy" ipaplatform where the AES
    HMAC-SHA2 encryption types are not enabled. "test_mkey_upgrade" uses
    this platform to initialize a domain with an aes256-cts-hmac-sha1-96
    master key, and test its upgrade to aes256-cts-hmac-sha384-192.
    
    Some parts of the test infrastructure had to be made aware of this new
    platform (e.g. firewall integration).
    
    Reviewed-By: Rob Crittenden <rcritten@redhat.com>
    (cherry picked from commit fb36633e69e76eabcdd32195a1d3ad08604ab199)

--- ipaplatform/setup.py
+++ ipaplatform/setup.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
             "ipaplatform.rhel",
             "ipaplatform.rhel_container",
             "ipaplatform.suse",
+            "ipaplatform.test_fedora_legacy",
             "ipaplatform.opencloudos",
             "ipaplatform.tencentos"
         ],
