1872dd19c0e3e839eb9c2777f46a42d39954f217
Rob Crittenden <rcritten@redhat.com>
2025-05-28 11:23:06 -0400

    Add token options to immutables for pki override
    Setting the HSM options in a pki-override-file will
    result in a successful installation but it affects the
    storage of the token library path. It is stored as None.
    
    We could also instead save the provided value from the
    override but an override isn't necessary in this case
    since IPA provides command-line options to enable it.
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- ipatests/test_integration/test_pki_config_override.py
+++ ipatests/test_integration/test_pki_config_override.py
@@ -20,6 +20,25 @@ ipa_ca_key_algorithm=SHA512withRSA
 ipa_ca_signing_algorithm=SHA512withRSA
 """
 
+HSM_OVERRIDE = """
+[CA]
+pki_hsm_enable = True
+pki_hsm_libfile = /usr/lib64/pkcs11/libsofthsm2.so
+pki_hsm_modulename = libsofthsm2
+pki_token_name = ipa_token
+pki_token_password = Secret123
+"""
+
+
+def server_install_teardown(func):
+    def wrapped(*args):
+        master = args[0].master
+        try:
+            func(*args)
+        finally:
+            tasks.uninstall_master(master)
+    return wrapped
+
 
 class TestPKIConfigOverride(IntegrationTest):
     @classmethod
@@ -40,3 +59,29 @@ class TestPKIConfigOverride(IntegrationTest):
         cert = load_pem_x509_certificate(ca_pem)
         assert cert.public_key().key_size == 4096
         assert cert.signature_hash_algorithm.name == hashes.SHA512.name
+
+
+class TestPKIHSMConfigOverride(IntegrationTest):
+    num_replicas = 0
+
+    @classmethod
+    def install(cls, mh):
+        pass
+
+    @server_install_teardown
+    def test_immutable_options(self):
+        """Providing the immunible HSM options should fail"""
+        pki_ini = tasks.upload_temp_contents(self.master, HSM_OVERRIDE)
+        extra_args = [
+            '--pki-config-override', pki_ini,
+        ]
+        result = tasks.install_master(
+            self.master, setup_dns=False, extra_args=extra_args,
+            raiseonerr=False
+        )
+        self.master.run_command(['rm', '-f', pki_ini])
+        tasks.assert_error(
+            result,
+            'immutable options: pki_hsm_enable, pki_hsm_libfile, '
+            'pki_hsm_modulename, pki_token_name, pki_token_password'
+        )
