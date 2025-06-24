bd2e3e4d57c1085a7cfa95232366c1b42061bed5
Antonio Torres <antorres@redhat.com>
2025-06-24 13:41:17 +0200

    dns: disable all previous Unbound configuration before deploying ours
    Previous configuration from another packages might break our Unbound
    setup. Rename the config files to disable them before deploying our
    configuration.
    
    Fixes: https://pagure.io/freeipa/issue/9814
    Signed-off-by: Antonio Torres <antorres@redhat.com>

--- ipaserver/install/dns.py
+++ ipaserver/install/dns.py
@@ -136,6 +136,16 @@ def _setup_dns_over_tls(options):
     # module_config_iterator is commented out if DNSSEC validation is
     # not disabled.
     module_config_iterator = '' if options.no_dnssec_validation else '# '
+
+    # disable all previous Unbound configuration
+    for filename in os.listdir(paths.UNBOUND_CONFIG_DIR):
+        if (os.path.join(paths.UNBOUND_CONFIG_DIR, filename)
+                == paths.UNBOUND_CONF):
+            continue
+        os.rename(os.path.join(paths.UNBOUND_CONFIG_DIR, filename),
+                  os.path.join(paths.UNBOUND_CONFIG_DIR,
+                               filename + ".disabled"))
+
     ipautil.copy_template_file(
         paths.UNBOUND_CONF_SRC,
         paths.UNBOUND_CONF,
