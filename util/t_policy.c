18c7bcc302d5cfbf9f1d592e50a908c8d5bd85fc
Rob Crittenden <rcritten@redhat.com>
2025-06-24 16:47:51 -0400

    Add support for libpwpolicy credit to password policy
    We already have libpwquality plumbed in. The ask here was
    to support the quality credits as well.
    
    There are four credits: digits, uppers, lowers and others
    
    These either count complexity towards satisfying the minimum
    length or set the minimum number of a type allowed.
    
    If any of the four credits is configured then it will cause
    the password policy plugin to go through the libpwquality.
    
    Reminder that libpwquality sets its own min password length
    floor that should be overridden already.
    
    If min_classes is set as well then that is still enforced.
    
    Fixes: https://pagure.io/freeipa/issue/9835
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>
    Reviewed-By: David Hanina <dhanina@redhat.com>

--- util/t_policy.c
+++ util/t_policy.c
@@ -16,7 +16,8 @@ static void
 set_policy(struct ipapwd_policy *policy,
                int min_pwd_length, int min_diff_chars, int max_repeat,
                int max_sequence, int max_class_repeat, int dict_check,
-               int user_check)
+               int user_check, int dcredit, int ucredit, int lcredit,
+               int ocredit)
                
 {
     /* defaults for things we aren't testing */
@@ -32,6 +33,10 @@ set_policy(struct ipapwd_policy *policy,
     policy->max_classrepeat = max_class_repeat;
     policy->dictcheck = dict_check;
     policy->usercheck = user_check;
+    policy->dcredit = dcredit;
+    policy->ucredit = ucredit;
+    policy->lcredit = lcredit;
+    policy->ocredit = ocredit;
 }
 
 int main(int argc, const char *argv[]) {
@@ -41,7 +46,7 @@ int main(int argc, const char *argv[]) {
     struct ipapwd_policy policy = {0}; 
 
     /* No policy applied */
-    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
     assert(ipapwd_check_policy(&policy, "password", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
     assert(ipapwd_check_policy(&policy, "abcddcba", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
@@ -50,7 +55,7 @@ int main(int argc, const char *argv[]) {
     assert(ipapwd_check_policy(&policy, "abc", NULL, 3, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
 
     /* Max repeats of 1 */
-    set_policy(&policy, 0, 0, 1, 0, 0, 0, 0);
+    set_policy(&policy, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "password", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_CONSECUTIVE);
     assert(ipapwd_check_policy(&policy, "Assembly", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_CONSECUTIVE);
 
@@ -58,37 +63,125 @@ int main(int argc, const char *argv[]) {
     assert(ipapwd_check_policy(&policy, "abc", NULL, 3, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_TOO_SHORT);
 
     /* Max repeats of 2 */
-    set_policy(&policy, 0, 0, 2, 0, 0, 0, 0);
+    set_policy(&policy, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "password", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
     assert(ipapwd_check_policy(&policy, "Assembly", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
     assert(ipapwd_check_policy(&policy, "permisssive", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_CONSECUTIVE);
 
     /* Max sequence of 1 */
-    set_policy(&policy, 0, 0, 0, 1, 0, 0, 0);
+    set_policy(&policy, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "abacab", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_SEQUENCE);
     assert(ipapwd_check_policy(&policy, "AbacAb", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_SEQUENCE);
 
     /* Max sequence of 2 */
-    set_policy(&policy, 0, 0, 0, 2, 0, 0, 0);
+    set_policy(&policy, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "AbacAb", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
     assert(ipapwd_check_policy(&policy, "abacabc", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_SEQUENCE);
 
     /* Palindrone */
-    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0);  /* Note there is no policy */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);  /* Note there is no policy */
     assert(ipapwd_check_policy(&policy, "password", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
     assert(ipapwd_check_policy(&policy, "abccba", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
-    set_policy(&policy, 0, 0, 3, 0, 0, 0, 0);  /* Set anything */
+    set_policy(&policy, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0);  /* Set anything */
     assert(ipapwd_check_policy(&policy, "abccba", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_PALINDROME);
 
     /* Dictionary check */
-    set_policy(&policy, 0, 0, 0, 0, 0, 1, 0);
+    set_policy(&policy, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "password", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_DICT_WORD);
     assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_DICT_WORD);
 
     /* User check */
     assert(ipapwd_check_policy(&policy, "userPDQ123", "user", 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
-    set_policy(&policy, 0, 0, 0, 0, 0, 0, 1);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0);
     assert(ipapwd_check_policy(&policy, "userPDQ123", "user", 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_USER);
+
+    /* Digit check.
+     * Negative == minimum # of digits required
+     * Zero == skip check
+     * Positive == amount to add towards min length
+     */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, -19, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_MIN_DIGITS);
+
+    /* dcredit > 0 gives a "credit" to a shorter password for having
+     * complexity so allows a shorter than minimum length password.
+     * Reminder that with libpwquality the minimum password len is 
+     * hardcoded at 6.
+     */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "abcd1", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "ab21", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+
+    /* Verify that no credits are added automatically. We need to set some
+     * pwquality option in order to validate it, so set length.
+     */
+    set_policy(&policy, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "abcd1", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_TOO_SHORT);
+
+    /* Upper check.
+     * Negative == minimum # of uppers required
+     * Zero == skip check
+     * Positive == amount to add towards min length
+     */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0);
+    assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, -19, 0, 0);
+    assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_MIN_UPPERS);
+
+    /* ucredit > 0 gives a "credit" to a shorter password for having
+     * complexity so allows a shorter than minimum length password.
+     * Reminder that with libpwquality the minimum password len is 
+     * hardcoded at 6.
+     */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0);
+    assert(ipapwd_check_policy(&policy, "abcdE", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0);
+    assert(ipapwd_check_policy(&policy, "abDE", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+
+    /* Verify that no credits are added automatically. We need to set some
+     * pwquality option in order to validate it, so set length.
+     */
+    set_policy(&policy, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "abcdE", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_TOO_SHORT);
+
+    /* Lower check.
+     * Negative == minimum # of uppers required
+     * Zero == skip check
+     * Positive == amount to add towards min length
+     */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0);
+    assert(ipapwd_check_policy(&policy, "SECREt123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 0, -19, 0);
+    assert(ipapwd_check_policy(&policy, "SECREt123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_MIN_LOWERS);
+
+    /* lcredit > 0 gives a "credit" to a shorter password for having
+     * complexity so allows a shorter than minimum length password.
+     * Reminder that with libpwquality the minimum password len is 
+     * hardcoded at 6.
+     */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0);
+    assert(ipapwd_check_policy(&policy, "ABCEe", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0);
+    assert(ipapwd_check_policy(&policy, "ABcd", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+
+    /* Verify that no credits are added automatically. We need to set some
+     * pwquality option in order to validate it, so set length.
+     */
+    set_policy(&policy, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    assert(ipapwd_check_policy(&policy, "ABCDE", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_TOO_SHORT);
+
+    /* Mixed credit checks */
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, -2, -2, -2, 0);
+    assert(ipapwd_check_policy(&policy, "SecreT123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_OK);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, -2, -2, -2, 0);
+    assert(ipapwd_check_policy(&policy, "SECREt123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_MIN_LOWERS);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, -2, -2, -2, 0);
+    assert(ipapwd_check_policy(&policy, "Secret123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_MIN_UPPERS);
+    set_policy(&policy, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1);
+    assert(ipapwd_check_policy(&policy, "SecreT123", NULL, 0, 0, 0, 0, NULL) == IPAPWD_POLICY_PWD_MIN_OTHERS);
 
     return 0;
 }
