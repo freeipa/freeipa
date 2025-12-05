e25c61250ddcf6322caf60595b7aff508b8165f7
Julien Rische <jrische@redhat.com>
2025-12-05 16:30:37 +0100

    ipa-getkeytab: pick salt based on credentials type
    When ipa_string_to_enctypes() is used by ipa-getkeytab to get
    identifiers of key/salt types, but the salt type is not provided,
    "normal" was used by default.
    
    In IPA, password-based keys should have a "special" salt type, and
    randomized keys should have a "normal" one. This commit takes this
    parameter into account to select the appropriate salt type.
    
    Signed-off-by: Julien Rische <jrische@redhat.com>

--- util/ipa_krb5.h
+++ util/ipa_krb5.h
@@ -138,8 +138,9 @@ void free_keys_contents(krb5_context krbctx, struct keys_container *keys);
 struct berval *create_key_control(struct keys_container *keys,
                                   const char *principalName);
 
-int ipa_string_to_enctypes(const char *str, struct krb_key_salt **encsalts,
-                           int *num_encsalts, char **err_msg);
+int ipa_string_to_enctypes(const char *str, krb5_int32 default_salttype,
+                           struct krb_key_salt **encsalts, int *num_encsalts,
+                           char **err_msg);
 
 int create_keys(krb5_context krbctx,
                 krb5_principal princ,
