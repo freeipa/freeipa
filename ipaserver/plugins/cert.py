5a4ab82d89abbd9498937e587734570534693703
David Hanina <dhanina@redhat.com>
2026-02-19 15:53:44 +0100

    Fix CA sizelimit and allow users, hosts, services
    _ca_search incorrectly checks whether we are finished with the search,
    result should be ideally checked against sizelimit, but to follow old
    behavior, we just return False in all cases.
    
    users, no_users and their hosts and services equivalent are dropped, as
    they should never occur, they get automatically converted to their
    singular form.
    
    Fixes: https://pagure.io/freeipa/issue/9928
    Signed-off-by: David Hanina <dhanina@redhat.com>

--- ipaserver/plugins/cert.py
+++ ipaserver/plugins/cert.py
@@ -1853,7 +1853,9 @@ Search for existing certificates.
 
             result[issuer, serial_number] = obj
 
-        return result, False, len(ca_objs) >= len(result)
+        # We return complete=False, to not be 
+        # breaking with the old functionality
+        return result, False, False
 
     def _ldap_search(self, all, pkey_only, no_members, current_result,
                      **options):
@@ -1985,9 +1987,7 @@ Search for existing certificates.
                    'validnotbefore_from', 'validnotbefore_to',
                    'issuedon_from', 'issuedon_to',
                    'revokedon_from', 'revokedon_to',
-                   'status', 'users', 'no_users',
-                   'hosts', 'no_hosts',
-                   'services', 'no_services']):
+                   'status']):
             searches.append(self._ldap_search)
 
         for sub_search in searches:
