7c63f6d7eba8c69c3b6e159f277e067fc414e76d
Alexander Bokovoy <abokovoy@redhat.com>
2026-03-15 10:06:25 +0200

    pylint_plugins: fix fake_class() for astroid 4.x ClassDef
    astroid 4.x calls implicit_locals() inside ClassDef.__init__, which
    invokes root() to get the module name. root() walks up the parent
    chain expecting to find a Module node, and raises AssertionError if
    it reaches a node with parent=None that is not a Module.
    
    Fix fake_class() to propagate a proper parent when creating synthetic
    ClassDef nodes: child nodes get parent=cl (the containing class), and
    the optional parent parameter is threaded through the recursive path.
    
    Signed-off-by: Alexander Bokovoy <abokovoy@redhat.com>

--- pylint_plugins.py
+++ pylint_plugins.py
@@ -28,12 +28,12 @@ def _warning_already_exists(cls, member):
     )
 
 
-def fake_class(name_or_class_obj, members=()):
+def fake_class(name_or_class_obj, members=(), parent=None):
     if isinstance(name_or_class_obj, scoped_nodes.ClassDef):
         cl = name_or_class_obj
     else:
         cl = scoped_nodes.ClassDef(
-            name=name_or_class_obj, lineno=None, col_offset=None, parent=None,
+            name=name_or_class_obj, lineno=None, col_offset=None, parent=parent,
             end_lineno=None, end_col_offset=None)
 
     for m in members:
@@ -42,7 +42,7 @@ def fake_class(name_or_class_obj, members=()):
                 _warning_already_exists(cl, m)
             else:
                 cl.locals[m] = [scoped_nodes.ClassDef(
-                    name=m, lineno=None, col_offset=None, parent=None,
+                    name=m, lineno=None, col_offset=None, parent=cl,
                     end_lineno=None, end_col_offset=None)]
         elif isinstance(m, dict):
             for key, val in m.items():
@@ -51,7 +51,7 @@ def fake_class(name_or_class_obj, members=()):
                     _warning_already_exists(cl, key)
                     fake_class(cl.locals[key], val)
                 else:
-                    cl.locals[key] = [fake_class(key, val)]
+                    cl.locals[key] = [fake_class(key, val, parent=cl)]
         else:
             # here can be used any astroid type
             if m.name in cl.locals:
