e5819bddb2911648c693643b353ffa7c5a3c7b1b
Sudhir Menon <sumenon@redhat.com>
2026-03-19 17:12:12 +0530

    ipatests: ipa-migrate-ds test scenarios
    This patch tests ipa-migrated-ds related sceanrios
    
    1. 389-ds instance is setup on client system.
    2. Attached sample instance1.ldif file in data/ds_migration folder.
    3. Attempt ipa-migrate-ds with configuration disabled.
    3. Attempt ipa-migrate-ds over ldaps.
    
    Assisted by: claude-4.6-opus-high <noreply@anthropic.com>
    Signed-off-by: Sudhir Menon <sumenon@redhat.com>
    Reviewed-By: Florence Blanc-Renaud <flo@redhat.com>
    Reviewed-By: Florence Blanc-Renaud <frenaud@redhat.com>

---
+++ ipatests/test_integration/test_ds_migration.py
@@ -0,0 +1,189 @@
+#
+# Copyright (C) 2026  FreeIPA Contributors see COPYING for license
+#
+
+"""
+ipa-migrate-ds migration acceptance tests.
+"""
+
+from __future__ import absolute_import
+
+import os
+import textwrap
+
+from ipatests.test_integration.base import IntegrationTest
+from ipatests.pytest_ipa.integration import tasks
+
+# 389-ds instance name and base DN on the client
+DS_INSTANCE_NAME = "dsinstance_01"
+DS_BASEDN = "dc=testrealm,dc=test"
+DS_PORT = 389
+DS_SECURE_PORT = 636
+
+
+def _setup_389ds_on_client(client, admin_password):
+    """
+    Install 389 Directory Server on the client and load migration
+    test data from instance1.ldif i.e (ou=People, ou=groups,
+    sample user/group).
+    """
+    tasks.install_packages(client, ["389-ds-base"])
+
+    # Create instance via dscreate
+    inf_content = textwrap.dedent("""\
+        [general]
+        full_machine_name = {hostname}
+        [slapd]
+        instance_name = {instance}
+        port = {port}
+        secure_port = {secure_port}
+        root_dn = cn=Directory Manager
+        root_password = {password}
+        [backend-userroot]
+        sample_entries = no
+        suffix = {basedn}
+    """).format(
+        hostname=client.hostname,
+        instance=DS_INSTANCE_NAME,
+        port=DS_PORT,
+        secure_port=DS_SECURE_PORT,
+        password=admin_password,
+        basedn=DS_BASEDN,
+    )
+    client.put_file_contents("/tmp/ds-instance.inf", inf_content)
+    client.run_command(
+        ["dscreate", "from-file", "/tmp/ds-instance.inf"]
+    )
+
+    # Load migration test data from instance1.ldif
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    ldif_path = os.path.join(
+        test_dir, "data", "ds_migration", "instance1.ldif"
+    )
+    with open(ldif_path) as f:
+        ldif_content = f.read()
+    client.put_file_contents("/tmp/instance1.ldif", ldif_content)
+    client.run_command(
+        [
+            "/usr/bin/ldapmodify",
+            "-a", "-x", "-H", "ldap://localhost:{}".format(DS_PORT),
+            "-D", "cn=Directory Manager", "-w", admin_password,
+            "-f", "/tmp/instance1.ldif",
+        ]
+    )
+
+
+class TestDSMigrationConfig(IntegrationTest):
+    """
+    Test ipa migrate-ds related scenarios.
+
+    Uses a client host with 389-ds populated from instance1.ldif
+    (ou=People, ou=groups, dc=testrealm,dc=test) for migration tests.
+    """
+
+    topology = "line"
+    num_replicas = 0
+    num_clients = 1
+
+    @classmethod
+    def install(cls, mh):
+        # Install master and IPA client (full topology)
+        super(TestDSMigrationConfig, cls).install(mh)
+        # On the client host, set up 389-ds with migration test data
+        _setup_389ds_on_client(
+            cls.clients[0],
+            cls.master.config.admin_password,
+        )
+
+    def test_attempt_migration_with_configuration_false(self):
+        """
+        Test attempts ipa-migrate-ds with migration disabled.
+        """
+        tasks.kinit_admin(self.master)
+        # Ensure migration is disabled
+        cmd = ["ipa", "config-mod", "--enable-migration", "FALSE"]
+        error_msg = "ipa: ERROR: no modifications to be performed"
+        result = self.master.run_command(cmd, raiseonerr=False)
+        assert result.returncode != 0
+        tasks.assert_error(result, error_msg)
+        client_host = self.clients[0].hostname
+        ldap_uri = "ldap://{}:{}".format(client_host, DS_PORT)
+        result = self.master.run_command(
+            [
+                "ipa",
+                "migrate-ds",
+                "--user-container=ou=People",
+                "--group-container=ou=groups",
+                ldap_uri,
+            ],
+            stdin_text=self.master.config.admin_password,
+            raiseonerr=False,
+        )
+        assert (
+            result.returncode != 0
+        ), "migrate-ds should fail when migration is disabled"
+        assert "migration mode is disabled" in (
+            result.stdout_text + result.stderr_text
+        ).lower()
+
+    def test_migration_over_ldaps(self):
+        """
+        Migrate from the client's 389-ds over LDAPS (port 636).
+        """
+        ca_cert_file = "/etc/ipa/remoteds.crt"
+        tasks.kinit_admin(self.master)
+        self.master.run_command(
+            ["ipa", "config-mod", "--enable-migration", "TRUE"],
+        )
+
+        # Copy 389-ds CA cert from client to master for LDAPS verification
+        client = self.clients[0]
+        ds_cert_dir = "/etc/dirsrv/slapd-{}".format(DS_INSTANCE_NAME)
+        cert_result = client.run_command(
+            [
+                "certutil", "-d", ds_cert_dir, "-L", "-n",
+                "Self-Signed-CA", "-a",
+            ],
+        )
+        self.master.put_file_contents(
+            ca_cert_file, cert_result.stdout_text
+        )
+        self.master.run_command(
+            ["restorecon", ca_cert_file], raiseonerr=False
+        )
+
+        client_host = client.hostname
+        ldaps_uri = "ldaps://{}:{}".format(client_host, DS_SECURE_PORT)
+        user_container = "ou=People,{}".format(DS_BASEDN)
+        group_container = "ou=groups,{}".format(DS_BASEDN)
+
+        self.master.run_command(
+            [
+                "ipa",
+                "migrate-ds",
+                "--with-compat",
+                "--user-container",
+                user_container,
+                "--group-container",
+                group_container,
+                ldaps_uri,
+                "--ca-cert-file",
+                ca_cert_file,
+            ],
+            stdin_text=self.master.config.admin_password,
+        )
+        # Verify migrated user and group from instance1.ldif
+        self.master.run_command(
+            ["ipa", "user-show", "ldapuser_0001"]
+        )
+        self.master.run_command(
+            ["ipa", "group-show", "ldapgroup_0001"]
+        )
+
+        # Clean up migrated user and group
+        self.master.run_command(
+            ["ipa", "user-del", "ldapuser_0001"], raiseonerr=False
+        )
+        self.master.run_command(
+            ["ipa", "group-del", "ldapgroup_0001"], raiseonerr=False
+        )
