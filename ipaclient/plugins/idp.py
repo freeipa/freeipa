a352cbfbaead28c64ffa8527ea8a0d1c84bcd0a9
Florence Blanc-Renaud <flo@redhat.com>
2026-04-24 11:21:59 +0200

    External Idp: add new parameters for client authentication
    ipa idp-add now supports
    --client-auth-method [client_secret|private_key_jwt|tls_client_auth]
    --client-cert-p12-file /path/to/pkcs12file
    
    If the client-auth-method is not provided it defaults to client_secret.
    
    ipa idp-mod allows to switch from one client auth method to another:
    - when switching to client_secret, the userpkcs12 and usercertificate
    values are removed.
    - when switching to private_key_jwt or tls_client_auth the pkcs12 file
    is required
    
    Signed-off-by: Florence Blanc-Renaud <flo@redhat.com>

---
+++ ipaclient/plugins/idp.py
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2026  FreeIPA Contributors see COPYING for license
+#
+
+from ipaclient.frontend import MethodOverride
+from ipalib import errors
+from ipalib.parameters import BinaryFile
+from ipalib.plugable import Registry
+from ipalib.text import _
+
+register = Registry()
+
+
+class IdpOverride(MethodOverride):
+    takes_options = (
+        BinaryFile(
+            'client_cert_p12_file?',
+            label=_("Input filename"),
+            doc=_('File to load the PKCS12 bundle from.'),
+            include='cli',
+        ),
+    )
+
+    def interactive_prompt_callback(self, kw):
+        # Password is required when the PKCS12 bundle is provided
+        if kw.get('client_cert_p12_file', None) and \
+           not kw.get('ipaidpclientsecret', None):
+            kw['ipaidpclientsecret'] = self.Backend.textui.prompt_password(
+                _("PKCS#12 password"), confirm=False,
+            )
+
+    def forward(self, *args, **options):
+        if self.api.env.context == 'cli':
+            if 'client_cert_p12_file' in options:
+                # Secret is required to open the PKCS12 bundle
+                if 'ipaidpclientsecret' not in options:
+                    raise errors.RequirementError(name='secret')
+
+                # Fill the client_cert_p12 option with the bytes
+                p12file_data = options.get('client_cert_p12_file')
+                options['userpkcs12'] = p12file_data
+                del options['client_cert_p12_file']
+
+        return super(IdpOverride, self).forward(*args, **options)
+
+
+@register(override=True, no_fail=True)
+class idp_add(IdpOverride):
+    pass
+
+
+@register(override=True, no_fail=True)
+class idp_mod(IdpOverride):
+    pass
--- ipatests/test_xmlrpc/test_idp_plugin.py
+++ ipatests/test_xmlrpc/test_idp_plugin.py
@@ -6,12 +6,22 @@
 Test the `ipaserver.plugins.idp` module.
 """
 
+import datetime
+import os
 import pytest
+import tempfile
 
-from ipalib import errors
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.x509.oid import NameOID
+
+from ipalib import api, errors
 from ipatests.test_xmlrpc.xmlrpc_test import (
     XMLRPC_test, raises_exact)
 from ipatests.test_xmlrpc.tracker.idp_plugin import IdpTracker
+from ipapython import ipautil
 
 google_auth = "https://accounts.google.com/o/oauth2/auth"
 google_devauth = "https://oauth2.googleapis.com/device/code"
@@ -263,3 +273,337 @@ class TestCreateIdp(XMLRPC_test):
             name='client_id'
         )):
             command()
+
+
+PKCS12PASSWORD = "PKCS12Password"
+
+
+def get_pkcs12_path(subject_cn, password):
+    """
+    Create a tempdir with various PKCS12 bundles
+
+    client.p12 with cert and key
+    onlycert.p12 with only the cert
+    onlykey.p12 with only the key
+    Returns the path to temp dir
+    """
+    tempdir = tempfile.mkdtemp(prefix="tmp-")
+    subject = x509.Name([x509.NameAttribute(NameOID.COMMON_NAME,
+                                            f"CN={subject_cn}"),])
+
+    # Generate the key
+    key = rsa.generate_private_key(
+        public_exponent=65537, key_size=2048, backend=default_backend())
+
+    # Generate self-signed cert with CN=ipa-oauth-client.example.com
+    builder = x509.CertificateBuilder()
+    builder = builder.serial_number(x509.random_serial_number())
+    builder = builder.issuer_name(subject)
+    builder = builder.subject_name(subject)
+    builder = builder.public_key(key.public_key())
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    builder = builder.not_valid_before(now)
+    builder = builder.not_valid_after(now + datetime.timedelta(days=365))
+    builder = builder.add_extension(
+        x509.SubjectAlternativeName([
+            x509.DNSName("localhost"), x509.DNSName(api.env.host)]),
+        critical=False,
+    )
+
+    cert = builder.sign(
+        private_key=key,
+        algorithm=hashes.SHA256(),
+        backend=default_backend(),
+    )
+
+    key_pem = key.private_bytes(
+        serialization.Encoding.PEM,
+        serialization.PrivateFormat.PKCS8,
+        serialization.BestAvailableEncryption(password.encode()),
+    )
+    cert_pem = cert.public_bytes(serialization.Encoding.PEM)
+
+    keyfile = os.path.join(tempdir, f'{subject_cn}.key')
+    certfile = os.path.join(tempdir, f'{subject_cn}.crt')
+    p12file = os.path.join(tempdir, 'client.p12')
+    onlycertfile = os.path.join(tempdir, 'onlycert.p12')
+    onlykeyfile = os.path.join(tempdir, 'onlykey.p12')
+
+    with open(keyfile, 'wb') as f:
+        f.write(key_pem)
+    with open(certfile, 'wb') as f:
+        f.write(cert_pem)
+
+    # Export key + cert in .p12 file
+    ipautil.run(['openssl', 'pkcs12', '-export', '-out', p12file,
+                 '-inkey', keyfile, '-in', certfile,
+                 '-passin', f'pass:{password}',
+                 '-passout', f'pass:{password}'])
+
+    # Export only cert in onlycert.p12
+    ipautil.run(['openssl', 'pkcs12', '-export', '-out', onlycertfile,
+                 '-in', certfile,
+                 '-passin', f'pass:{password}',
+                 '-passout', f'pass:{password}',
+                 '-nokeys'])
+
+    # Export only key in onlykey.p12
+    ipautil.run(['openssl', 'pkcs12', '-export', '-out', onlykeyfile,
+                 '-inkey', keyfile,
+                 '-passin', f'pass:{password}',
+                 '-passout', f'pass:{password}',
+                 '-nocerts'])
+    return tempdir
+
+
+@pytest.fixture(scope='class')
+def pkcs12_path():
+    return get_pkcs12_path("ipa-oauth-client", PKCS12PASSWORD)
+
+
+@pytest.fixture(scope='class')
+def pkcs12_new_path():
+    return get_pkcs12_path("ipa-new-oauth-client", PKCS12PASSWORD)
+
+
+def read_pkcs12(p12file):
+    """ Extract pkcs12 data and cert from pkcs12 file """
+    with open(p12file, 'rb') as f:
+        p12file_data = f.read()
+    return p12file_data
+
+
+@pytest.mark.tier1
+class TestCreateIdpClientAuth(XMLRPC_test):
+    """ Tests for client authentication with certificate """
+
+    def test_create_idp_client_secret(self, pkcs12_path):
+        """ Creation with client_secret and p12 file"""
+        # Specify --client-cert-p12-file and --client-auth-method=client_secret
+        idp = IdpTracker(
+            'inconsistent_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod="client_secret",
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")))
+        idp.track_create()
+        command = idp.make_create_command()
+        with raises_exact(errors.MutuallyExclusiveError(
+            reason='cannot use client_secret authentication and '
+                   'client-cert-p12-file'
+        )):
+            command()
+
+        # Specify --client-cert-p12-file and no --client-auth-method
+        # (defaults to client_secret)
+        idp2 = IdpTracker(
+            'inconsistent2_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")))
+        idp2.track_create()
+        command = idp2.make_create_command()
+        with raises_exact(errors.MutuallyExclusiveError(
+            reason='cannot use client_secret authentication and '
+                   'client-cert-p12-file'
+        )):
+            command()
+
+    @pytest.mark.parametrize("method", ['tls_client_auth', 'private_key_jwt'])
+    def test_create_idp_missing_p12(self, method):
+        """ Creation with cert auth and missing p12 file"""
+        # Specify --client-auth-method=method but no p12 file
+        idp = IdpTracker(
+            'missingp12_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod=method)
+        idp.track_create()
+        command = idp.make_create_command()
+        with raises_exact(errors.RequirementError(
+            name="client-cert-p12-file"
+        )):
+            command()
+
+    @pytest.mark.parametrize("method", ['tls_client_auth', 'private_key_jwt'])
+    def test_create_idp_wrong_secret(self, method, pkcs12_path):
+        """ Creation with cert auth and p12 file but wrong secret"""
+        idp = IdpTracker(
+            'bad_pass_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod=method,
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+            ipaidpclientsecret='WrongPassword')
+        idp.track_create()
+        command = idp.make_create_command()
+        with raises_exact(errors.ValidationError(
+            name='client_cert_p12_file',
+            error="Cannot decode PKCS12 file: "
+                  "Invalid password or PKCS12 data")):
+            command()
+
+    @pytest.mark.parametrize("method", ['tls_client_auth', 'private_key_jwt'])
+    def test_create_idp_secret(self, method, pkcs12_path):
+        """ Creation with cert auth and p12 file"""
+        idp = IdpTracker(
+            f'{method}_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod=method,
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+            ipaidpclientsecret=PKCS12PASSWORD)
+        idp.create()
+        idp.delete()
+
+    @pytest.mark.parametrize("method", ['tls_client_auth', 'private_key_jwt'])
+    def test_switch_to_client_secret(self, method, pkcs12_path):
+        """ Switch to client secret"""
+        idp = IdpTracker(
+            f'{method}_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod=method,
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+            ipaidpclientsecret=PKCS12PASSWORD)
+        idp.create()
+        # Switch to client secret with a pkcs12file, expect error
+        command = idp.make_update_command(updates=dict(
+            ipaidpclientauthmethod='client_secret',
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+        ))
+        with raises_exact(errors.MutuallyExclusiveError(
+            reason='cannot use client_secret authentication and '
+                   'client-cert-p12-file'
+        )):
+            command()
+        # Switch to client secret without additional param, success
+        idp.update(
+            updates=dict(ipaidpclientauthmethod='client_secret')
+        )
+        # Re-run the update with no change, expect error
+        command = idp.make_update_command(
+            updates=dict(ipaidpclientauthmethod='client_secret')
+        )
+        with raises_exact(errors.EmptyModlist()):
+            command()
+
+        idp.delete()
+
+    @pytest.mark.parametrize("method", ['tls_client_auth', 'private_key_jwt'])
+    def test_switch_to_cert_auth(self, method, pkcs12_path):
+        """ Switch to another auth method"""
+        idp = IdpTracker(
+            f'{method}_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+        )
+        idp.create()
+        # Switch to cert auth without a pkcs12file, expect error
+        command = idp.make_update_command(updates=dict(
+            ipaidpclientauthmethod=method,
+        ))
+        with raises_exact(errors.RequirementError(name="client-cert-p12-file")):
+            command()
+
+        # Switch to cert auth with pkcs12 file but wrong pwd, failure
+        command = idp.make_update_command(updates=dict(
+            ipaidpclientauthmethod=method,
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+            ipaidpclientsecret='WrongPass')
+        )
+        with raises_exact(errors.ValidationError(
+            name='client_cert_p12_file',
+            error="Cannot decode PKCS12 file: "
+                  "Invalid password or PKCS12 data")):
+            command()
+
+        # Switch to cert auth with pkcs12 file and good pwd
+        idp.update(
+            updates=dict(
+                ipaidpclientauthmethod=method,
+                userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+                ipaidpclientsecret=PKCS12PASSWORD),
+        )
+        idp.delete()
+
+    def test_switch_cert_auth(self, pkcs12_path):
+        """
+        Switch from tls_client_auth to private_key_jwt and back
+        """
+        idp = IdpTracker(
+            'certauth_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod='tls_client_auth',
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+            ipaidpclientsecret=PKCS12PASSWORD
+        )
+        idp.create()
+        # Switch to private_key_jwt
+        idp.update(updates=dict(
+            ipaidpclientauthmethod='private_key_jwt',
+        ))
+        # Switch back to tls_client_auth
+        idp.update(updates=dict(
+            ipaidpclientauthmethod='tls_client_auth',
+        ))
+        idp.delete()
+
+    def test_replace_pkcs12file(self, pkcs12_path, pkcs12_new_path):
+        """
+        Update the PKCS12 data
+        """
+        method = 'tls_client_auth'
+        idp = IdpTracker(
+            'newpkcs12_idp', ipaidpauthendpoint=google_auth,
+            ipaidpdevauthendpoint=google_devauth,
+            ipaidptokenendpoint=google_token,
+            ipaidpuserinfoendpoint=google_userinfo,
+            ipaidpkeysendpoint=google_jwks,
+            ipaidpclientid="idp1client",
+            ipaidpclientauthmethod=method,
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_path,"client.p12")),
+            ipaidpclientsecret=PKCS12PASSWORD
+        )
+        idp.create()
+        # Use another pkcs12 file, wrong password
+        command = idp.make_update_command(updates=dict(
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_new_path,"client.p12")),
+            ipaidpclientsecret='WrongPass')
+        )
+        with raises_exact(errors.ValidationError(
+            name='client_cert_p12_file',
+            error="Cannot decode PKCS12 file: "
+                  "Invalid password or PKCS12 data")):
+            command()
+        # Use another pkcs12 file, good password
+        idp.update(updates=dict(
+            userpkcs12=read_pkcs12(os.path.join(pkcs12_new_path,"client.p12")),
+            ipaidpclientsecret=PKCS12PASSWORD)
+        )
+        idp.delete()
