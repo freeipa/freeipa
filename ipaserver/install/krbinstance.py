f339bc3dba715ef63370a02c09b3e648187a2c0a
David Hanina <dhanina@redhat.com>
2026-06-01 08:56:40 +0200

    Integrate keytab rotation in ipactl and krbinstance
    Start krb5kdc before other IPA services so keytab rotation can run
    with a live KDC. When rotation updates keytabs, stop and restart all
    services so every daemon loads the new keys.
    
    Refactors ipactl start/restart.
    
    Signed-off-by: David Hanina <dhanina@redhat.com>

--- ipaserver/install/krbinstance.py
+++ ipaserver/install/krbinstance.py
@@ -42,6 +42,7 @@ from ipalib.constants import ANON_USER
 from ipalib.install import certmonger
 from ipapython.dn import DN
 from ipapython.dogtag import KDC_PROFILE
+from ipapython.krb5util import cleanup_scheduled_keytab_removals
 
 from ipaserver.install import replication
 from ipaserver.install import certs
@@ -86,18 +87,29 @@ def is_pkinit_enabled():
     return False
 
 
-def get_krb_key_types():
+def _get_krb_key_types():
+    """
+    Retrieves master key and supported types through ipa-getkeytab
+    as strings, supported types are delimited by a space.
+
+    :raise EnvironmentError: If no encryption types are provided
+
+    :return: master key type, supported key types
+    :rtype: str, str
+    """
     result = ipautil.run([paths.IPA_GETKEYTAB, '--permitted-enctypes'],
                          capture_output=True)
     permitted_enctypes = result.output.splitlines()
     if not permitted_enctypes:
-        raise EnvironmentError('ipa-getkeytab provided no permitted '
-                               'encryption types')
+        raise EnvironmentError(
+            "ipa-getkeytab provided no permitted encryption types"
+        )
 
-    supported_enctypes = [e + ':' + s for e in permitted_enctypes
-                          for s in ['special', 'normal']]
+    supported_enctypes = [
+        e + ":" + s for e in permitted_enctypes for s in ["special", "normal"]
+    ]
 
-    return (permitted_enctypes[0], ' '.join(supported_enctypes))
+    return (permitted_enctypes[0], " ".join(supported_enctypes))
 
 
 class KpasswdInstance(service.SimpleServiceInstance):
@@ -309,7 +321,7 @@ class KrbInstance(service.Service):
                              INCLUDES=includes,
                              FIPS='#' if fips_enabled else '')
 
-        mkey_type, supported_key_types = get_krb_key_types()
+        mkey_type, supported_key_types = _get_krb_key_types()
 
         self.sub_dict['SUPPORTED_ENCTYPES'] = supported_key_types
         self.sub_dict['MASTER_KEY_TYPE'] = mkey_type
@@ -706,3 +718,5 @@ class KrbInstance(service.Service):
         ipautil.remove_file(paths.KRB5_KEYTAB)
         ipautil.remove_file(paths.KRB5_FREEIPA_DEFAULTS)
         ipautil.remove_file(paths.KRB5_FREEIPA_SERVER)
+
+        cleanup_scheduled_keytab_removals()
