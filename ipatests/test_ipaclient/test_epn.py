f59fbceef826ea528b9e7f59003d804ad48535d1
Florence Blanc-Renaud <flo@redhat.com>
2026-07-08 10:16:06 +0200

    ipa-epn: fix transposed uid/gid and fail closed on drop_privileges()
    drop_privileges() passed pw_uid to setgid() and gr_gid to setuid(), and
    swallowed any exception so the notifier continued as root when the drop
    failed.
    
    Pass the right ids to the right syscalls, and abort on failure.
    
    Add a unit test for the drop_privileges method
    
    Fixes: https://codeberg.org/freeipa/freeipa/issues/10030
    Signed-off-by: Huzaifa Sidhpurwala <huzaifas@redhat.com>
    Signed-off-by: Florence Blanc-Renaud <flo@redhat.com>
    Reviewed-By: Alexander Bokovoy <abbra@users.noreply.github.com>

---
+++ ipatests/test_ipaclient/test_epn.py
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2026  FreeIPA Contributors see COPYING for license
+#
+
+import pytest
+import ipatests.util
+
+ipatests.util.check_ipaclient_unittests()
+
+from ipaclient.install.ipa_epn import drop_privileges
+from ipapython import admintool
+
+
+@pytest.mark.parametrize(
+    "user,group", [
+        ("unknown_user", "daemon"),
+        ("daemon", "unknown_group"),
+        ("unknown_user", "unknown_group")])
+def test_drop_privileges(user, group):
+    """ Test the drop_privileges method with unknown uid/gid"""
+    try:
+        drop_privileges(user, group)
+    except admintool.ScriptError:
+        # Raised error as expected
+        pass
+    else:
+        assert False
