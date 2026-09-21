65f058a2c01d5c9707ef3cfdaa265a8a16492bf4
Florence Blanc-Renaud <flo@redhat.com>
2026-09-21 19:52:44 +0200

    Azure CI: force m2r2 version
    With the new version of m2r2, the azure pipeline Docs is failing.
    Force version < 1.0.0
    
    Fixes: https://codeberg.org/freeipa/freeipa/issues/10068
    
    Signed-off-by: Florence Blanc-Renaud <flo@redhat.com>
    Reviewed-By: David Hanina <dhanina@redhat.com>

--- ipasphinx/setup.py
+++ ipasphinx/setup.py
@@ -16,5 +16,7 @@ if __name__ == "__main__":
         doc=__doc__,
         package_dir={"ipasphinx": ""},
         packages=["ipasphinx"],
-        install_requires=["ipaserver", "ipalib", "sphinx > 3.0", "m2r2"],
+        install_requires=[
+            "ipaserver", "ipalib",
+            "sphinx > 3.0", "m2r2 < 1.0.0"],
     )
